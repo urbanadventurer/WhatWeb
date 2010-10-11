f7a8825886a9163ab4fc87522373c38646782136
Andrew Horton <horton.nz@gmail.com>
2010-10-11 19:57:58 +1300

    Updated episerver plugin

--- plugins/episerver.rb
+++ plugins/episerver.rb
@@ -5,30 +5,23 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+
+# Version 0.2 by Andrew Horton
+# removed VIEWSTATE match as it matches ASP.NET, removed :probability=>100
+
 Plugin.define "EPiServer" do
 author "Patrik Wallström"
-version "0.1"
+version "0.2"
 description "CMS from EPiServer - http://episerver.com/"
 
 examples %w| www.stockholm.se www.sweden.se www.fritidsresor.se|
 
-# identifying strings
-# <meta name="GENERATOR" content="EPiServer" /> 
-# <!-- EPiServer -->
-# <input type="hidden" name="__VIEWSTATE" value=
-
 matches [
 {:name=>"meta generator tag with EPiServer", 
-:probability=>100,
 :text=>"<meta name=\"GENERATOR\" content=\"EPiServer\" />"},
 
 {:name=>"EPiServer comment", 
-:probability=>100,
-:text=>"<!-- EPiServer -->"},
-
-{:name=>"using __VIEWSTATE",
-:probability=>100,
-:text=>"<input type=\"hidden\" name=\"__VIEWSTATE\" id=\"__VIEWSTATE\" value=\""}
+:text=>"<!-- EPiServer -->"}
 
 ]
 
