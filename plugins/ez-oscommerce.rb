55ee1c909838e5a8a151c6ab7e4e627e71f47f71
Brendan Coles <discord@brendon.vpsnode.net>
2010-10-12 23:10:38 +1030

    Added ez-oscommerce plugin

---
+++ plugins/ez-oscommerce.rb
@@ -0,0 +1,24 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "ez-oscommerce" do
+author "Brendan Coles <bcoles@gmail.com>" # 2010-10-12
+version "0.1"
+description "PHP powered shopping cart - homepage: http://www.eptcel.com.br/"
+
+# 1 result for "powered by ez oscommerce" @ 2010-10-12
+examples %w|
+www.eptcel.com.br/ecommerce/product_reviews.php
+|
+
+matches [
+
+{ :text=>'Powered by <a href="http://www.eptcel.com.br" target="_blank">ez oscommerce</a>' },
+
+]
+
+end
+
