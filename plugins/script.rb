e26a9d552203727b3c5eacdb4ce4c3f66f3f13f4
bcoles <bcoles@gmail.com>
2010-10-15 00:53:50 +0800

    Updated script plugin

--- plugins/script.rb
+++ plugins/script.rb
@@ -4,9 +4,12 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 
+# Fixed regex to return multiple scripts
+##
 Plugin.define "Script" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-14
-version "0.1"
+version "0.2"
 description "This plugin detects instances of script HTML elements and grabs the URL."
 
 # 384 results for "your browser does not support javascript" @ 2010-10-14
@@ -18,12 +21,17 @@ wordpress.com
 www.microsoft.com
 |
 
-matches [
-
 # URL Extraction
-{ :version=>/<[\s]*script[^src]+src[\s]*=[\s]*[\"|\']*([^>^\"]+)/i, :version_regexp_offset=>0 },
+def passive
+	m=[]
+		if @body =~ /<[\s]*script[^src]+src[\s]*=[\s]*[\"|\']*([^>^\"]+)/i
+		version=@body.scan(/<[\s]*script[^src]+src[\s]*=[\s]*[\"|\']*([^>^\"]+)/i)
+		m << { :version=>version }
+	end
 
-]
+	m
+
+end
 
 end
 
