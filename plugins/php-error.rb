a237ebfc957b9f7e5fdddf4574d5bfa24789eb14
bcoles <bcoles@gmail.com>
2010-10-18 23:40:40 +1030

    Updated php-error plugin

--- plugins/php-error.rb
+++ plugins/php-error.rb
@@ -4,31 +4,31 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
+# Version 0.3 # Brendan Coles <bcoles@gmail.com>
+# Added local file path extraction
 # Version 0.2
 # removed :certainty
 
-Plugin.define "PHPError" do
+Plugin.define "PHP-Error" do
 author "Andrew Horton"
-version "0.2"
-description "PHP errors"
+version "0.3"
+description "This plugin identifies PHP errors and extracts the local file path."
 
-examples %w| www.orodinapoli.it www.surfpix.co.nz|
+examples %w|
+itpak.net/wp-content/themes/default/
+studio306.com/weblog/wp-content/themes/default/
+yvonnelao.com/wp-content/themes/default/
+zendedel.com/blog/wp-content/themes/classic/index.php
+www.nurse-power.com/blog/wp-content/themes/default/
+www.wunschrausch.com/blog/wp-content/themes/default/
+|
 
 matches [
-{:name=>"php error",
-:regexp=>/<b>(Warning|Fatal error)<\/b>: .*on line <b>[0-9]+<\/b><br \/>/ }
-]
 
-# return filepaths that end in .inc :) -- source disclosure
-#def passive
-#	m={}
-#	
-#	if @body.scan(/<b>(Warning|Fatal error)<\/b>: .*([^ ]+\.inc) on line <b>[0-9]+<\/b><br \/>/)
-#		inc=@body.scan(/<b>(Warning|Fatal error)<\/b>: .*([^ ]+\.inc) on line <b>[0-9]+<\/b><br \/>/)[0][1]
-#	end
-#	m
-#end
+# Extract local file path
+{ :version=>/<b>(Warning|Fatal error)<\/b>: .* in <b>([^<]+)<\/b> on line <b>[0-9]+<\/b><br \/>/, :version_regexp_offset=>1 },
+
+]
 
 end
 
