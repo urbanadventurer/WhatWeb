61e36ff6b11f6f4f64bf7567e33b50cb9f5ea037
bcoles <bcoles@gmail.com>
2010-10-20 12:38:10 +0800

    Updated confluence plugin

--- plugins/confluence.rb
+++ plugins/confluence.rb
@@ -4,11 +4,15 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
+# Version 0.2 #
+# Updated version detection
+##
 Plugin.define "Confluence" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-06
-version "0.1"
+version "0.2"
 description "Confluence combines powerful online authoring capabilities, deep Office integration and an extensive plugin catalog to help people work better together and share information effortlessly. - homepage: http://www.atlassian.com/software/confluence/"
+
+# About 3,490 results for "Atlassian Confluence" +"the Enterprise Wiki" +"Intranet software for documentation and knowledge management" +"Report a bug" @ 2010-06-01
 examples %w|
 sandbox.onconfluence.com
 artemistech.com
@@ -37,22 +41,16 @@ support.process-one.net/doc/display/
 
 matches [
 
-# About 3,490 results @ 2010-06-01
-{:certainty=>75, :ghdb=>'"Atlassian Confluence" +"the Enterprise Wiki" +"Intranet software for documentation and knowledge management" +"Report a bug"'},
-{:name=>"powered by text",:text=>'<li class="noprint"><a href="http://www.atlassian.com/software/confluence" class="hover-footer-link">Atlassian Confluence</a> '}
+# GHDB Match
+{ :certainty=>75, :ghdb=>'"Atlassian Confluence" "the Enterprise Wiki" "Intranet software for documentation and knowledge management" "Report a bug"' },
+
+# Powered by text
+{ :text=>'<li class="noprint"><a href="http://www.atlassian.com/software/confluence" class="hover-footer-link">Atlassian Confluence</a>' },
+
+# Version detection # Powered by text
+{ :version=>/<a href="http:\/\/www.atlassian.com\/software\/confluence" class="hover-footer-link">Atlassian Confluence<\/a> ([0-9_\.]+)/, :version_regexp_offset=>0 },
 
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<li class="noprint"><a href="http:\/\/www.atlassian.com\/software\/confluence" class="hover-footer-link">Atlassian Confluence<\/a> [0-9_\.]+/
-		v=@body.scan(/<a href="http:\/\/www.atlassian.com\/software\/confluence" class="hover-footer-link">Atlassian Confluence<\/a> ([0-9_\.]+)/)[0].to_s
-		m << {:name=>"powered by version", :version=>v }
-        end
-        m
-
-end
 
 end
 
