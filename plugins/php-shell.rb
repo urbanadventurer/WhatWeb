201c69d67a11b87608ac54aad35e40a9dc824272
bcoles <bcoles@gmail.com>
2010-10-20 23:21:34 +1030

    Updated php-shell plugin

--- plugins/php-shell.rb
+++ plugins/php-shell.rb
@@ -4,10 +4,16 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Fixed regex
+##
 Plugin.define "PHP-Shell" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-06
-version "0.1"
+version "0.2"
 description "php remote shell and file explorer"
+
+# http://johnny.ihackstuff.com/ghdb/?function=detail&id=833
+# About 48 results for +filetype:php +HAXPLORER +"Server Files Browser" +Browsing +"Script Location" @ 2010-06-06
 examples %w|
 www.raveneye1.com/1.php
 www.s-solution.sk/honey/filemanager/1.php
@@ -19,24 +25,14 @@ www.island.net/~zapper/cgi.php
 
 matches [
 
-# http://johnny.ihackstuff.com/ghdb/?function=detail&id=833
-# About 48 results @ 2010-06-06
-{:name=>'GHDB: +filetype:php +HAXPLORER +"Server Files Browser" +Browsing +"Script Location"',
-:certainty=>75,
-:ghdb=>'+filetype:php +HAXPLORER +"Server Files Browser" +Browsing +"Script Location"'
-}
+# GHDB Match
+{ :ghdb=>'+filetype:php +HAXPLORER +"Server Files Browser" +Browsing +"Script Location"', :certainty=>75 },
+
+# Version detection
+{ :version=>/PHPShell by [a-zA-Z0-9]+ - Version ([0-9a-z\.]+)/, :version_regexp_offset=>0 },
 
 ]
 
-def passive
-        m=[]
-
-        if @body =~ /PHPShell by [a-zA-Z0-9]+ - Version/
-                        v=@body.scan(/PHPShell by [a-zA-Z0-9]+ - Version ([0-9a-z\.]+)/)[0].to_s
-                        m << {:name=>"footer html", :version=>v }
-        end
-        m
 end
 
-end
 
