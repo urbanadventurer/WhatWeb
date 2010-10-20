fdea430bd638b6427e63d204d539c40275ef5467
bcoles <bcoles@gmail.com>
2010-10-20 17:42:00 +0800

    Added meta-zipcode plugin

---
+++ plugins/meta-zipcode.rb
@@ -0,0 +1,25 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Meta-Zipcode" do
+author "Brendan Coles <bcoles@gmail.com>" # 2010-10-20
+version "0.1"
+description "This plugin retrieves the zipcode from the meta zipcode tag. This tag is used by the Gigablast search engine."
+
+examples %w|
+http://www.gigablast.com/tagsdemo.html
+|
+
+matches [
+
+# Extract zipcode
+{ :version=>/<meta[^>]+name[\s]*=[\s]*[\'|\"]?zipcode[^>]+content[\s]*=[\s]*[\'|\"]?([^\'^\"\/^>]+)/i, :version_regexp_offset=>0 },
+{ :version=>/<meta[^>]+content[\s]*=[\s]*[\'|\"]?([^\"^\']+)[^>]+name[\s]*=[\s]*[\'|\"]?zipcode/i, :version_regexp_offset=>0 },
+
+]
+
+end
+
