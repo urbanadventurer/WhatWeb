7ebd1252a642236045b7cc1e51284a4a158b96e6
bcoles <bcoles@gmail.com>
2010-10-20 20:02:41 +0800

    Updated philboard plugin

--- plugins/philboard.rb
+++ plugins/philboard.rb
@@ -4,10 +4,15 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Updated version detection and ghdb match
+##
 Plugin.define "PhilBoard" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-27
-version "0.1"
-description "forum - homepage: http://www.philboards.com/"
+version "0.2"
+description "PhilBoard - ASP powered forum - homepage: http://www.philboards.com/"
+
+# 47 results for "powered by philboard" -Vulnerabilities -Vulnerability @ 2010-06-27
 examples %w|
 www.indiessoft.com/doeaccalumni/mb/philboard.asp
 www.seawindrc.com/forums/philboard.asp
@@ -26,31 +31,13 @@ www.ipkps.bsu.edu.ru/forum/philboard.asp
 
 matches [
 
-# 47 results @ 2010-06-27
-{:name=>'GHDB: "powered by philboard" -Vulnerabilities -Vulnerability',
-:certainty=>75,
-:ghdb=>'"powered by philboard" -Vulnerabilities -Vulnerability'
-},
+# GHDB Match
+{ :ghdb=>'"powered by philboard" inurl:philboard.asp', :certainty=>75 },
+
+# Version detection # Default logo HTML
+{ :version=>/<img src="images\/philboard_small.gif" alt="powered by philboard [v]*([\d\.]+)" width="76" height="21" border="0" align="middle">/, :version_regexp_offset=>0 },
 
 ]
-
-# <img src="images/philboard_small.gif" alt="powered by philboard 1.02" width="76" height="21" border="0" align="middle">
-# <img src="images/philboard_small.gif" alt="powered by philboard 1.12" width="76" height="21" border="0" align="middle">
-# <img src="images/philboard_small.gif" alt="powered by philboard v1.14" width="76" height="21" border="0" align="middle">
-# <img src="images/philboard_small.gif" alt="powered by philboard 1.12" width="76" height="21" border="0" align="middle">
-
-def passive
-        m=[]
-
-        if @body =~ /<img src="images\/philboard_small.gif" alt="powered by philboard [v]*[\d\.]+" width="76" height="21" border="0" align="middle">/
-                version=@body.scan(/<img src="images\/philboard_small.gif" alt="powered by philboard [v]*([\d\.]+)" width="76" height="21" border="0" align="middle">/)[0][0]
-                m << {:certainty=>100,:name=>"powered by version text",:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
