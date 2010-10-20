f381e9ba86a447ebb14df71df022fbf895a378cd
bcoles <bcoles@gmail.com>
2010-10-20 14:05:05 +1030

    Moved rss_feed plugin

---
+++ disabled-plugins/rss_feed.rb
@@ -0,0 +1,33 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+# Version 0.2 # Brendan Coles <bcoles@gmail.com>
+# Fixed regex
+##
+Plugin.define "RSS-Feed" do
+author "Caleb Anderson"
+version "0.2"
+description "This plugin detects RSS feeds and extracts the RSS feed source."
+
+examples %w|
+itsecuritysolutions.org
+morningstarsecurity.com
+ha.ckers.org
+www.kiwicon.org
+adrianlamologs.blogspot.com
+www.wired.com
+|
+
+matches [
+
+# Extract RSS feed source
+{ :version=>/<link[^>]*href[\s]*=[\s]*["']([^\'^\"^\s^>]+)[^>]*type[\s]*=[\s]*["']application\/rss\+xml["'][^>]*/i, :version_regexp_offset=>0 },
+{ :version=>/<link[^>]*type[\s]*=[\s]*["']application\/rss\+xml["'][^>]*href[\s]*=[\s]*["']([^\'^\"^\s^>]+)[^>]*/i, :version_regexp_offset=>0 },
+
+]
+
+end
+
