f38e8056b49daa9237dc610fd8bfd1a1af669edb
bcoles <bcoles@gmail.com>
2010-10-20 17:11:05 +0800

    Added meta-keywords plugin

---
+++ disabled-plugins/meta-keywords.rb
@@ -0,0 +1,27 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Meta-Keywords" do
+author "Brendan Coles <bcoles@gmail.com>" # 2010-10-20
+version "0.1"
+description "This plugin retrieves the keywords from the meta keywords tag."
+
+examples %w|
+itsecuritysolutions.org
+yehg.net
+www.backtrack-linux.org
+|
+
+matches [
+
+# Get keywords
+{ :version=>/<meta[^>]+name[\s]*=[\s]*[\'|\"]?keywords[^>]+content[\s]*=[\s]*[\'|\"]?([^\'^\"\/^>]+)/i, :version_regexp_offset=>0 },
+{ :version=>/<meta[^>]+content[\s]*=[\s]*[\'|\"]?([^\"^\']+)[^>]+name[\s]*=[\s]*[\'|\"]?keywords/i, :version_regexp_offset=>0 },
+
+]
+
+end
+
