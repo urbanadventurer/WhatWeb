8971f68705549bd739a083dc6c5efb9812849817
bcoles <bcoles@gmail.com>
2010-10-20 19:13:15 +1030

    Updated cgiproxy plugin

--- plugins/cgiproxy.rb
+++ plugins/cgiproxy.rb
@@ -4,10 +4,15 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Updated version detection and matches
+##
 Plugin.define "CGIProxy" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-06
-version "0.1"
+version "0.2"
 description "This CGI script acts as an HTTP or FTP proxy. Through it, you can retrieve any resource that is accessible from the server it runs on. - homepage: http://www.jmarshall.com/tools/cgiproxy/"
+
+# About 15 results for +filetype:cgi "Start browsing through this CGI-based proxy by entering a URL below" @ 2010-06-06
 examples %w|
 forrestspick.com/proxy/newcgi/nph-proxy.cgi
 www.awxcnx.de/cgi-bin/proxy3/nph-proxy.cgi
@@ -18,29 +23,17 @@ webringlink.com/cgi-bin/nph-os.cgi
 
 matches [
 
+# GHDB Match
 # http://johnny.ihackstuff.com/ghdb?function=detail&id=531
-# About 15 results @ 2010-06-06
-{:name=>'GHDB: +filetype:cgi "Start browsing through this CGI-based proxy by entering a URL below"',
-:certainty=>25,
-:ghdb=>'+filetype:cgi "Start browsing through this CGI-based proxy by entering a URL below"'
-},
+{ :ghdb=>'filetype:cgi "Start browsing through this CGI-based proxy by entering a URL below"', :certainty=>75 },
 
-{:name=>"default title",
-:text=>'<title>Start Using CGIProxy</title>'
-}
+# Default title
+{ :text=>'<title>Start Using CGIProxy</title>' },
+
+# Vesion detection
+{ :version=>/<a href="http:\/\/www.jmarshall.com\/tools\/cgiproxy\/"><i>CGIProxy ([^<]+)<\/i><\/a>/, :version_regexp_offset=>0 },
 
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<a href="http:\/\/www.jmarshall.com\/tools\/cgiproxy\/"><i>CGIProxy [a-z0-9_\.]+<\/i><\/a>/
-		v=@body.scan(/<a href="http:\/\/www.jmarshall.com\/tools\/cgiproxy\/"><i>CGIProxy ([a-z0-9_\.]+)<\/i><\/a>/)[0].to_s
-		m << {:name=>"powered by version", :version=>v }
-        end
-        m
-
-end
 
 end
 
