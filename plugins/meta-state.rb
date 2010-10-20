0d8a190cc9b19a19444ef66b34e556e6906bb8a8
bcoles <bcoles@gmail.com>
2010-10-20 20:23:06 +1030

    Added meta-state plugin

---
+++ plugins/meta-state.rb
@@ -0,0 +1,25 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Meta-State" do
+author "Brendan Coles <bcoles@gmail.com>" # 2010-10-20
+version "0.1"
+description "This plugin retrieves the state from the meta state tag. This tag is used by the Gigablast search engine."
+
+examples %w|
+http://www.gigablast.com/tagsdemo.html
+|
+
+matches [
+
+# Extract state
+{ :version=>/<meta[^>]+name[\s]*=[\s]*[\'|\"]?state[^>]+content[\s]*=[\s]*[\'|\"]?([^\'^\"\/^>]+)/i, :version_regexp_offset=>0 },
+{ :version=>/<meta[^>]+content[\s]*=[\s]*[\'|\"]?([^\"^\']+)[^>]+name[\s]*=[\s]*[\'|\"]?state/i, :version_regexp_offset=>0 },
+
+]
+
+end
+
