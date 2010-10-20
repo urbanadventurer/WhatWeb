b611f30c3f4dbb48799bcbd7a68231aa3010eb66
bcoles <bcoles@gmail.com>
2010-10-20 20:55:09 +0800

    Updated zoph plugin

--- plugins/zoph.rb
+++ plugins/zoph.rb
@@ -4,10 +4,14 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Fixed regex
+##
 Plugin.define "Zoph" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-11
-version "0.1"
+version "0.2"
 description "Zoph (Zoph Organizes Photos) is a web based digital image presentation and management system. In other words, a photo album. It is built with PHP, MySQL and Perl. - homepage: http://www.zoph.org/"
+
 # 26 results for inurl:albums.php inurl:parent_album_id @ 2010-07-10
 examples %w|
 www.zoph.org/demo/zoph.php
@@ -26,8 +30,10 @@ number.ch/zoph/zoph.php
 
 matches [
 
+# Default menu HTML
 {:text=>'<li class="selected"><a href="zoph.php">home</a></li><li ><a href="albums.php">albums</a></li><li ><a href="categories.php">categories</a></li><li >' },
 
+# Default title
 {:text=>"<title>Zoph - Home</title>" },
 
 ]
@@ -35,22 +41,25 @@ matches [
 def passive
         m=[]
 
-	# new versions
+        # new versions
         if @body =~ /        <p class="version">/
-                version=@body.scan(/            Zoph ([\d\.a-zA-Z\-]+)/)[0][0]
-                m << {:version=>version}
+                if @body =~ /            Zoph ([\d\.a-zA-Z\-]+)/
+                        version=@body.scan(/            Zoph ([\d\.a-zA-Z\-]+)/)[0][0]
+                        m << {:version=>version}
+                end
         end
 
-	# old versions
+        # old versions
         if @body =~ /          <td colspan="2" align="center">/
-                version=@body.scan(/            <font size="-1">Zoph ([\d\.a-zA-Z\-]+)<\/font>/)[0][0]
-                m << {:version=>version}
+                if @body =~ /            <font size="-1">Zoph ([\d\.a-zA-Z\-]+)<\/font>/
+                        version=@body.scan(/            <font size="-1">Zoph ([\d\.a-zA-Z\-]+)<\/font>/)[0][0]
+                        m << {:version=>version}
+                end
         end
 
         m
 
 end
-
 
 end
 
