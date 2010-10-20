341f1fbb755c1a9a14456bb5f8c97a2a969fa3e3
bcoles <bcoles@gmail.com>
2010-10-20 21:01:36 +0800

    Updated vulnerable-to-xss plugin

--- plugins/vulnerable-to-xss.rb
+++ plugins/vulnerable-to-xss.rb
@@ -4,23 +4,27 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Fixed regex
+##
 Plugin.define "Vulnerable-To-XSS" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-06
-version "0.1"
+version "0.2"
 description "This plugin can be used as a very basic xss scanner. It searches for instances of <script>alert(*)</script> in the HTML source."
 
 def passive
         m=[]
-	result=""
+        result=""
         if @body =~ /<script>([\s]*)(alert\([a-zA-Z0-9\/\'\"]+\))([\s]*[\;]?[\s]*)<\/script>/i
-                version = @body.scan(/<script>([\s]*)(alert\([a-zA-Z0-9\/\'\"]+\))[\s]*[\;]?[\s]*/i) { |match|
-  result << "#{match} "
-}
-                m << {:certainty=>25,:name=>"vulnerable to XSS",:version=>result}
+                version = @body.scan(/<script>([\s]*)(alert\([a-zA-Z0-9\/\'\"]+\))[\s]*[\;]?[\s]*<\/script>/i) { |match|
+                        result << "#{match} "
+                }
+                m << {:version=>result, :certainty=>25}
         end
 
         m
 end
 
 end
+
 
