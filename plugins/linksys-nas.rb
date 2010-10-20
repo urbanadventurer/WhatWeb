45ab0913aabbd2894c80799e1a9c1b0f0e861bca
bcoles <bcoles@gmail.com>
2010-10-20 23:06:51 +1030

    Updated linksys-nas plugin

--- plugins/linksys-nas.rb
+++ plugins/linksys-nas.rb
@@ -4,9 +4,12 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Fixed regex
+##
 Plugin.define "Linksys-NAS" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-15
-version "0.1"
+version "0.2"
 description "Linksys NAS web frontend - homepage: http://www.linksys.com/"
 
 # http://www.hackersforcharity.org/ghdb/?function=detail&id=1669
@@ -18,10 +21,8 @@ prominence.info
 
 matches [
 
-{:text=>'<html><head><title>Ethernet Network Attached Storage  Utility</title>' },
-
-{:text=>'	<td nowrap colspan="3"><img src="Admin_top.JPG" width="750" height="52" alt="" border="0" hspace="0" vspace="0"></td>' },
-
+{ :text=>'<html><head><title>Ethernet Network Attached Storage  Utility</title>' },
+{ :text=>'	<td nowrap colspan="3"><img src="Admin_top.JPG" width="750" height="52" alt="" border="0" hspace="0" vspace="0"></td>' },
 
 ]
 
@@ -29,14 +30,16 @@ def passive
         m=[]
 
         if @body =~ /	 <td nowrap class="hst">&nbsp; &nbsp;Version:<\/td>/
-                version=@body.scan(/	          <td nowrap class="hst">V([\d\.a-zA-Z]+)<\/td>/)[0][0]
-                m << {:version=>version}
+                if @body =~ /	          <td nowrap class="hst">V([\d\.a-zA-Z]+)<\/td>/
+                        version=@body.scan(/	          <td nowrap class="hst">V([\d\.a-zA-Z]+)<\/td>/)[0][0]
+                        m << { :version=>version }
+                end
         end
 
         m
 
 end
 
-
 end
+
 
