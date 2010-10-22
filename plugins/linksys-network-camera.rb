1648f1e7695109eddd8f95d37318dd8521b78ba6
bcoles <bcoles@gmail.com>
2010-10-22 16:51:03 +1030

    Updated linksys-network-camera plugin

--- plugins/linksys-network-camera.rb
+++ plugins/linksys-network-camera.rb
@@ -4,9 +4,12 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Changed :version to :model
+##
 Plugin.define "Linksys-Network-Camera" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-22
-version "0.1"
+version "0.2"
 description "Linksys network camera web interface - homepage: http://www.linksys.com/"
 
 # 14 Google results for inurl:/img/main.cgi inurl:next_file @ 2010-07-22
@@ -18,13 +21,17 @@ examples %w|
 
 matches [
 
-{ :text=>'<meta name="description" content="WVC54GCA">', :version=>"WVC54GCA" },
+# WVC54GCA # Meta description
+{ :text=>'<meta name="description" content="WVC54GCA">', :model=>"WVC54GCA" },
 
-{ :text=>'<td bgcolor="#6766CC" align="right" valign="top"><span class="model" style="position:relative;top:-12px">WVC54GCA</span><img src="../blue_top_right.gif" alt="" border="0">', :version=>"WVC54GCA" },
+# WVC54GCA # Default logo HTML
+{ :text=>'<td bgcolor="#6766CC" align="right" valign="top"><span class="model" style="position:relative;top:-12px">WVC54GCA</span><img src="../blue_top_right.gif" alt="" border="0">', :model=>"WVC54GCA" },
 
-{ :text=>'<meta name="description" content="WVC80N">', :version=>"WVC80N" },
+# WVC80N # Meta description
+{ :text=>'<meta name="description" content="WVC80N">', :model=>"WVC80N" },
 
-{ :text=>'<td bgcolor="#6766CC" align="right" valign="top"><span class="model" style="position:relative;top:-12px">WVC80N</span><img src="../blue_top_right.gif" alt="" border="0">', :version=>"WVC80N" },
+# WVC80N # Default logo HTML
+{ :text=>'<td bgcolor="#6766CC" align="right" valign="top"><span class="model" style="position:relative;top:-12px">WVC80N</span><img src="../blue_top_right.gif" alt="" border="0">', :model=>"WVC80N" },
 
 ]
 
