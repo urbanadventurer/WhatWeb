64527d5a38e0fae8e81e4f7b99818507a9565a44
bcoles <bcoles@gmail.com>
2010-10-22 13:54:37 +0800

    Updated cpanel plugin

--- plugins/cpanel.rb
+++ plugins/cpanel.rb
@@ -4,34 +4,60 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
-
+# Version 0.3 # Brendan Coles <bcoles@gmail.com>
+# Added version detection. Added OS detection.
+##
 # Version 0.2
 # removed :name and :certainty=>100
-
-
+##
 Plugin.define "CPanel" do
 author "Andrew Horton"
-version "0.2"
-description "homepage:www.cpanel.net"
-examples %w| 202.191.40.29 |
+version "0.3"
+description "Site configuration and management software application. Supporting many operating systems while allowing endusers to control every aspect of their webhosting experience. - homepage: http://www.cpanel.net/"
 
-# default apache + cpanel
-
-# <title>cPanel&reg;</title>
-# <div id="footer_images"><img src="sys_cpanel/images/powered_by.gif" />
-# Apache is working on your cPanel<sup>&reg;</sup> and WHM&#8482; Server
-# 
+examples %w|
+demo.cpanel.net
+202.191.40.29
+classyserver.com:2082
+www.pinoakwebhosting.com:2082
+mango.catalyst2.com:2082
+www.bent-palestine.com:2082
+cpanel.il-toys.com
+il-kamasutra.com
+|
 
 matches [
+
+# Welcome page # Default title
 {:text=>"<title>cPanel&reg;</title>" },
-{:text=>'<div id="footer_images"><img src="sys_cpanel/images/powered_by.gif" />' },
-{:text=>"Apache is working on your cPanel<sup>&reg;</sup> and WHM&#8482; Server" }
+
+# Welcome page # Default logo HTML
+{ :text=>'<div id="footer_images"><img src="sys_cpanel/images/powered_by.gif" />' },
+
+# Welcome page # Default text
+{ :text=>"Apache is working on your cPanel<sup>&reg;</sup> and WHM&#8482; Server", :modules=>"Apache" },
+
+# Login page # Default CSS
+{ :regexp=>/<link rel="stylesheet" href="[^>]*\/unprotected\/cpanel\/style_optimized.css" type="text\/css" \/>/ },
+
+# Version detection # Login page # Default title
+{ :version=>/<title>cPanel&reg;[\s]*([\d\.]+)<\/title>/, :version_regexp_offset=>0 },
+
 ]
 
+# Version detection # HTTP Server header
+def passive
+	m=[]
 
+	if @meta["server"].to_s =~ /cpsrvd\/([\d\.]+)/
+		version=@meta["server"].scan(/cpsrvd\/([\d\.]+)/).to_s
+		m << { :version=>version }
+	end
+
+	m
 
 end
 
+end
 
 
