3864fb6899be65fe5d2a54a2ed2d9da2afffff9d
bcoles <bcoles@gmail.com>
2010-10-26 19:46:50 +1030

    Updated phpmyadmin plugin

--- plugins/phpmyadmin.rb
+++ plugins/phpmyadmin.rb
@@ -4,42 +4,75 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 #
+# Updated regex
+# Added version detection
+# Added favicon md5 hash
+# Added cookie matches
+##
 Plugin.define "phpMyAdmin" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-01 
-version "0.1"
+version "0.2"
 description "phpMyAdmin is a free software tool written in PHP intended to handle the administration of MySQL over the World Wide Web. - homepage:http://www.phpmyadmin.net/home_page/index.php"
+
+# About 3,490 results for +intitle:phpMyAdmin +"Language: Afrikaans" +"Welcome to phpMyAdmin"@ 2010-06-01
+# About 1868 ShodanHQ results for pma_theme=original @ 2010-10-26
 examples %w|
-http://demo.phpmyadmin.net/MAINT_2_11_10/
-http://demo.phpmyadmin.net/MAINT_3_3_3/
-http://demo.phpmyadmin.net/QA_3_3/
-http://demo.phpmyadmin.net/TESTING/
-http://demo.phpmyadmin.net/STABLE/
-http://phpmyadmin.hosthat.com/
-http://yohost.com/demo/_/3rdparty/phpMyAdmin/
-http://sst8.com/
-http://nsd2.planet-work.com/
-http://dbmy0028.whservidor.com/
-http://2012.cp.volit.com/
-http://www.mars.abzone.cz/phpmyadmin/
-http://www.pic7.info/
+demo.phpmyadmin.net/MAINT_2_11_11/
+demo.phpmyadmin.net/MAINT_3_3_8/
+demo.phpmyadmin.net/QA_3_3/
+demo.phpmyadmin.net/TESTING/
+demo.phpmyadmin.net/STABLE/
+phpmyadmin.hosthat.com
+yohost.com/demo/_/3rdparty/phpMyAdmin/
+dbmy0028.whservidor.com
+www.mars.abzone.cz/phpmyadmin/
+www.pic7.info
+89.223.109.37
+122.115.32.100
+193.190.66.22
+88.57.182.30
+208.43.32.3
+200.27.93.32
+88.151.101.243
+98.126.210.171
+216.183.235.117
+194.150.224.13
 |
 
 matches [
 
-# About 3,490 results @ 2010-06-01
-{:name=>'GHDB: +intitle:phpMyAdmin +"Language: Afrikaans" +"Welcome to phpMyAdmin"',
-:certainty=>75,
-:ghdb=>'+intitle:phpMyAdmin +"Language: Afrikaans" +"Welcome to phpMyAdmin"'
-},
+# GHDB Match
+{ :certainty=>75, :ghdb=>'+intitle:phpMyAdmin +"Language: Afrikaans" +"Welcome to phpMyAdmin"' },
 
-{:name=>"default title", 
-:regexp=>/<title>phpMyAdmin[0-9a-zA-Z\ \.-]+<\/title>/},
+# Version Detection # Default Title
+{ :version=>/<title>phpMyAdmin ([^\s^<]+)[^<]*<\/title>/, :version_regexp_offset=>0 },
 
-{:name=>"default logo",
-:text=>'<img src="./themes/original/img/logo_right.png" id="imLogo" name="imLogo" alt="phpMyAdmin" border="0" /></a>'
-}
+# Version Detection # Default heading HTML
+{ :version=>/<h2>Welcome to  phpMyAdmin ([^<]+)<\/h2>/, :version_regexp_offset=>0 },
+
+# Default Logo HTML
+{ :text=>'<img src="./themes/original/img/logo_right.png" id="imLogo" name="imLogo" alt="phpMyAdmin" border="0" /></a>' },
+
+# Default logo
+{ :md5=>"d037ef2f629a22ddadcf438e6be7a325", :url=>"favicon.ico" },
+
+# Login page # Default form HTML
+{ :text=>'<form method="post" action="index.php" target="_parent"><input type="hidden" name="phpMyAdmin" value="' },
+{ :text=>'<form method="post" action="index.php" target="_top"><input type="hidden" name="phpMyAdmin" value="' },
 
 ]
+
+# Cookies
+def passive
+	m=[]
+
+	m << {:name=>"phpMyAdmin Cookie" } if @meta["set-cookie"] =~ /phpMyAdmin=[0-9]{32}/
+	m << {:name=>"pma_theme Cookie" } if @meta["set-cookie"] =~ /pma_theme=original/
+
+	m
+
+end
 
 end
 
