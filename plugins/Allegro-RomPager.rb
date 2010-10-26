62f76d90cb7bfbcab4e16b566c50f5617c6288a6
bcoles <bcoles@gmail.com>
2010-10-26 14:54:47 +0800

    Added Allegro-RomPager plugin

---
+++ plugins/Allegro-RomPager.rb
@@ -0,0 +1,54 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Allegro-RomPager" do
+author "Brendan Coles <bcoles@gmail.com>" # 2010-10-26
+version "0.1"
+description "The RomPager suite of products enable your engineering team to leverage proven Web-based technologies in your networked embedded device. - homepage: http://www.allegrosoft.com/rpproduct.html"
+
+# About 2247816 ShodanHQ results for "server: RomPager" @ 2010-10-26
+# About 190212 ShodanHQ results for "server: Allegro-Software-RomPager" @ 2010-10-26
+examples %w|
+74.189.97.193
+68.68.104.145
+203.1.55.183
+152.2.189.5
+67.103.40.123
+63.202.242.22
+118.222.183.51
+99.151.224.78
+24.42.153.177
+96.61.248.100
+71.2.238.96
+92.81.141.159
+69.232.80.182
+183.80.68.157
+110.139.190.183
+110.139.164.11
+99.59.65.251
+125.166.145.144
+123.27.50.94
+125.161.205.182
+|
+
+# HTTP Header
+def passive
+	m=[]
+
+	# Server # RomPager
+	m << { :version=>@meta["server"].to_s.scan(/^[\s]*RomPager\/([^\s^\r^\n]+)/i).to_s } if @meta["server"].to_s =~ /^[\s]*RomPager\/([^\s^\r^\n]+)/i
+	m << { :version=>@meta["Server"].to_s.scan(/^[\s]*RomPager\/([^\s^\r^\n]+)/i).to_s } if @meta["Server"].to_s =~ /^[\s]*RomPager\/([^\s^\r^\n]+)/i
+
+	# Server # Allegro-Software-RomPager
+	m << { :version=>@meta["server"].to_s.scan(/^[\s]*Allegro-Software-RomPager\/([^\s^\r^\n]+)/i).to_s } if @meta["server"].to_s =~ /^[\s]*Allegro-Software-RomPager\/([^\s^\r^\n]+)/i
+	m << { :version=>@meta["Server"].to_s.scan(/^[\s]*Allegro-Software-RomPager\/([^\s^\r^\n]+)/i).to_s } if @meta["Server"].to_s =~ /^[\s]*Allegro-Software-RomPager\/([^\s^\r^\n]+)/i
+
+	m
+
+end
+
+end
+
