308b20564547b059326928340c8d7ba70e8bc9d7
Andrew Horton <horton.nz@gmail.com>
2010-10-28 01:02:19 +1300

    Updated mailto plugin, fixed bugs

--- plugins/mailto.rb
+++ plugins/mailto.rb
@@ -10,18 +10,22 @@
 # removed :certainty, end at an optional ?, return the email address as a :string
 # Version 0.3
 # added \' to regex
+# Version 0.4
+# Updated regex, added > and comments
 
 Plugin.define "Mailto" do
 author "Andrew Horton"
-version "0.2"
-description "email addresses in mailto: links"
+version "0.4"
+description "Extract email addresses from links containing mailto:"
 # identifying strings
 # mailto: email@address
+# A regex for valid email addresses is not included so that anti-spam email addresses can be returned, eg bob at gmail dot com.
 
 def passive
 	m=[]
-	emails=@body.scan(/mailto:([^\'\"\?]+)/)
-	if emails.size > 0
+	emails=@body.scan(/<[^>]+href=[^>]*mailto:([^\'\"\?>]+)[^>]*>/)
+
+	unless emails.empty?
 		match=true
 		emails.flatten!
 		emails.map! {|e| CGI.unescapeHTML(e)  }
@@ -33,7 +37,4 @@ def passive
 end
 
 end
-
-
-
 
