f531d447a036625417e9975785f36c9f630e38e0
Andrew Horton <horton.nz@gmail.com>
2010-12-19 23:31:17 +1300

    Removed match for /incoming/ folder from EscenicEngine5 plugin

--- plugins/EscenicEngine5.rb
+++ plugins/EscenicEngine5.rb
@@ -1,14 +1,13 @@
 Plugin.define "EscenicEngine5" do
 author "nk@dsigned.gr"
+
 version "0.1"
 description "Identifies Escenic Engine CMS v5.x. Homepage: http://www.vizrt.com/products/escenic_content_engine/"
 examples %w| thetimes.co.uk |
 
-
 matches [
 {:name=>"Contains Escenic Engine image urls", :regexp=>/.*(\.ece).*(ALTERNATES|BINARY).*(w\d+)/,:certainty=>85 },
 {:name=>"Contains default .ece urls", :regexp=>/.*href=.*(\.ece).*/,:certainty=>75 },
-{:name=>"Builtin section exists", :url=>'/incoming/',:certainty=>50 }
 ]
 
 end
