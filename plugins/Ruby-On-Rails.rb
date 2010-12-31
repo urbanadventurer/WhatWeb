e0b5d06fb4178fca933468cb5b936c9247430f97
Andrew Horton <horton.nz@gmail.com>
2010-12-31 15:41:41 +1300

    Added license headers to plugins

--- plugins/Ruby-On-Rails.rb
+++ plugins/Ruby-On-Rails.rb
@@ -1,3 +1,9 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
 Plugin.define "Ruby-on-Rails" do
 author "Andrew Horton"
 version "0.1"
