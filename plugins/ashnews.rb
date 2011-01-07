2a13a3d2f8e9359ed8ec6feceb233e717984f9bd
bcoles <bcoles@gmail.com>
2011-01-07 17:32:49 +1030

    Updated ashnews plugin

--- plugins/ashnews.rb
+++ plugins/ashnews.rb
@@ -4,33 +4,31 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-07 #
+# Updated version detection method
+##
 Plugin.define "ashnews" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-28
-version "0.1"
+version "0.2"
 description "ashnews is a simple program using PHP/MySQL that allows you to easily add a news/blog system to your site. - homepage: http://projects.ashwebstudio.com/"
 
 # 19 results for "News powered by ashNews" -allinurl -Vulnerabilities @ 2010-08-28
+
+# Examples #
 examples %w|
 www.onecrazyfan.com/reeze.php
 axoft.com.ar/wwwroot/
 www.duelunelive.net/ashnews.php
-www.callac.com/Chris/blog.php
 www.turismoaventura.com
 |
 
-# Version detection using powered by text
-def passive
-        m=[]
+# Matches #
+matches [
 
-        if @body =~ /    <td align="center"><font face=Arial size=1>News powered by <a href=http:\/\/projects.ashwebstudio.com>ashnews v[\d\.]+<\/a><\/font><\/td>/
-                version=@body.scan(/    <td align="center"><font face=Arial size=1>News powered by <a href=http:\/\/projects.ashwebstudio.com>ashnews v([\d\.]+)<\/a><\/font><\/td>/)[0][0]
-                m << {:version=>version}
-        end
+	# Version Detection # powered by text
+	{ :version=>/<td align="center"><font face=Arial size=1>News powered by <a href=http:\/\/projects.ashwebstudio.com>ashnews v([\d\.]+)<\/a><\/font><\/td>/, :regexp_offset=>0 },
 
-        m
-
-end
-
+]
 
 end
 
