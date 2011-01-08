f0d65f2c3fddfe44b3a6ce880884f4c8a167eb3c
bcoles <bcoles@gmail.com>
2011-01-08 11:35:14 +0800

    Updated BXR plugin

--- plugins/bxr.rb
+++ plugins/bxr.rb
@@ -4,34 +4,31 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-08 #
+# Updated HTTP Server Header match
+##
 Plugin.define "BXR" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-06
-version "0.1"
+version "0.2"
 description "BXR is an interface to manage a Blosxom-Based blog through the web or XML-RPC. Current it supports blogger, and metaWeblog APIs. - homepage: http://sourceforge.net/projects/bxr/"
 
 # 1 result for "BXR Web Login" intitle:"Please Enter Password" @ 2010-08-06
+
+# Examples #
 examples %w|
 r.ookee.com/cgi-bin/rookee-xmlrpc.cgi
 |
 
+# Matches
 matches [
 
-{ :text=>'<meta name="generator" content="BXR::Web" />' },
+	# Meta Generator
+	{ :text=>'<meta name="generator" content="BXR::Web" />' },
+
+	# Version Detection # Powered by text
+	{ :version=>/<div class="bxrlogo">Powered by <a href="http:\/\/sourceforge.net\/projects\/bxr\/">BXR<\/a> ([\d\.]+)<\/div>/, :regexp_offset=>0 },
 
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<div class="bxrlogo">Powered by <a href="http:\/\/sourceforge.net\/projects\/bxr\/">BXR<\/a> [\d\.]+<\/div>/
-                version=@body.scan(/<div class="bxrlogo">Powered by <a href="http:\/\/sourceforge.net\/projects\/bxr\/">BXR<\/a> ([\d\.]+)<\/div>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
