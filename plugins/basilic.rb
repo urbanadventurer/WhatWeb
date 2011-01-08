f29f82b0f36f54902a5cd0462ebc7d7498e3d2f2
bcoles <bcoles@gmail.com>
2011-01-08 12:17:10 +0800

    Updated basilic plugin

--- plugins/basilic.rb
+++ plugins/basilic.rb
@@ -4,12 +4,17 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-08 #
+# Updated version detection
+##
 Plugin.define "Basilic" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-14
-version "0.1"
+version "0.2"
 description "Basilic is a bibliography server for research laboratories. It automates and facilitates the diffusion of research publications over the internet, automatically generating web pages from a publication database. - homepage: http://artis.imag.fr/Software/Basilic"
 
 # 14 results for "powered by basilic" @ 2010-08-14
+
+# Examples #
 examples %w|
 www.lifl.fr/GRAPHIX/publications.php
 secure.ntsg.umt.edu/publications/
@@ -17,31 +22,18 @@ www.liaad.up.pt/pub/
 www.inis.iarc.uaf.edu/publications/
 |
 
+# Matches #
 matches [
 
-{ :text=>"Powered by <a href='http://artis.imag.fr/Software/Basilic/'>Basilic</a></p>" },
-{ :text=>"<a href='http://artis.imag.fr/Software/Basilic'>Powered by Basilic</a></span>" },
+	# Powered by text
+	{ :text=>"Powered by <a href='http://artis.imag.fr/Software/Basilic/'>Basilic</a></p>" },
+	{ :text=>"<a href='http://artis.imag.fr/Software/Basilic'>Powered by Basilic</a></span>" },
+
+	# Version Detection # Powered by footer text
+	{ :version=>/<span>Powered by <a href='http:\/\/artis.imag.fr\/Software\/Basilic'>Basilic ([\d\.]+)<\/a><\/span>/, :regexp_offset=>0 },
+	{ :version=>/<a href='http:\/\/artis.imag.fr\/Software\/Basilic'[^>]+>&copy; Powered by Basilic ([\d\.]+)/, :regexp_offset=>0 },
 
 ]
-
-# Version detection using powered by footer text
-def passive
-        m=[]
-
-        if @body =~ /  <span>Powered by <a href='http:\/\/artis.imag.fr\/Software\/Basilic'>Basilic [\d\.]+<\/a><\/span>/
-                version=@body.scan(/  <span>Powered by <a href='http:\/\/artis.imag.fr\/Software\/Basilic'>Basilic ([\d\.]+)<\/a><\/span>/)[0][0]
-                m << {:version=>version}
-        end
-
-	if @body =~ /<a href='http:\/\/artis.imag.fr\/Software\/Basilic'[^>]+>&copy; Powered by Basilic [\d\.]+/
-		version=@body.scan(/<a href='http:\/\/artis.imag.fr\/Software\/Basilic'[^>]+>&copy; Powered by Basilic ([\d\.]+)/)[0][0]
-		m << {:version=>version}
-	end
-
-        m
-
-end
-
 
 end
 
