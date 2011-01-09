06a28a93535da854bcee36482d6f1a79dce61428
bcoles <bcoles@gmail.com>
2011-01-09 17:16:31 +0800

    Updated censura plugin

--- plugins/censura.rb
+++ plugins/censura.rb
@@ -4,20 +4,23 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-09 #
+# Updated version detection
+##
 Plugin.define "Censura" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-01
-version "0.1"
+version "0.2"
 description "Censura - the flexible, robust, reviews and ratings application. - homepage: http://www.censura.info/"
 
 # 70 results for "Powered by: Censura" @ 2010-08-01
+
+# Examples #
 examples %w|
 www.censurademo.com
 www.astronomyforbeginners.com/reviews/
 www.dwuog.com/reviews/
 omahasrestaurants.com
 www.n0hr.com/HamRadioReviews/
-www.censuradevelopment.com
-supplementdigest.com
 usa-hotelguide.com
 americacoasters.com/database/
 www.coindealerratings.com/reviews/
@@ -35,20 +38,15 @@ www.archerytalkreviews.com/reviews/
 www.bikesuperhub.com/reviews/
 |
 
-# The "brand removal" option is a one-off fee per license. There is a link within your client area to allow you to purchase a "brand removal" option.
-# http://www.censura.info/support/kb/article/000001
-def passive
-        m=[]
+# Matches #
+matches [
 
-        if @body =~ /Powered by: <a[\ target="_blank"]* href="http:\/\/www.censura.info">Censura<\/a> version [\d\.]+/
-                version=@body.scan(/Powered by: <a[\ target="_blank"]* href="http:\/\/www.censura.info">Censura<\/a> version ([\d\.]+)/)[0][0]
-                m << {:version=>version}
-        end
+	# Version Detection # Powered by footer
+	# The "brand removal" option is a one-off fee per license. There is a link within your client area to allow you to purchase a "brand removal" option.
+	# http://www.censura.info/support/kb/article/000001
+	{ :version=>/Powered by: <a[^>]* href="http:\/\/www.censura.info">Censura<\/a> version ([\d\.]+)/, :regexp_offset=>0 },
 
-        m
-
-end
-
+]
 
 end
 
