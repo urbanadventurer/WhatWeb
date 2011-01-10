a33663fb1b9f7367eae343946e841965c4a1a0b5
bcoles <bcoles@gmail.com>
2011-01-10 14:12:47 +0800

    Updated CMS-WebManager-Pro plugin

--- plugins/CMS-WebManager-Pro.rb
+++ plugins/CMS-WebManager-Pro.rb
@@ -4,37 +4,31 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-10 #
+# Updated version detection
+##
 Plugin.define "CMS-WebManager-Pro" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-09-04
-version "0.1"
+version "0.2"
 description "CMS WebManager-Pro - is a software product for creating and maintaining websites of any complexity. - homepage: http://webmanager-pro.com/"
 
 # 2 results for "powered by CMS WebManager-Pro" @ 2010-09-04
+
+# Examples #
 examples %w|
 www.21.com.ua
-test21vek.solnce.com.ua
 |
 
+# Matches #
 matches [
 
-# Powered by text
-{ :text=>'Powered by <a style="color:#555555;" href="http://webmanager-pro.com/">CMS Webmanager-pro</a></li>' },
+	# Powered by text
+	{ :text=>'Powered by <a style="color:#555555;" href="http://webmanager-pro.com/">CMS Webmanager-pro</a>' },
+
+	# Version detection # Meta generator
+	{ :version=>/<meta name="generator" content="Webmanager-pro v([\d\.]+)" \/>/, :regexp_offset=>0 },
 
 ]
-
-# Version detection using meta generator
-def passive
-        m=[]
-
-        if @body =~ /<meta name="generator" content="Webmanager-pro v([\d\.]+)" \/>/
-                version=@body.scan(/<meta name="generator" content="Webmanager-pro v([\d\.]+)" \/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
