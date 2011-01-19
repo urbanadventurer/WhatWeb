b7705181e0d3802e53dd4a8d35b4932647ffdfcd
bcoles <bcoles@gmail.com>
2011-01-19 16:25:07 +1030

    Updated PHP plugin

--- plugins/PHP.rb
+++ plugins/PHP.rb
@@ -4,13 +4,18 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-19 #
+# Updated regex matches
+##
 Plugin.define "PHP" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-26
-version "0.1"
+version "0.2"
 description "PHP is a widely-used general-purpose scripting language that is especially suited for Web development and can be embedded into HTML. - homepage: http://www.php.net/"
 
-# About 3838895 ShodanHQ results for "server: PHP" @ 2010-10-26
-# About 2343143 ShodanHQ results for "X-Powered-By: PHP" @ 2010-10-26
+# About 3,838,895 ShodanHQ results for "server: PHP" @ 2010-10-26
+# About 2,343,143 ShodanHQ results for "X-Powered-By: PHP" @ 2010-10-26
+
+# Examples #
 examples %w|
 129.217.108.130
 202.235.202.65
@@ -22,29 +27,25 @@ examples %w|
 67.222.45.85
 129.121.70.1
 96.46.184.46
-109.201.57.179
 81.169.173.100
 209.205.127.135
 77.104.173.253
 96.0.245.3
 95.187.225.232
 195.42.102.207
-109.177.86.84
 116.58.194.4
 64.130.117.21
 |
 
-# HTTP Header
+# Passive #
 def passive
 	m=[]
 
-	# Server
+	# HTTP # Server
 	m << { :version=>@meta["server"].to_s.scan(/[^\r^\n]*PHP\/([^\s^\r^\n]+)/i).to_s } if @meta["server"].to_s =~ /[^\r^\n]*PHP\/([^\s^\r^\n]+)/i
-	m << { :version=>@meta["Server"].to_s.scan(/[^\r^\n]*PHP\/([^\s^\r^\n]+)/i).to_s } if @meta["Server"].to_s =~ /[^\r^\n]*PHP\/([^\s^\r^\n]+)/i
 
-	# X-Powered-By
+	# HTTP # X-Powered-By
 	m << { :version=>@meta["x-powered-by"].to_s.scan(/[^\r^\n]*PHP\/([^\s^\r^\n]+)/i).to_s } if @meta["x-powered-by"].to_s =~ /[^\r^\n]*PHP\/([^\s^\r^\n]+)/i
-	m << { :version=>@meta["X-Powered-By"].to_s.scan(/[^\r^\n]*PHP\/([^\s^\r^\n]+)/i).to_s } if @meta["X-Powered-By"].to_s =~ /[^\r^\n]*PHP\/([^\s^\r^\n]+)/i
 
 	m
 
