8c0ecc1201810bf5b86a2395a849157a087c963c
bcoles <bcoles@gmail.com>
2011-01-25 02:00:13 +1030

    Updated eSyndiCat plugin

--- plugins/eSyndiCat.rb
+++ plugins/eSyndiCat.rb
@@ -4,12 +4,17 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-25 #
+# Updated version detection
+##
 Plugin.define "eSyndiCat" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-08
-version "0.1"
+version "0.2"
 description "eSyndiCat is a professional, affordable directory software solution. - homepage: http://www.esyndicat.com/"
 
 # 8 results for "Powered by eSyndiCat Web Directory" @ 2010-08-08
+
+# Examples #
 examples %w|
 www.esyndicat.com/demo/
 www.esyndicat.com/demo/admin/
@@ -17,42 +22,26 @@ omega.esyndicat.com
 omega.esyndicat.com/admin/
 www.pornblogsdir.com
 www.spiritueleforum.nl/links/
-www.link2porno.net/pornolink/
 www.seksdatabase.nl
 |
 
+# Matches #
 matches [
 
-{ :regexp=>/<div class="copyright">&copy; [0-9]{4}[\-0-9]* Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat[\ Web]* Directory [Script|Software]+<\/a><\/div>/ },
+	# Copyright Footer
+	{ :regexp=>/<div class="copyright">&copy; [0-9]{4}[\-0-9]{0,5} Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat[^<]{0,5} Directory [^<]{5,8}<\/a><\/div>/ },
 
-# Admin Panel
-{ :text=>'	<title>Login To eSyndiCat Admin Panel</title>' },
+	# Admin Panel
+	{ :text=>'<title>Login To eSyndiCat Admin Panel</title>' },
+
+	# Version Detection # Meta Generator
+	{ :version=>/<meta name="generator" content="eSyndiCat Web Directory Software ([\d\.]+)" \/>/, :regexp_offset=>0 },
+
+	# Version Detection # Powered by text
+	{ :version=>/Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat Pro v([\d\.]+)<\/a><br \/>/, :regexp_offset=>0 },
+	{ :version=>/Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat<\/a> Version ([\d\.]+)<br \/>/, :regexp_offset=>0 },
 
 ]
-
-# Version detection with meta generator and admin panel copyright footer text
-def passive
-        m=[]
-
-        if @body =~ /	<meta name="generator" content="eSyndiCat Web Directory Software [\d\.]+" \/>/
-                version=@body.scan(/	<meta name="generator" content="eSyndiCat Web Directory Software ([\d\.]+)" \/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        if @body =~ /			Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat Pro v[\d\.]+<\/a><br \/>/
-                version=@body.scan(/			Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat Pro v([\d\.]+)<\/a><br \/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        if @body =~ /	Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat<\/a> Version [\d\.]+<br \/>/
-                version=@body.scan(/	Powered by <a href="http:\/\/www.esyndicat.com\/">eSyndiCat<\/a> Version ([\d\.]+)<br \/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
