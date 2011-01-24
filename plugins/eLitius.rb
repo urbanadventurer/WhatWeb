c9de1ac20ed2555e31a2468fedbe166e130705c5
bcoles <bcoles@gmail.com>
2011-01-24 23:19:07 +0800

    Updated eLitius plugin

--- plugins/eLitius.rb
+++ plugins/eLitius.rb
@@ -4,37 +4,33 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-01-25 #
+# Updated version detection
+##
 Plugin.define "eLitius" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-25
-version "0.1"
+version "0.2"
 description "eLitius is a free software package that allows a person to easily create and manage his own Affiliate Program, recruit affiliates, track visitors and sales. - homepage: http://www.elitius.com/"
 
 # 21 results for "Powered by eLitius Version" -exploit -vulnerabilities
+
+# Examples #
 examples %w|
 www.elitius.com/demo/
-www.kernbits.com/affiliate/
 secure.iicinternet.com/affilate/
 www.scherrer.tv/eLitius/
 |
 
+# Matches #
 matches [
 
-{ :text=>'	<meta name="description" content="eLitius Affiliate Management Software" />' },
+	# Meta Description
+	{ :text=>'<meta name="description" content="eLitius Affiliate Management Software" />' },
+
+	# Version Detection # Powered by text
+	{ :version=>/[\s]+Powered by <a href="http:\/\/www.elitius.com\/" target="_blank" title="Affiliate [^>]{8,10} Software">eLitius<\/a> Version ([\d\.]+)<br \/>/, :regexp_offset=>0 },
 
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /[\s]+Powered by <a href="http:\/\/www.elitius.com\/" target="_blank" title="Affiliate [Tracking|Management]* Software">eLitius<\/a> Version [\d\.]+<br \/>/
-                version=@body.scan(/[\s]+Powered by <a href="http:\/\/www.elitius.com\/" target="_blank" title="Affiliate [Tracking|Management]* Software">eLitius<\/a> Version ([\d\.]+)<br \/>/)[0][0]
-                m << {:version=>version}
-        end
- 
-	m
-
-end
-
 
 end
 
