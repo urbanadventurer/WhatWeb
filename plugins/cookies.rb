715a31b67c63f0967681eff7ede54e0c4a5aa62a
bcoles <bcoles@gmail.com>
2011-01-30 20:10:24 +1030

    Updated cookies plugin

--- plugins/cookies.rb
+++ plugins/cookies.rb
@@ -4,25 +4,29 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
-
+# Version 0.3 # 2011-01-30 # Brendan Coles <bcoles@gmail.com>
+# Fixed regex bug # Cookies were always split by "=" even when it wasn't present
+##
 # Version 0.2
 # removed :certainty=>100
-
+##
 Plugin.define "Cookies" do
 author "Andrew Horton"
-version "0.1"
-description "Display the names of cookies in the HTTP headers. I have chosen not to record cookie values to save on space."
+version "0.3"
+description "Display the names of cookies in the HTTP headers. The values are not returned to save on space."
 
+# Passive #
 def passive
 	m=[]
-	found=[]
-	
+
+	# Extract cookie names #
 	unless @cookies.nil? or @cookies.empty?
-		found=@cookies.map {|x| x.split("=")[0] }.sort.uniq		
-		m << {:name=>"cookie names", :string=>found}
+		@cookies.each do |cookie|
+			m << { :string=>cookie.split("=")[0] } if cookie =~ /=/
+		end
 	end
 
+	# Return passive match
 	m
 end
 
