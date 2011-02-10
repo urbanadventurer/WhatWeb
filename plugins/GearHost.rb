2df7f9db792b9b50e83817e8b48e678aea7a9096
bcoles <bcoles@gmail.com>
2011-02-10 19:00:26 +0800

    Updated GearHost plugin

--- plugins/GearHost.rb
+++ plugins/GearHost.rb
@@ -4,12 +4,18 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-10 #
+# Updated regex match
+##
 Plugin.define "GearHost" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-25
-version "0.1"
+version "0.2"
 description "GearHost is a Microsoft Gold Certified Windows Hosting Partner that specializes in complex hosted managed servers, CloudServers and shared Windows Web Hosting Solutions for ASP.NET, ColdFusion, Exchange Business Email and other technologies. - Homepage: http://www.gearhost.com/"
 
-# 1369 ShodanHQ results for Hosted-With: GearHost Inc. (www.gearhost.com)
+# ShodanHQ results as at 2010-10-25 #
+# 1,369 for Hosted-With: GearHost Inc. (www.gearhost.com)
+
+# Examples #
 examples %w|
 69.24.78.59
 69.24.78.66
@@ -33,11 +39,11 @@ examples %w|
 def passive
 	m=[]
 
-	m << { :version=>"Windows", :module=>"ASP.NET, ColdFusion, PHP, Perl" } if @meta["Hosted-With"] =~ /[\s]*GearHost Inc. \(www.gearhost.com\)/
-	m << { :version=>"Windows", :module=>"ASP.NET, ColdFusion, PHP, Perl" } if @meta["hosted-with"] =~ /[\s]*GearHost Inc. \(www.gearhost.com\)/
+	# Hosted-With HTTP Header
+	m << { :string=>"OS:Windows", :module=>"ASP.NET, ColdFusion, PHP, Perl" } if @meta["hosted-with"] =~ /[\s]*GearHost Inc\. \(www.gearhost.com\)/
 
+	# Return Passive Matches
 	m
-
 end
 
 end
