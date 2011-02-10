fbe76c0f145b6788906452cdc3a2581b80086e7e
bcoles <bcoles@gmail.com>
2011-02-10 18:54:57 +0800

    Updated GeekLog plugin

--- plugins/GeekLog.rb
+++ plugins/GeekLog.rb
@@ -4,12 +4,17 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-10 #
+# Updated version detection
+##
 Plugin.define "GeekLog" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-09-25
-version "0.1"
+version "0.2"
 description "Geeklog is an open source application for managing dynamic web content. It is written in PHP and supports MySQL, PostgreSQL, or MS SQL as the database backend. - homepage: http://www.geeklog.net/"
 
 # 352 results for "powered by GeekLog" @ 2010-09-25
+
+# Examples #
 examples %w|
 www.geeklog.net
 news.infoshop.org
@@ -21,42 +26,23 @@ www.panama-guide.com
 www.webhostingsitesreviews.com
 www.roadshow.ubc.ca/links/index.php
 bigforex.com
-www.faithingiving.com
-www.serve2help.com
-www.harnessu.com
-www.hope2serve.com
-www.faithinserving.com
-www.faithtohope.com
 www.abend.org
-www.j-soho.or.jp
 |
 
+# Matches #
 matches [
 
 # Powered by text
 { :text=>'Powered By <a href="http://www.geeklog.net/">Geeklog</a>' },
 { :text=>'Powered By <a class="footer" href="http://www.geeklog.net/">GeekLog</a>' },
-{ :text=>'Powered by&nbsp;<a href="http://www.geeklog.net/">Geeklog</a>' },
 
 # HTML Comment
 { :text=>'<!--If you want the splash, uncomment the last line. If you want no splash, make sure it is commented out.-->' },
 
+# Version Detection # Powered by text
+{ :version=>/Powered by <a href="http:\/\/www.geeklog.net\/">Geeklog ([\d\.a-z]{1,10})<\/a>/, :regexp_offset=>0 },
+
 ]
-
-# Version detection
-def passive
-        m=[]
-
-	# Powered by text
-        if @body =~ /Powered by <a href="http:\/\/www.geeklog.net\/">Geeklog ([\d\.]+)<\/a>/
-                version=@body.scan(/Powered by <a href="http:\/\/www.geeklog.net\/">Geeklog ([\d\.]+)<\/a>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
