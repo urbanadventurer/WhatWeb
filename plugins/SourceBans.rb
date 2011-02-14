297368b4cfc08636c4c5ad43b15e236754fac32d
bcoles <bcoles@gmail.com>
2011-02-14 18:13:44 +1030

    Added SourceBans plugin

---
+++ plugins/SourceBans.rb
@@ -0,0 +1,44 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "SourceBans" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-02-14
+version "0.1"
+description "SourceBans is a free global administration and banning system for Source engine based servers. - Homepage: http://www.sourcebans.net/"
+
+# Google results as at 2011-02-14 #
+# 10 for inurl:exportbans.php
+# 14 for inurl:web_upload ext:php +steam
+
+# Examples #
+examples %w|
+aldarion.bgmeet.com/sourcebans/web_upload/
+c7clan.net/sourcebans/
+apeha.comli.com/web_upload/index.php
+banidos.actioncss.com.br
+gc01317.gc-server.de/web_upload/
+tracker.comuv.com
+ventilaator.planet.ee/web_upload/
+bans.gp-emerica.ru
+|
+
+# Matches #
+matches [
+
+# Version Detection # Powered by footer HTML # English
+{ :version=>/		<a href="http:\/\/www.sourcebans.net" target="_blank"><img src="images\/sb.png" alt="SourceBans" border="0" \/><\/a><br\/>\n		<div id="footqversion">Version ([\d\.]+) <\/div>/, :regexp_offset=>0 },
+
+# Default Logo HTML
+{ :text=>'		<a href="http://www.sourcebans.net" target="_blank"><img src="images/sb.png" alt="SourceBans" border="0" /></a><br/>' },
+
+# Error Message
+{ :regexp=>/<div class='dbg' id='[0-9a-f]{7}'><span style='font-weight:bold'>Table '[a-z]+\.sb_settings' doesn't exist<\/span> <br \/><br \/><span>SQL Query type: <\/span><span style='font-weight:bold'>EXECUTE<\/span><span><br \/><span>/ },
+
+]
+
+end
+
+
