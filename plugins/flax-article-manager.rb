71904bca5f18ad60ee067d837d3c3eb2c8b2253c
bcoles <bcoles@gmail.com>
2011-02-17 03:00:39 +0800

    Updated flax-article-manager plugin

--- plugins/flax-article-manager.rb
+++ plugins/flax-article-manager.rb
@@ -4,11 +4,15 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-17 #
+# Updated version detection
+##
 Plugin.define "Flax-Article-Manager" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-08
-version "0.1"
+version "0.2"
 description "Managing large dynamic websites has always been a difficult task for several webmasters, we have developed Flax Article Manager keeping you in mind. By using this system one can easily manage their news and/or articles quite easily. - homepage: http://www.flaxweb.com/products/articles"
 
+# Examples #
 examples %w|
 www.articlesitedemo.com
 www.articlesitedemo.com/admin/admin.php
@@ -18,34 +22,27 @@ www.findanexpertonline.com/FlaxArticlesManager/
 www.findanexpertonline.com/FlaxArticlesManager/admin/admin.php
 |
 
+# Matches #
 matches [
 
+# Default image HTML
 { :text=>'/images/flaxweb_newsletter_bg.gif" bgcolor="#FFFFFF" valign="top"> ' },
-{ :text =>'              <b><a href="http://www.flaxweb.com/products/articles"><font color="#666666">Article ' },
+
+# Default Article link HTML
+{ :text=>'<b><a href="http://www.flaxweb.com/products/articles"><font color="#666666">Article ' },
 
 # Admin Panel
 { :text=>'<title>Flax Article CMS Administration Panel</title>' },
-{ :text=>'              recent updates for Flax Article Manager</font></b></a></div>' },
-{ :text=>'          <td width=67%><font color=#FFFFFF><a href=admin.php class=htext>Flax ' },
+{ :text=>'recent updates for Flax Article Manager</font></b></a></div>' },
+{ :text=>'<td width=67%><font color=#FFFFFF><a href=admin.php class=htext>Flax ' },
 
-# 403 error text (/admin/ directory, for example)
-{ :text=>'          <td><a href="http://www.flaxweb.com/products/articles">For more details about Flax article manager (CMS) please click ' },
+# 403 Error text (/admin/ directory, for example)
+{ :text=>'<td><a href="http://www.flaxweb.com/products/articles">For more details about Flax article manager (CMS) please click ' },
+
+# Version Detection # Copyright footer text
+{ :version=>/[\s]+Copyright [0-9]{4} &copy; Flax Article Manager v([\d\.]+)<\/div>/, :regexp_offset=>0 },
 
 ]
-
-# Version detection with copyright footer text
-def passive
-        m=[]
-
-        if @body =~ /              Copyright [0-9]{4} &copy; Flax Article Manager v[\d\.]+<\/div>/
-                version=@body.scan(/              Copyright [0-9]{4} &copy; Flax Article Manager v([\d\.]+)<\/div>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
