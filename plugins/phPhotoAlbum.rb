e663deeaf60f0ab242dca6092f344a88c0a437da
bcoles <bcoles@gmail.com>
2011-02-21 12:56:06 +1030

    Updated phPhotoAlbum plugin

--- plugins/phPhotoAlbum.rb
+++ plugins/phPhotoAlbum.rb
@@ -4,37 +4,34 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-21 #
+# Updated version detection
+##
 Plugin.define "phPhotoAlbum" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-14
-version "0.1"
+version "0.2"
 description "PHP Photo Album - homepage: http://www.phphq.net/?script=phPhotoAlbum"
 
-# 4 results for "powered by PHPhotoalbum" intitle:My Picture Album" @ 2010-08-14
+# Google results as at 2010-08-14 #
+# 4 for "powered by PHPhotoalbum" intitle:My Picture Album"
+
+# Examples #
 examples %w|
 www.hardmaps.com/BOTD/BOTD/phPhotoAlbum.php
 www.keshershalosh.com/photos/album.php
 oldstyle.3x.ro/Album/index.php
 |
 
+# Matches #
 matches [
 
+# Default Title
 { :text=>'<title>My Picture Album</title>', :certainty=>75 },
 
+# Version Detection # powered by footer text
+{ :version=>/<td align="right"><font style="font-face:Verdana; font-size:9; font-color:#000000;">Powered By: <a href="http:\/\/www.phphq.net\/\?script=phPhotoAlbum" target="_blank"><b>phPhotoAlbum v([\d\.]+)<\/b><\/a><\/font><\/td>/, :regexp_offset=>0 },
+        
 ]
-
-# Version detection using powered by footer text
-def passive
-        m=[]
-
-        if @body =~ /<td align="right"><font style="font-face:Verdana; font-size:9; font-color:#000000;">Powered By: <a href="http:\/\/www.phphq.net\/\?script=phPhotoAlbum" target="_blank"><b>phPhotoAlbum v[\d\.]+<\/b><\/a><\/font><\/td>/
-                version=@body.scan(/<td align="right"><font style="font-face:Verdana; font-size:9; font-color:#000000;">Powered By: <a href="http:\/\/www.phphq.net\/\?script=phPhotoAlbum" target="_blank"><b>phPhotoAlbum v([\d\.]+)<\/b><\/a><\/font><\/td>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
