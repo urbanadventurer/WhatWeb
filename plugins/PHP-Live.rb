be23d3b8184c6bafdf6b5dd923239518df9502da
bcoles <bcoles@gmail.com>
2011-02-24 21:57:46 +1030

    Updated PHP-Live plugin

--- plugins/PHP-Live.rb
+++ plugins/PHP-Live.rb
@@ -4,14 +4,20 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-24 #
+# Updated version detection
+##
 Plugin.define "PHP-Live" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-09-03
-version "0.1"
+version "0.2"
 description "PHP Live! enables live help and live customer support communication directly from your website. - homepage: http://www.phplivesupport.com/"
 
-# 158 results "powered by PHP Live!" -Vulnerability @ 2010-08-31
-# 7 results for "powered by PHP Live!" intitle:"Knowledge BASE (FAQ)" inurl:knowledge_search.php @ 2010-08-31
-# 14 results for "powered by PHP Live!" (intitle:"Request Live! Support"|"Please leave a message") inurl:request_email.php @ 2010-08-31
+# Google results as at 2010-08-31 #
+# 158 for "powered by PHP Live!" -Vulnerability
+# 7   for "powered by PHP Live!" intitle:"Knowledge BASE (FAQ)" inurl:knowledge_search.php
+# 14  for "powered by PHP Live!" (intitle:"Request Live! Support"|"Please leave a message") inurl:request_email.php
+
+# Examples #
 examples %w|
 74.208.90.54:400/rep.ipmanage.net/httpdocs/phplive/admin/traffic/knowledge_search.php
 jswcrm.com/livechat/request_email.php
@@ -40,38 +46,28 @@ www.hotelangelshome.com/support/request_email.php
 www.robotsetup.com/support/request_email.php
 |
 
+# Matches #
 matches [
 
 # Javascript
 { :text=>'// image is NOT CACHED (Netscape problem).  keep this or bad things could happen' },
 
-# HTML comment
+# HTML Comments
 { :text=>'<!-- copyright OSI Codes Inc. http://www.osicodes.com [DO NOT DELETE] -->' },
 { :text=>'<!-- copyright OSI Codes, http://www.osicodes.com [DO NOT DELETE] -->' },
 { :text=>'<!-- BEGIN PHP Live! Code, copyright 2001 OSI Codes -->' },
 { :text=>'<!-- END PHP Live! Code, copyright 2001 OSI Codes -->' },
 
-# FAQ title
+# FAQ # Default Title
 { :text=>'<title> Knowledge BASE (FAQ) </title>' },
 
 # Error page
 { :text=>'<font color="#FF0000">[Configuration Error: config files not found!] Exiting' },
 
+# Version Detection # Powered by text
+{ :version=>/	<div id="copyright">Powered by <a href='http:\/\/www.phplivesupport.com\/\?link' target='newwin'>PHP Live\!<\/a>  v([\d\.]+) &copy; OSI Codes Inc.<\/div>/, :regexp_offset=>0 },
+
 ]
-
-# Version detection using powered by text
-def passive
-        m=[]
-
-        if @body =~ /	<div id="copyright">Powered by <a href='http:\/\/www.phplivesupport.com\/\?link' target='newwin'>PHP Live\!<\/a>  v([\d\.]+) &copy; OSI Codes Inc.<\/div>/
-                version=@body.scan(/	<div id="copyright">Powered by <a href='http:\/\/www.phplivesupport.com\/\?link' target='newwin'>PHP Live\!<\/a>  v([\d\.]+) &copy; OSI Codes Inc.<\/div>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
