79da2e1492b17b6a015bbb7c7e164889591792f7
bcoles <bcoles@gmail.com>
2011-02-25 16:02:20 +1030

    Updated linksys-usb-hdd plugin

--- plugins/linksys-usb-hdd.rb
+++ plugins/linksys-usb-hdd.rb
@@ -4,16 +4,22 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.3 # 2011-02-25 #
+# Updated version detection
+##
 # Version 0.2 #
 # Updated firmware version detection.
 # Added model detection.
 ##
 Plugin.define "Linksys-USB-HDD" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-27
-version "0.2"
+version "0.3"
 description "Linksys external USB HDD web frontend - homepage: http://www.linksys.com/"
 
-# 7 results for intitle:"Network Storage Link for USB 2.0 Disks" Firmware @ 2010-06-27
+# Google results as at 2010-06-27 #
+# 7 for intitle:"Network Storage Link for USB 2.0 Disks" Firmware
+
+# Examples #
 examples %w|
 94.224.183.90:8080
 82.90.216.198
@@ -21,37 +27,22 @@ examples %w|
 kinsella.dnsalias.org
 |
 
+# Matches #
 matches [
 
 # GHDB Match
-# http://www.hackersforcharity.org/ghdb/?function=detail&id=1382
 { :ghdb=>'intitle:"Network Storage Link for USB 2.0 Disks" Firmware', :certainty=>75 },
 
 # Default title
 { :text=>'<title>Network Storage Link for USB 2.0 Disks</title>' },
 
+# Firmware Detection
+{ :firmware=>/          Version: &nbsp;V([\d\.\-a-zA-Z]+)<\/span> &nbsp;&nbsp;<\/td>/, :regexp_offset=>0 },
+
+# Model Detection
+{ :model=>/	 <td align="center" width="100" class="mname">([^<]+)<\/td>/, :regexp_offset=>0 },
+
 ]
-
-# Extract model and firmware
-def passive
-        m=[]
-
-	# Firmware
-        if @body =~ /          Version: &nbsp;V[\d\.\-a-zA-Z]+<\/span> &nbsp;&nbsp;<\/td>/
-                firmware=@body.scan(/          Version: &nbsp;V([\d\.\-a-zA-Z]+)<\/span> &nbsp;&nbsp;<\/td>/)
-                m << { :firmware=>firmware }
-        end
-
-	# Model
-	if @body =~ /	 <td align="center" width="100" class="mname">([^<]+)<\/td>/
-		model=@body.scan(/	 <td align="center" width="100" class="mname">([^<]+)<\/td>/)
-		m << { :model=>model }
-	end
-
-        m
-
-end
-
 
 end
 
