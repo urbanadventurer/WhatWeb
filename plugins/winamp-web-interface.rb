5f960b8ec9e0682a62583998cd72d703353e55fa
bcoles <bcoles@gmail.com>
2011-02-25 16:06:15 +1030

    Updated winamp-web-interface plugin

--- plugins/winamp-web-interface.rb
+++ plugins/winamp-web-interface.rb
@@ -4,35 +4,34 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-25 #
+# Updated version detection
+##
 Plugin.define "Winamp-Web-Interface" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-02
-version "0.1"
+version "0.2"
 description "web interface for winamp mp3 player - homepage: http://www.winamp.com/"
+
+# Google results as at 2010-07-02 #
+# 5 for "About Winamp Web Interface" intitle:"Winamp Web Interface"
+
+# Examples #
 examples %w|
-radio.hfqradio.com
-everquest.ita.chalmers.se
 |
 
+# Matches #
 matches [
 
-# 5 results @ 2010-07-02
+# GHDB Match
 {:ghdb=>'"About Winamp Web Interface" intitle:"Winamp Web Interface"', :certainty=>75 },
 
-{:text=>'<noframes><p><b>Frames are required for this site!<b></p><p>If no-frames operation is required, check the <b>No Frames Mode</b> box in the Options screen.</p>' }
+# Default noframes HTML
+{:text=>'<noframes><p><b>Frames are required for this site!<b></p><p>If no-frames operation is required, check the <b>No Frames Mode</b> box in the Options screen.</p>' },
+
+# Version Detection
+{ :version=>/<a href="JavaScript:about\(\)">About Winamp Web Interface v([\.\d]+)<\/a><\/h6><hr>/, :regexp_offset=>0 },
 
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<a href="JavaScript:about\(\)">About Winamp Web Interface v[\.\d]+<\/a><\/h6><hr>/
-                version=@body.scan(/<a href="JavaScript:about\(\)">About Winamp Web Interface v([\.\d]+)<\/a><\/h6><hr>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-end
-
 
 end
 
