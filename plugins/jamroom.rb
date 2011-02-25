2b0ea76bf015ccae37244a2ec053745e4e76bfc9
bcoles <bcoles@gmail.com>
2011-02-25 16:51:07 +1030

    Updated jamroom plugin

--- plugins/jamroom.rb
+++ plugins/jamroom.rb
@@ -4,12 +4,18 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-25 #
+# Updated version detection
+##
 Plugin.define "Jamroom" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-25
-version "0.1"
+version "0.2"
 description "Jamroom specializes in building web communities centered around media producers such as musicians, photographers, video enthusiasts and more - Jamroom gives you the capability to provide dozens of different services to your users. - homepage: http://www.jamroom.net/"
 
-# 201 results for "Powered by Jamroom" @ 2010-07-25
+# Google results as at 2010-07-25 #
+# 201 for "Powered by Jamroom"
+
+# Examples #
 examples %w|
 demo.jamroom.net
 alvandiweb.com/jams/
@@ -121,28 +127,23 @@ www.xyphire.com/jamroom/
 |
 
 # If you are a Licensed Jamroom User, you are free to remove ALL references to Jamroom or Jamroom.net from any of the OUTPUT templates (i.e. Artist Themes, Ranking templates, chart templates, etc.).
+
+# Matches #
 matches [
 
+# Default search input tag
 { :text=>'<input type="text" name="search_string" class="jform s_input" style="width:300px;">' },
 
+# Meta Designer
 { :text=>'<meta name="designer" content="Talldude Networks, LLC.">' },
 
+# Powered by link HTML
 { :regexp=>/<a href="http:\/\/www.jamroom.net"><img src="[^"]*" alt="Powered by Jamroom - the Powerful Social Media Platform" title="Powered by Jamroom - the Powerful Social Media Platform" border="0"><\/a>/ },
 
+# Version Detection # Meta Generator
+{ :version=>/<meta name="generator" content="Jamroom ([\d\.]+)">/, :regexp_offset=>0 },
+
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<meta name="generator" content="Jamroom [\d\.]+">/
-                version=@body.scan(/<meta name="generator" content="Jamroom ([\d\.]+)">/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
