19e3dd625a1088b2f24232677b5202d79a4baa1f
bcoles <bcoles@gmail.com>
2011-02-25 17:11:53 +1030

    Updated eMeeting-Online-Dating-Software plugin

--- plugins/eMeeting-Online-Dating-Software.rb
+++ plugins/eMeeting-Online-Dating-Software.rb
@@ -4,42 +4,40 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-25 #
+# Updated version detection
+##
 Plugin.define "eMeeting-Online-Dating-Software" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-30
-version "0.1"
+version "0.2"
 description "Dating Software by eMeeting Ltd - homepage: http://www.datingscripts.org/"
 
-# 53 results for "eMeeting Dating Software Version" @ 2010-08-30
+# Google results as at 2010-08-30 #
+# 53 for "eMeeting Dating Software Version"
+
+# Examples #
 examples %w|
 datingscripts.co.uk/dating-software-demo/
 datingscripts.ws/newadmin/
 datingscripts.ws/index.php
 |
 
+# Matches #
 matches [
 
-# Admin page
+# Admin Page # Default Title
 { :text=>'<html xmlns="http://www.w3.org/1999/xhtml"><head><title>Admin Area Login</title>' },
+
+# Admin Page # Default h2 Heading
 { :text=>'				<td valign="bottom"><h2>Admin Area Login</h2></td>' },
 
 # Powered by text
 { :text=>'<a href="http://www.datingscripts.org" alt="Dating Software by eMeeting Ltd" target="_blank">Dating Software Powered by eMeeting Ltd</a>' },
 
+# Version Detection # Powered by text
+{ :version=>/<p>eMeeting Dating Software <strong>Version ([\d\.]+)<\/strong>/, :regexp_offset=>0 },
+
 ]
-
-# Version detection using powered by text
-def passive
-        m=[]
-
-        if @body =~ /<p>eMeeting Dating Software <strong>Version ([\d\.]+)<\/strong>/
-                version=@body.scan(/<p>eMeeting Dating Software <strong>Version ([\d\.]+)<\/strong>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
