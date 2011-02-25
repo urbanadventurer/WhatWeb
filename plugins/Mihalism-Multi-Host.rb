6a6f367452f8201200014559403e30acca9b2cda
bcoles <bcoles@gmail.com>
2011-02-25 12:11:12 +1030

    Updated Mihalism-Multi-Host plugin

--- plugins/Mihalism-Multi-Host.rb
+++ plugins/Mihalism-Multi-Host.rb
@@ -4,13 +4,19 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-25 #
+# Updated version detection
+##
 Plugin.define "Mihalism-Multi-Host" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-28
-version "0.1"
+version "0.2"
 description "An easy image hosting solution for everyone. - homepage: http://www.mihalism.com/product/mmh/"
 
-# 355 results for "powered by Mihalism Multi Host" @ 2010-08-28
-# 78 results for "powered by Mihalism Multi Host" intitle:"MySQL Error (Powered by Mihalism Multi Host)" @ 2010-08-28
+# Google results as at 2010-08-28 #
+# 355 for "powered by Mihalism Multi Host"
+# 78  for "powered by Mihalism Multi Host" intitle:"MySQL Error (Powered by Mihalism Multi Host)"
+
+# Examples #
 examples %w|
 graphicshost.net
 ph.crosscomp.eu
@@ -45,6 +51,7 @@ www.anaqamaghribia.com/pic/
 www.ilhapirata.org/imagehost/gallery.php
 |
 
+# Matches #
 matches [
 
 # Powered by text
@@ -54,29 +61,18 @@ matches [
 { :regexp=>/<!-- Powered by Mihalism Multi Host - Copyright \(c\) [\d\,\ ]+ Mihalism Technologies \(www.mihalism.net\) -->/ },
 { :regexp=>/<!-- Powered by Mihalism Multi Host - Copyright \(c\) [\d\,\ ]+ Mihalism, Inc. \(www.mihalism.com\) -->/ },
 
-# Error page
+# Error Page # Default Title
 { :text=>'					<title>Fatal Error (Powered by Mihalism Multi Host)</title>' },
 { :text=>'					<title>MySQL Error (Powered by Mihalism Multi Host)</title>' },
 
-# Install page
+# Install Page
 { :text=>'    <title>Mihalism Multi Host » Installation</title>' },
 { :text=>'<b style="color: #F00;">Warning:</b> Using this installer will erase any already existing Mihalism Multi Host installation.' },
 
+# Version Detection # Meta Version
+{ :version=>/    <meta name="version" content="Mihalism Multi Host v([\d\.]+)" \/>/, :regexp_offset=>0 },
+
 ]
-
-# Version detection using meta version
-def passive
-        m=[]
-
-        if @body =~ /    <meta name="version" content="Mihalism Multi Host v([\d\.]+)" \/>/
-                version=@body.scan(/    <meta name="version" content="Mihalism Multi Host v([\d\.]+)" \/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
