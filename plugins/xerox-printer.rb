365c7f46d99e7a41fdefe0830b2335dc1a53272c
bcoles <bcoles@gmail.com>
2011-02-25 17:32:32 +1030

    Updated xerox-printer plugin

--- plugins/xerox-printer.rb
+++ plugins/xerox-printer.rb
@@ -4,15 +4,19 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-02-25 #
+# Updated model detection
+##
 Plugin.define "Xerox-Printers" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-07-22
-version "0.1"
+version "0.2"
 description "Xerox printers web interface - homepage: http://www.xerox.com/"
 
-# 28 results for "Phaser 6250" "Printer Neighborhood" "XEROX CORPORATION" -intitle @ 2010-07-22
-# http://www.hackersforcharity.org/ghdb/?function=detail&id=406
-# 25 Google results for intext:centreware inurl:status @ 2010-07-22
-# http://www.hackersforcharity.org/ghdb/?function=detail&id=450
+# Google results as at 2010-07-22 #
+# 28 for "Phaser 6250" "Printer Neighborhood" "XEROX CORPORATION" -intitle
+# 25 for intext:centreware inurl:status
+
+# Examples #
 examples %w|
 130.236.83.191
 accb8-2.usc.edu
@@ -32,35 +36,32 @@ giotto.fisica.unimo.it/status.html
 jordan-clr-laser.uchicago.edu/status/statgeneralx.htm
 |
 
+# Matches #
 matches [
 
-{ :text=>'Phaser 6250DP</title>', :version=>"Phaser 6250DP" },
-{ :text=>'Phaser 6250N</title>', :version=>"Phaser 6250N" },
+# Phaser 6250DP
+{ :text=>'Phaser 6250DP</title>', :model=>["Phaser 6250DP"] },
+{ :text=>'Phaser 6250N</title>', :model=>["Phaser 6250N"] },
 
-{ :text=>'<title> Home - Xerox Phaser 3300MFP </title>', :version=>"Phaser 3300MFP" },
+# Phaser 3300MFP
+{ :text=>'<title> Home - Xerox Phaser 3300MFP </title>', :model=>["Phaser 3300MFP"] },
 
-{ :text=>'	<br><font class=s4 face="Arial,Helvetica" size=4 color="#0000FF"><b><font size=4>&nbsp;&nbsp;&nbsp;&nbsp;</font>DocuPrint N2125</b></font></td>', :version=>"DocuPrint N2125" },
+# DocuPrint N2125
+{ :text=>'	<br><font class=s4 face="Arial,Helvetica" size=4 color="#0000FF"><b><font size=4>&nbsp;&nbsp;&nbsp;&nbsp;</font>DocuPrint N2125</b></font></td>', :model=>["DocuPrint N2125"] },
 
-{ :text=>'                              color=#008000 face="Arial,Helvetica" size=2>Phaser 4500DT</font></td>', :version=>"Phaser 4500DT" },
+# Phaser 4500DT
+{ :text=>'                              color=#008000 face="Arial,Helvetica" size=2>Phaser 4500DT</font></td>', :model=>["Phaser 4500DT"] },
 
-{ :text=>'<td valign=top width=85><font class=s2 face="Arial,Helvetica" size=2 color="#008000"> Phaser 7700DN </font></td>', :version=>"Phaser 7700DN" },
+# Phaser 7700DN
+{ :text=>'<td valign=top width=85><font class=s2 face="Arial,Helvetica" size=2 color="#008000"> Phaser 7700DN </font></td>', :model=>["Phaser 7700DN"] },
 
-{ :text=>'                              color=#008000 face="Arial,Helvetica" size=2>Phaser 8400DP-1</font></td>', :version=>"Phaser 8400DP-1" },
+# Phaser 8400DP-1
+{ :text=>'                              color=#008000 face="Arial,Helvetica" size=2>Phaser 8400DP-1</font></td>', :model=>["Phaser 8400DP-1"] },
+
+# Model Detection # Default Title
+{ :model=>/<title>(FX[0-9A-Z]{6})-Home<\/title>/, :regexp_offset=>0 },
 
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<title>FX[0-9A-Z]{6}-Home<\/title>/
-                version=@body.scan(/<title>FX([0-9A-Z]{6})-Home<\/title>/)[0][0]
-                m << {:version=>"FX"+version}
-        end
-
-        m
-
-end
-
 
 end
 
