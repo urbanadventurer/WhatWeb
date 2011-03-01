0fe5eb097651660c02d617e0cb38526f553905d2
bcoles <bcoles@gmail.com>
2011-03-01 11:35:23 +0800

    Updated juniper-netscreen-secure-access plugin

--- plugins/juniper-netscreen-secure-access.rb
+++ plugins/juniper-netscreen-secure-access.rb
@@ -63,8 +63,6 @@ https://sap.bsli.in/
 
 matches [
 
-{:name => 'default url',:url=>'/dana-na/auth/url_default/welcome.cgi'},
-
 {:name => 'juniper logo md5', :md5=> '1ec04eec4e1898da8258215a2eb4758b', :url=>'/dana-na/auth/welcome.cgi?p=rolelogo'},
 
 {:name => 'html body', :regexp=>/src="\/dana\-na\/css\/ds\.js">|<img border="0" src="welcome\.cgi\?p=logo|src="\/dana\-na\/imgs\/space\.gif"|document\.cookie = "DSPREAUTH="\+ escape\(""\)|src="\/dana\-na\/auth\/url_default\/s/i }
