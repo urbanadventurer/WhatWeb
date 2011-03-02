30772e8d666b2ed2931607bb6810f29b7149ef84
bcoles <bcoles@gmail.com>
2011-03-02 20:41:43 +1030

    Updated x10media-mp3-search-engine plugin

--- plugins/x10media-mp3-search-engine.rb
+++ plugins/x10media-mp3-search-engine.rb
@@ -4,16 +4,24 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-03-02 #
+# Updated version detection
+##
 Plugin.define "X10media-MP3-Search-Engine" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-08-01
-version "0.1"
+version "0.2"
 description "X10media MP3 search engine - homepage: http://www.x10media.com/mp3-script.php"
 
-# 134 results for "powered by x10media.com a MP3 Search Script" @ 2010-08-01
-# 196 results for "This search engine is in no way intended for illegal downloads. " @ 2010-08-01
+# Google results as at 2010-08-01 #
+# 134 for "powered by x10media.com a MP3 Search Script"
+# 196 for "This search engine is in no way intended for illegal downloads. "
+
+# Examples #
 examples %w|
 x10media.com/mp3/
 buscomusic.com
+mp3man.net
+huntmymusic.com
 findmp3s.com
 freemusictown.com/install/
 justfreemp3.com
@@ -23,16 +31,22 @@ mp3url.net
 musicprowler.com
 myfreemp3s.org
 searchjams.com
+wesearchmusic.com
 songthief.com/search/
 www.smokingmp3.com
 osmp3.com
 www.burunet.com
 www.easymp3.eu
+www.justgamerz.com/Nuevo%20Directorio/New%20install/install/
+www.baixarmusicas.org/baixar/install/
 www.fastdownloadmp3.com
 www.freemp3musicdownloads.ca/search/
+www.t1712.greatnet.de
 www.freemusicdownloadwebsites.com/mp3-search-engine/
 www.indiamp3.com
 www.itrackz.net
+www.officialvideo.org/install/
+www.soundtrack.co
 www.mp351.com
 www.mp3download1.com
 www.mp3lover.info
@@ -51,48 +65,34 @@ www.searchmuzik.com
 www.searchsongs.net
 |
 
+# Matches #
 matches [
 
+# Meta Tags
 { :text=>'	<meta name="copyright" content="X10Media"/>', :certainty=>75 },
 { :text=>'	<meta name="contributor" content="X10Media"/>', :certainty=>75 },
+
+# Default Title
 { :text=>"<title>x10media Mp3 Search Engine</title>" },
+
+# Powered by text
 { :text=>'Powered by <a href="http://www.x10media.com">x10media.com</a> a <a href="http://www.x10media.com/mp3-script.php">MP3 Search Script</a>' },
+
+# Database Error
 { :text=>'Cannot connect to the database. Possibly, the X10Media Mp3 Search engine is not installed. Click <a href="install/">here</a>, to install it.' },
 
+# Version Detection
+{ :version=>/<meta name="publisher" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/, :regexp_offset=>0 },
+
+{ :version=>/<meta name="dc\.rights" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/, :regexp_offset=>0 },
+
+{ :version=>/<meta name="dc\.publisher" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/, :regexp_offset=>0 },
+
+{ :version=>/<meta name="owner" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/, :regexp_offset=>0 },
+
+{ :version=>/<title>x10media`s Mp3 Search Engine V\.([\d\.]+)[\ Installer]*<\/title>/, :regexp_offset=>0 },
+
 ]
-
-def passive
-        m=[]
-
-        if @body =~ /<meta name="publisher" content="x10media`s Mp3 Search Engine V\.[\d\.]+"\/>/
-                version=@body.scan(/<meta name="publisher" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        if @body =~ /<meta name="dc\.rights" content="x10media`s Mp3 Search Engine V\.[\d\.]+"\/>/
-                version=@body.scan(/<meta name="dc\.rights" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        if @body =~ /<meta name="dc\.publisher" content="x10media`s Mp3 Search Engine V\.[\d\.]+"\/>/
-                version=@body.scan(/<meta name="dc\.publisher" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        if @body =~ /<meta name="owner" content="x10media`s Mp3 Search Engine V\.[\d\.]+"\/>/
-                version=@body.scan(/<meta name="owner" content="x10media`s Mp3 Search Engine V\.([\d\.]+)"\/>/)[0][0]
-                m << {:version=>version}
-        end
-
-        if @body =~ /<title>x10media`s Mp3 Search Engine V\.[\d\.]+[\ Installer]*<\/title>/
-                version=@body.scan(/<title>x10media`s Mp3 Search Engine V\.([\d\.]+)[\ Installer]*<\/title>/)[0][0]
-                m << {:version=>version}
-        end
-
-        m
-
-end
-
 
 end
 
