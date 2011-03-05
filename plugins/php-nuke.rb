8fede08fd7ada5d6e20f43d8013b11fa052245ba
bcoles <bcoles@gmail.com>
2011-03-06 02:37:36 +1030

    Updated php-nuke plugin

--- plugins/php-nuke.rb
+++ plugins/php-nuke.rb
@@ -4,35 +4,31 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
-# Version 0.2
-# remove :certainty
+# Version 0.4 # 2011-03-06 # Brendan Coles <bcoles@gmail.com>
+# Updated module detection
+##
 # Version 0.3
 # uses :module
-
+##
+# Version 0.2
+# remove :certainty
+##
 Plugin.define "PHPNuke" do
 author "Andrew Horton"
-version "0.3"
-
+version "0.4"
 description "PHP-Nuke is a free CMS. Homepage: phpnuke.org. The plugin passively recognises modules. An obvious improvement would be to aggresively discover modules and discover the phpnuke version"
 
-examples %w|http://www.patientjourney.org.nz http://www.schoolhistory.co.nz http://www.sil3nt.zl1my.co.nz http://www.tagairsoft.co.nz http://www.freeze.co.nz/community/index.php http://www.ig42.com http://www.isea.org.nz http://www.kakariki.net http://www.tweakforce.com www.vcdeasy.org www.quransunnah.com|
+# Google results as at 2011-03-06 #
+# 497 for allinurl:"modules.php?name=Your_Account"
+# 305 for "powered by phpnuke"
+#  88 for allinurl:phpnuke/modules.php?name=Search
 
-# www.vcdeasy.org and www.quransunnah.com are disguised 
+# Examples #
+examples %w|http://www.patientjourney.org.nz http://www.schoolhistory.co.nz http://www.sil3nt.zl1my.co.nz http://www.tagairsoft.co.nz http://www.freeze.co.nz/community/index.php http://www.ig42.com http://www.isea.org.nz http://www.kakariki.net http://www.tweakforce.com www.vcdeasy.org www.quransunnah.com www.p90.nl/phpnuke/ dayfamily4.us/PHPNuke/ www.snafu-clan.co.uk/phpnuke/ www.carsena.com/phpnuke/ fullhousesoftware.com/phpnuke/ www.nmpfd.com/phpnuke/ www.clubafrika.com/phpnuke/ www.rollingthundergaming.com/phpnuke/ beachpool.com/phpnuke/|
 
-#  ["<a href=\"modules.php?name=Your_Account\">", 19],
-# "<META NAME=\"RATING\" CONTENT=\"GENERAL\">"
-# <a href=\"http://phpnuke.org/files/gpl.txt\">"
-# <a href="modules.php?name=Feedback">Feedback</a>
-#<a href='http://www.phpnuke.org'><img src='images/powered/phpnuke.gif' title="The engine code is copyright of PHP-Nuke." border='0'></a>
-
-#<meta name="generator" content="AutoTheme - http://spidean.mckenzies.net / PHP-Nuke - http://phpnuke.org">
-#<meta name="generator" content="PHP-Nuke Copyright (c) 2006 by Francisco Burzi.
-# <META NAME="GENERATOR" CONTENT="PHP-Nuke Copyright (c) 2005 by Francisco Burzi. This is free software, and you may redistribute it under the GPL (http://phpnuke.org/files/gpl.txt). PHP-Nuke comes with absolutely no warranty, for details, see the license (http://phpnuke.org/files/gpl.txt).">
-
-#www.FreePHPNuke.org uses gate.html instead of modules.php - snfl.freephpnuke.org
-
+# Matches #
 matches [
+
 {:name=>"meta generator tag with PHP-Nuke", 
 :regexp=>/<meta name="generator" content="[^"]+(PHP-Nuke|http:\/\/phpnuke.org)/i},
 
@@ -55,18 +51,12 @@ matches [
 :regexp=>/<a href="[^"]*modules.php\?name=[a-zA-Z0-9_]+[^"]*">/},
 
 {:name=>"engine code is copyright of PHP-Nuke",
-:text=>"<a href='http://www.phpnuke.org'><img src='images/powered/phpnuke.gif' title=\"The engine code is copyright of PHP-Nuke.\" border='0'></a>"}
-]
+:text=>"<a href='http://www.phpnuke.org'><img src='images/powered/phpnuke.gif' title=\"The engine code is copyright of PHP-Nuke.\" border='0'></a>"},
 
-def passive
-	m=[]
-	if @body =~ /<a href="[^"]*modules.php\?name=[a-zA-Z0-9_]+[^"]*">/
-#		pp @body.scan(/<a href="modules.php\?name=([a-zA-Z0-9_]+)">[a-zA-Z0-9_]+<\/a>/)
-		modules = @body.scan(/<a href="[^"]*modules.php\?name=([a-zA-Z0-9_]+)[^"]*">/).flatten.compact.sort.uniq.join(",")
-		m << {:certainty=>75,:module=>modules}
-	end
-	m
-end
+# Module Detection
+{ :certainty=>75, :module=>/<a href="[^"]*modules.php\?name=([a-zA-Z0-9_]+)[^"]*">/, :regexp_offset=>0 },
+
+]
 
 end
 
