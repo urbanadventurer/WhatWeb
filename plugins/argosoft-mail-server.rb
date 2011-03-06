3c90887fa8c62ced25ad3b5aa440840081ded412
bcoles <bcoles@gmail.com>
2011-03-06 19:43:58 +0800

    Updated argosoft-mail-server plugin

--- plugins/argosoft-mail-server.rb
+++ plugins/argosoft-mail-server.rb
@@ -4,6 +4,9 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.4 # 2011-03-06 #
+# Updated OS detection
+##
 # Version 0.3 # 2011-01-07 #
 # Updated HTTP Server Header match
 # Added OS detection
@@ -13,26 +16,21 @@
 ##
 Plugin.define "ArGoSoft-Mail-Server" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-13 
-version "0.3"
+version "0.4"
 description "ArGoSoft-Mail-Server web interface - homepage: http://www.argosoft.com/"
 
-# 26 results for GHDB: +intitle:"adding new user" "ArGoSoft Mail Server" +inurl:addnewuser -inurl @ 2010-06-13
+# Google results as at 2010-06-13 #
+# 26 for +intitle:"adding new user" "ArGoSoft Mail Server" +inurl:addnewuser -inurl 
 
 # Examples #
 examples %w|
 www.mecmont.info/addnewuser
-www.network-options.net/addnewuser
-www.nextwaver.com:8383/addnewuser
 vbsca.ca:81/addnewuser
 www.chielie.com:8081/addnewuser
-inyt.pte.hu:81/addnewuser
-mail.bluecreek.k12.mt.us:82/addnewuser
 softkiller.ws:85/addnewuser
 163.247.50.25/addnewuser
-talshiar.adsl.dk:81/addnewuser
 www.buchhorn.net:1080/addnewuser
 www.mascot.gr:8080/addnewuser
-mail.jzstock.com.cn:82/addnewuser
 mail.fws.net/addnewuser
 |
 
@@ -43,8 +41,8 @@ matches [
 	{ :ghdb=>'intitle:"adding new user" "ArGoSoft Mail Server" inurl:addnewuser', :certainty=>75 },
 
 	# Version and OS detection
-	{ :version=>/ArGoSoft Mail Server Pro for WinNT\/2000, Version [\d\.]+ \(([\d\.]+)\)<\/p>/, :string=>"OS:Windows NT/2000" },
-	{ :version=>/ArGoSoft Mail Server Pro for WinNT\/2000\/XP, Version [\d\.]+ \(([\d\.]+)\)<\/p>/, :string=>"OS:Windows NT/2000/XP" },
+	{ :version=>/ArGoSoft Mail Server Pro for WinNT\/2000, Version [\d\.]+ \(([\d\.]+)\)<\/p>/, :os=>"Windows NT/2000" },
+	{ :version=>/ArGoSoft Mail Server Pro for WinNT\/2000\/XP, Version [\d\.]+ \(([\d\.]+)\)<\/p>/, :os=>"Windows NT/2000/XP" },
 
 ]
 
@@ -55,11 +53,11 @@ def passive
 	# Version and OS detection # HTTP Header Server text
 	if @meta["server"] =~ /ArGoSoft Mail Server Pro for WinNT\/2000, Version [\d\.]+ \(([\d\.]+)\)/
 		version=@meta["server"].scan(/ArGoSoft Mail Server Pro for WinNT\/2000, Version [\d\.]+ \(([\d\.]+)\)/)[0][0]
-		m << { :version=>version, :string=>"OS:Windows NT/2000" }
+		m << { :version=>version, :os=>"Windows NT/2000" }
 	end
 	if @meta["server"] =~ /ArGoSoft Mail Server Pro for WinNT\/2000\/XP, Version [\d\.]+ \(([\d\.]+)\)/
 		version=@meta["server"].scan(/ArGoSoft Mail Server Pro for WinNT\/2000\/XP, Version [\d\.]+ \(([\d\.]+)\)/)[0][0]
-		m << { :version=>version, :string=>"OS:Windows NT/2000/XP" }
+		m << { :version=>version, :os=>"Windows NT/2000/XP" }
 	end
 
 	m
