0f021f744729fb5db44e22d0079def234b90d428
bcoles <bcoles@gmail.com>
2011-03-11 06:34:33 +1030

    Updated Resin plugin

--- plugins/Resin.rb
+++ plugins/Resin.rb
@@ -4,10 +4,18 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-03-11 #
+# Updated version detection
+##
 Plugin.define "Resin" do
 author "Brendan Coles <bcoles@gmail.com>" # 2011-02-24
-version "0.1"
-description "Resin provides a reliable, fast Web server which can also be used as a load balancer - homepage: http://www.caucho.com/resin/admin/http-server.xtp"
+version "0.2"
+description "Resin provides a reliable, fast Web server which can also be used as a load balancer. Resin can operate as a standalone server or as a fast servlet runner for IIS or Apache - homepage: http://www.caucho.com/resin/admin/http-server.xtp"
+
+# Resin provides a fast servlet runner for IIS, allowing IIS to run servlets and JSP files :
+# http://www.caucho.com/resin/admin/starting-resin-iis-7.xtp
+# Resin provides a fast servlet runner for Apache 1.3 or greater, allowing Apache to run servlets and JSP files :
+# http://www.caucho.com/resin/admin/starting-resin-apache.xtp
 
 # ShodanHQ results as at 2011-02-24 #
 # 25,565 for server Resin
@@ -29,8 +37,11 @@ examples %w|
 def passive
 	m=[]
 
-	# Version Detection # HTTP Server Header
-	m << { :version=>@meta['server'].scan(/Resin\/([\d\.]+)/) } if @meta['server'] =~ /Resin\/([\d\.]+)/
+	# Version Detection # HTTP Server Header # Standalone
+	m << { :version=>@meta["server"].scan(/^Resin\/([^\s]+)/), :string=>"Standalone" } if @meta["server"] =~ /^Resin\/([^\s]+)/
+
+	# Version Detection # HTTP Server Header # Plugin
+	m << { :version=>@meta["server"].scan(/[\s]+Resin\/([^\s]+)/), :string=>"Plugin" } if @meta["server"] =~ /[\s]+Resin\/([^\s]+)/
 
 	# Return passive matches
 	m
