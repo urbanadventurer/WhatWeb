fce7c0425713c6b63e426ad21dc141ddc646b818
bcoles <bcoles@gmail.com>
2011-03-19 21:58:56 +1030

    Added Tencent-QQ plugin

---
+++ plugins/Tencent-QQ.rb
@@ -0,0 +1,39 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Tencent-QQ" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-03-19
+version "0.1"
+description "Tencent QQ, generally referred to as QQ, is the most popular free instant messaging computer program in Mainland China. As of September 30, 2010, the active QQ users accounts for QQ IM amounted to 636.6 million, possibly making it the world's largest online community. The number of simultaneous online QQ accounts exceeded 100 million. - Homepage: http://www.qq.com/"
+# More info: http://en.wikipedia.org/wiki/Tencent_QQ
+
+# ShodanHQ results as at 2011-03-19 #
+# 8 for Tencent/HTTP_Magic_Expression
+
+# Examples #
+examples %w|
+112.90.136.71
+203.195.110.20
+124.153.72.86
+203.184.141.212
+119.147.7.210
+119.147.15.33
+|
+
+# Passive #
+def passive
+	m=[]
+
+	# HTTP Server Header
+	m << { :name=>"HTTP Server Header" } if @meta["server"] =~ /^HTTP_ME\/\d\.\d Tencent\/HTTP_Magic_Expression$/
+
+	# Return passive matches
+	m
+
+end
+
+end
+
