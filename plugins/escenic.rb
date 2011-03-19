99f7924216859faa11b06e442de70420ca560aa8
bcoles <bcoles@gmail.com>
2011-03-19 18:53:18 +0800

    Updated escenic plugin

--- plugins/escenic.rb
+++ plugins/escenic.rb
@@ -4,34 +4,73 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-03-19 # Brendan Coles <bcoles@gmail.com>
+# Updated regex to decrease false positives
+##
 Plugin.define "Escenic" do
 author "Erik Inge Bolsø <knan+whatweb@redpill-linpro.com>"
 version "0.2"
-description "Escenic is a commercial CMS popular in the Nordic region."
-examples %w| escenic.com |
+description "Escenic is a commercial CMS popular in the Nordic region. - Homepage: http://escenic.com/"
 
-# a comment block here is a good place to make notes for yourself and others
+# Google results as at 2011-03-19 #
+# 273 for filetype:ece
 
-# These are the types of matches:
-# :regexp        -- it's a regular expression in ruby, eg. /^foobar$/
-# :text          -- case sensitive text
-# :ghdb          -- Google Hack Database. u can use intitle: inurl: -
-# :md5           -- MD5 hash
-# :tagpattern    -- a pattern made by the html entities. fuzzy matching coming soon
-# :url           -- you can combine this with other variables or use by itself
-# :name          -- naming the matches is optional
-# :version       -- this is used to return data
-# :string        -- this is used to return data
+# Examples #
+examples %w|
+escenic.com
+www.barnetid.no
+www.side3.no
+www.filmweb.no
+www.reiserogferie.no
+golf.no
+spn.dk
+www.arbeidsmanden.no
+www.folkebladet.no
+www.na24.no
+www.spillmagasinet.no
+epn.dk
+www.vareveger.no
+www.oslokino.no
+www.industrien.no
+www.idg.no
+fpn.dk
+www.banett.no
+www.oa.no
+www.thetimes.co.uk
+www.thesundaytimes.co.uk
+www.nyteknik.se
+www.gooieneemlander.nl
+www.timesonline.co.uk
+jp.dk
+www.upstreamonline.com
+reise.bt.no
+www.ed.nl
+www.pzc.nl
+www.thesun.co.uk
+www.majalla.com
+|
 
-# Matches are enclosed in {} brackets and separated by commas
+# Matches #
 matches [
-{:name=>"escenic 4.x image storage structure",
-:certainty=>100,
-:regexp=>/.*\/archive\/\d{5}\//},
 
-{:name=>"escenic .ece url match",
-:certainty=>75,
-:regexp=>/<a .*\.ece.*<\/a>/},
+# HTML Comment
+{ :text=>'<!-- Start Escenic Analysis Engine client script -->' },
+
+# Meta Author
+{ :certainty=>75, :text=>'<meta name="author" content="Escenic AS"/>' },
+
+# Search Form
+{ :certainty=>75, :regexp=>/<form action="\/sok\/" id="[^"]+" method="get" accept-charset="utf-8"[^>]*>/ },
+
+# GHDB # Filetype
+{ :certainty=>75, :ghdb=>"filetype:ece inurl:article" },
+
+# escenic 4.x image storage structure
+{ :certainty=>25, :regexp=>/<img[^>]+src="[^"^>]+\/archive\/\d{5}\/[^"^>]+"[^>]*>/ },
+
+# escenic .ece url match
+{ :certainty=>25, :regexp=>/<a[^>]+href="[^"^>]+\/article[\d]{5,10}\.ece">.*<\/a>/ },
+
 ]
 
 end
