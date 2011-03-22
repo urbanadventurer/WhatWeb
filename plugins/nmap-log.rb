6798f1763b2209a6e30b1e666d0c3f8224b37f17
bcoles <bcoles@gmail.com>
2011-03-22 17:30:42 +1030

    Updated nmap-log plugin

--- plugins/nmap-log.rb
+++ plugins/nmap-log.rb
@@ -4,15 +4,20 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-03-22 #
+# Updated regex
+##
 Plugin.define "Nmap-Log" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-21
-version "0.1"
-description "This plugin identifies nmap logs and extracts the hostname, OS, active ports and nmap version."
+version "0.2"
+description "This plugin identifies nmap plain-text logs and extracts the hostname, OS, active ports and nmap version. It does not work for logs in the XML file format. - Homepage: http://www.insecure.org/nmap/"
 
-# 32 results for "Starting nmap" "fyodor@insecure.org" (ext:txt | ext:log) @ 2010-10-21
+# Google results as at 2010-10-21 #
+# 32 for "Starting nmap" "fyodor@insecure.org" (ext:txt | ext:log)
+
+# Examples #
 examples %w|
 web.textfiles.com/hacking/portscan.txt
-kambing.ui.ac.id/onnopurbo/library/library-ref-ind/ref-ind-1/application/policy/mustika-ratu-case/capture2.txt
 www.armory.com/~cerberus/tutorials/unix-black-hat.txt
 www.ossec.net/rootkits/studies/knark.txt
 staff.washington.edu/dittrich/misc/power.analysis.txt
@@ -22,45 +27,36 @@ nsh.altervista.org/docs/mdocs/neural/primipassi.txt
 www.ataliba.eti.br/sections/old-hacking/unsekurity/texto1/detonakiddies.txt
 |
 
-matches [
-
-# Get Nmap version # older version # 2.x +
-{ :version=>/^Starting nmap V. ([\d\.]+) by fyodor@insecure.org/ },
-
-# Get Nmap version # newer versions # 4.x +
-{ :version=>/^Starting Nmap ([\d\.]+) \( http:\/\/nmap.org \) at / },
-
-]
-
-# Extract details
+# Passive #
 def passive
 	m=[]
 
-	if @body =~ /^Interesting ports on ([^\r^\n]+)/ and (@body =~ /^Starting nmap / or @body =~ /^Starting Nmap /)
+	# Extract details
+	if @body =~ /^Interesting ports on (.+):[\r]?$/ and @body =~ /^Starting (n|N)map /
 
-		# Hostname
-		m << { :model=>@body.scan(/^Interesting ports on ([^\r^\n]+)/)[0][0] }
+		# Version # Newer versions # 4.x
+		m << { :version=>@body.scan(/^Starting Nmap ([^\s]+) \( http:\/\/nmap.org \) at /) } if @body =~ /^Starting Nmap ([^\s]+) \( http:\/\/nmap.org \) at /
+
+		# Version # Older version # 2.x
+		m << { :version=>@body.scan(/^Starting nmap V. ([^\s]+) by fyodor@insecure.org/) } if @body =~ /^Starting nmap V. ([^\s]+) by fyodor@insecure.org/
+
+		# Target
+		m << { :string=>"Target: "+@body.scan(/^Interesting ports on (.+):[\r]?$/)[0][0] }
 
 		# Operating System
-		if @body =~ /Remote operating system guess: ([^\r^\n]*)/
-			accounts=@body.scan(/^Remote operating system guess: ([^\r^\n]*)/)[0][0]
-			m << { :account=>accounts }
-		end
+		m << { :string=>"OS: "+@body.scan(/^Remote operating system guess: ([^\r^\n]*)/)[0][0] } if @body =~ /Remote operating system guess: ([^\r^\n]*)/
 
 		# Ports
-		if @body =~ /Port[\s]+State[\s]+Service/i
+		if @body =~ /Port[\s]+State[\s]+Service/i and @body =~ /^([\d]{1,5})\/(udp|tcp)[\s]+open[\s]+([a-z]+)/
 
-			if @body =~ /^([\d]{1,5})\/([udp|tcp]{3})[\s]+open[\s]+([a-z]+)/
-				modules=@body.scan(/^([\d]{1,5})\/([udp|tcp]{3})[\s]+open[\s]+([a-z]+)/)
-				m << { :module=>modules }
-			end
+			@body.scan(/^([\d]{1,5})\/(udp|tcp)[\s]+open[\s]+([a-z]+)/).each { |service| m << { :string=>service[0]+"("+service[1]+")("+service[2]+")" } if service.size == 3 and service[0] =~ /[\d]{1,5}/ }
 
 		end
 	
 	end
 
+	# Return passive matches
 	m
-
 end
 
 end
