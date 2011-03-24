e4563c9b0549ed281b6e7ee06243636e4f0c6514
Andrew Horton <horton.nz@gmail.com>
2011-03-25 01:10:26 +1300

    Updtaed mikrotik router

--- plugins/mikrotik-router.rb
+++ plugins/mikrotik-router.rb
@@ -9,10 +9,12 @@
 # remove :certainty & :name 
 # Version 0.3
 # Uses :version=>//
+# Version 0.4
+# Added md5 match for 401 page
 
 Plugin.define "MikroTik" do
 author "Andrew Horton"
-version "0.3"
+version "0.4"
 description "MikroTik router. Homepage: http://www.mikrotik.com"
 
 # identifying strings
@@ -23,7 +25,8 @@ description "MikroTik router. Homepage: http://www.mikrotik.com"
 
 matches [
 {:text=>"<title>mikrotik routeros > administration</title>"},
-{:version=>/<div class="top">mikrotik routeros ([^ ]+) configuration page</,  :name=>"mikrotik routeros ([^ ]+) configuration page" } 
+{:version=>/<div class="top">mikrotik routeros ([^ ]+) configuration page</,  :name=>"mikrotik routeros ([^ ]+) configuration page" },
+{:md5=>"bacf8a0c6f3e702db9be393989b2a0b5", :name=>"401 page"}
 ]
 
 
