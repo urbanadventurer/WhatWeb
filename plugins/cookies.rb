a7f1f427f25af3cf0ac7dc955056067734be5115
bcoles <bcoles@gmail.com>
2011-04-08 07:05:33 +0800

    Updated cookies plugin

--- plugins/cookies.rb
+++ plugins/cookies.rb
@@ -4,6 +4,9 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.4 # 2011-04-08 # Brendan Coles <bcoles@gmail.com>
+# Added username and localfile path detection for cookies containing public_html
+##
 # Version 0.3 # 2011-01-30 # Brendan Coles <bcoles@gmail.com>
 # Fixed regex bug # Cookies were always split by "=" even when it wasn't present
 ##
@@ -12,8 +15,13 @@
 ##
 Plugin.define "Cookies" do
 author "Andrew Horton"
-version "0.3"
+version "0.4"
 description "Display the names of cookies in the HTTP headers. The values are not returned to save on space."
+
+# ShodanHQ results as at 2011-04-08 #
+# 2,527,334 for set-cookie
+# 37 for set-cookie path home public_html
+# 17 for set-cookie path=/home/ /public_html
 
 # Passive #
 def passive
@@ -23,6 +31,12 @@ def passive
 	unless @cookies.nil? or @cookies.empty?
 		@cookies.each do |cookie|
 			m << { :string=>cookie.split("=")[0] } if cookie =~ /=/
+		end
+
+		# Detect local file paths containing public_html
+		if @cookies.to_s =~ /path=\/home[\d]*\/([^\/]+)\/public_html\//
+			m << { :account=>@cookies.to_s.scan(/path=\/home[\d]*\/([^\/]+)\/public_html\//) }
+			m << { :filepath=>@cookies.to_s.scan(/path=(\/home[\d]*\/[^\/]+\/public_html\/)/) }
 		end
 	end
 
