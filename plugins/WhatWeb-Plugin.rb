a0066fd0c93e499b400e8d37970609f327463d3e
bcoles <bcoles@gmail.com>
2011-04-12 21:37:47 +0930

    Updated WhatWeb-Plugin plugin

--- plugins/WhatWeb-Plugin.rb
+++ plugins/WhatWeb-Plugin.rb
@@ -4,9 +4,12 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-04-12 #
+# Updated regex
+##
 Plugin.define "WhatWeb-Plugin" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-16
-version "0.1"
+version "0.2"
 description "This plugin detects instances of WhatWeb plugins. If this plugin is returned then chances are the other results are false positives."
 
 # Plugin list: http://github.com/urbanadventurer/WhatWeb/tree/master/plugins/
@@ -14,28 +17,32 @@ examples %w|
 http://github.com/urbanadventurer/WhatWeb/raw/master/plugins/1024-CMS.rb
 |
 
-# Extract plugin details
+# Passive #
 def passive
 	m=[]
 
-	if @body =~ /# redistribution and commercial restrictions. Please see the WhatWeb/ or @body =~ /# This file is part of WhatWeb and may be subject to/
+	# Extract plugin details
+	if @body =~ /^# redistribution and commercial restrictions. Please see the WhatWeb/ or @body =~ /^# This file is part of WhatWeb and may be subject to/
 
 		# Extract version
 		if @body =~ /^version "([^\"]+)"/
-			version=@body.scan(/^version "([^\"]+)"/)
-			m<<{:version=>version}
+			m << { :version=>@body.scan(/^version "([^\"]+)"/) }
 		end
 
 		# Extract plugin name
 		if @body =~ /^Plugin.define "([^\"]+)" do/
-			modules=@body.scan(/^Plugin.define "([^\"]+)" do/)
-			m<<{:module=>modules}
+			m << { :string=>@body.scan(/^Plugin.define "([^\"]+)" do/) }
+		end
+
+		# Extract modules
+		if @body =~ /^def ([a-z]+)[\s]?$/
+			m << { :module=>@body.scan(/^def ([a-z]+)[\s]?$/) }
 		end
 
 	end
 
+	# Return passive matches
 	m
-
 end
 
 end
