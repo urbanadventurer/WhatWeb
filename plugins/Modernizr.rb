ad6f8c517c29918a811b59f2200d1c4862622a76
bcoles <bcoles@gmail.com>
2011-04-15 14:20:24 +0930

    Added Modernizr plugin

---
+++ plugins/Modernizr.rb
@@ -0,0 +1,31 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Modernizr" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-04-15
+version "0.1"
+description "Modernizr adds classes to the <html> element which allow you to target specific browser functionality in your stylesheet. You don't actually need to write any Javascript to use it. [JavaScript] - Homepage: http://www.modernizr.com/"
+
+# Examples #
+examples %w|
+www.modernizr.com
+http://nettuts.s3.amazonaws.com/449_javascriptlibraries/examples/modernizrExample.html
+|
+
+# Matches #
+matches [
+
+# JavaScript Source
+{ :regexp=>/<script [^>]*src=["'][^>]*modernizr[^>]*\.js/i },
+
+# Version Detection # JavaScript Source
+{ :version=>/<script [^>]*src=["'][^>]*modernizr-([^>]+)\.js/i },
+
+]
+
+end
+
+
