5b406d7536f0fb1e126b4ed24dde8cf3d142c905
bcoles <bcoles@gmail.com>
2011-04-21 23:37:21 +0930

    Added CollegiateLink plugin

---
+++ plugins/CollegiateLink.rb
@@ -0,0 +1,51 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "CollegiateLink" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-04-22
+version "0.1"
+description "CollegiateLink - Homepage: http://www.collegiatelink.net/"
+
+# 120 for "Powered by CollegiateLink Version"
+# 109 for "Powered by CollegiateLink Version" inurl:"Community?action"
+
+# Dorks #
+dorks [
+'"Powered by CollegiateLink Version" inurl:"Community?action"'
+]
+
+# Examples #
+examples %w|
+wpunj.collegiatelink.net
+muhlenberg.collegiatelink.net
+allegheny.collegiatelink.net
+nicholls.collegiatelink.net
+clubhaven.collegiatelink.net
+buffalotraining.collegiatelink.net
+rulinked.camden.rutgers.edu
+callink.berkeley.edu
+myinvolvement.indiana.edu
+es-salink-tst.uits.indiana.edu
+outsidetheclassroom.pitt.edu
+studentlink.iupui.edu
+atuvalegacy.student.virginia.edu
+csilink.ubalt.edu
+|
+
+# Matches #
+matches [
+
+# Version detection # Powered by footer
+{ :version=>/Powered by <a href="http:\/\/www\.collegiatelink\.net">CollegiateLink<\/a> Version ([\d\.]+)<\/p><div id="links">CollegiateLink uses / },
+
+# HTML Comments + divs + span classes
+{ :text=>'</div></div></div><div class="salink"><span class="shadow"><!----></span></div></div><span class="clearDiv"><!----></span></div></div><span class="clearDiv"><!----></span><script type="text/javascript">' },
+
+]
+
+end
+
+
