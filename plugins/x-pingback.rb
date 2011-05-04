c5aa09ab3ef6b208feadc0c517e06148e6a0c98c
bcoles <bcoles@gmail.com>
2011-05-04 16:26:49 +0800

    Updated x-pingback plugin

--- plugins/x-pingback.rb
+++ plugins/x-pingback.rb
@@ -9,7 +9,10 @@ author "Brendan Coles <bcoles@gmail.com>" # 2010-10-28
 version "0.1"
 description "A pingback is one of three types of linkbacks, methods for Web authors to request notification when somebody links to one of their documents. This enables authors to keep track of who is linking to, or referring to their articles. Some weblog software, such as Movable Type, Serendipity, WordPress and Telligent Community, support automatic pingbacks where all the links in a published article can be pinged when the article is published. - More info: http://en.wikipedia.org/wiki/Pingback"
 
-# About 111664 ShodanHQ results for x-pingback @ 2010-10-28
+# ShodanHQ results as at 2010-10-28 #
+# 111,664 ShodanHQ results for x-pingback
+
+# Examples #
 examples %w|
 www.wordpress.com
 195.158.41.1
@@ -40,10 +43,10 @@ def passive
 	m=[]
 	
 	# X-Pingback HTTP Header
-	m << { :string=>@meta["x-pingback"].scan(/[\s]*([^\r^\n]*)/i) } if @meta["x-pingback"] =~ /[\s]*([^\r^\n]*)/i
+	m << { :string=>@meta["x-pingback"] } unless @meta["x-pingback"].nil?
 
+	# Return passive matches
 	m
-
 end
 
 end
