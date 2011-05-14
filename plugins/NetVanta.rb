8e97b01b918341e2e5552d2a6c69c543f1901808
bcoles <bcoles@gmail.com>
2011-05-14 20:49:55 +0930

    Updated NetVanta plugin
    The date contained a typo

--- plugins/NetVanta.rb
+++ plugins/NetVanta.rb
@@ -9,7 +9,7 @@ author "Brendan Coles <bcoles@gmail.com>" # 2011-05-14
 version "0.1"
 description "The NetVanta Series from Adtran includes: Fast Ethernet switches, Gigabit Ethernet switches, Power over Ethernet Switches, Integrated switch-routers, Fixed port Routers, Modular Routers, Multiservice Access Routers, Internet security/firewall appliance, IP Business Gateways, IP Communication Platforms, IP Phones, Integrated Access Devices, 802.11a/b/g Wireless Access Points, Network Management - Homepage: http://www.adtran.com/web/page/portal/Adtran/group/1"
 
-# ShodanHQ results as at 2011-03-13 #
+# ShodanHQ results as at 2011-05-14 #
 # 296 for netvanta
 #  86 for netvanta port:80
 
