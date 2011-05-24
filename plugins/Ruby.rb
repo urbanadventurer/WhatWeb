51faaa8cd8fa5f3463aeaabfccdf660009398330
bcoles <bcoles@gmail.com>
2011-05-24 12:02:54 +0930

    Updated Ruby plugin

--- plugins/Ruby.rb
+++ plugins/Ruby.rb
@@ -4,12 +4,19 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-05-24 #
+# Added WEBrick detection
+##
 Plugin.define "Ruby" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-26
-version "0.1"
+version "0.2"
 description "Ruby is a dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write. - homepage: http://www.ruby-lang.org/"
 
-# About 15698 ShodanHQ results for "server: ruby" @ 2010-10-26
+# ShodanHQ results as at 2011-05-24 #
+# 22,172 for server: ruby
+#    822 for WEBrick
+
+# Examples #
 examples %w|
 219.118.99.129
 12.49.88.2
@@ -18,20 +25,31 @@ examples %w|
 75.101.162.117
 93.159.43.99
 213.132.96.34
-78.159.108.70
 134.106.62.56
 212.75.96.249
+24.233.112.57
+50.17.236.165
 |
 
-# HTTP Header
+# Passive #
 def passive
 	m=[]
 
 	# Server # Version Detection
-	m << { :version=>@meta["server"].to_s.scan(/[^\r^\n]*Ruby\/([^\s^\r^\n]+)/).to_s } if @meta["server"].to_s =~ /[^\r^\n]*Ruby\/([^\s^\r^\n]+)/
+	m << { :version=>@meta["server"].to_s.scan(/Ruby\/([^\s^\/^\(]+)/).to_s } if @meta["server"].to_s =~ /Ruby\/([^\s^\/^\(]+)/
 
+	if @meta["server"] =~ /^WEBrick\/[\d\.]+ \(Ruby\/[\d\.]+\/[\d]{4}-[\d]{2}-[\d]{2}\)/
+
+		# WEBrick Version Detection
+		m << { :module=>@meta["server"].scan(/^(WEBrick\/[\d\.]+) \(Ruby\/[\d\.]+\/[\d]{4}-[\d]{2}-[\d]{2}\)/) }
+
+		# Version Detection
+		m << { :version=>@meta["server"].scan(/^WEBrick\/[\d\.]+ \(Ruby\/([\d\.]+)\/[\d]{4}-[\d]{2}-[\d]{2}\)/) }
+
+	end
+
+	# Return passive matches
 	m
-
 end
 
 end
