f3d5c13f6d45be01bc465b05643fa3b229761e24
bcoles <bcoles@gmail.com>
2011-05-30 18:23:09 +0930

    Added Avaya-IP-Office plugin

---
+++ plugins/Avaya-IP-Office.rb
@@ -0,0 +1,52 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Avaya-IP-Office.rb" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-05-30
+version "0.1"
+description "IP Office is Avaya's telephone system for small and medium enterprises. IP Office systems can be run in several modes. - Homepage: http://marketingtools.avaya.com/knowledgebase/"
+
+# ShodanHQ results as at 2011-05-30 #
+# 909 for IPOffice
+
+# Examples #
+examples %w|
+212.247.78.133
+116.90.138.70
+70.88.206.115
+195.182.128.66
+93.186.49.204
+|
+
+# Matches #
+matches [
+
+# /ssa/index.html
+{ :text=>'<html><head><title>IP Office System Status</title><style' },
+
+]
+
+# Passive #
+def passive
+	m=[]
+
+	# HTTP Server Header
+	if @meta["server"] =~ /^IPOffice\/([^\s]+)$/
+
+		# Version Detection
+		m << { :version=>@meta["server"].scan(/^IPOffice\/([^\s]+)$/) }
+
+		# Version Detection
+		m << { :version=>@body.scan(/<title>About IP Office ([^\s^<]+)<\/title>/) } if @body =~ /<title>About IP Office ([^\s^<]+)<\/title>/
+
+	end
+
+	# Return passive matches
+	m
+end
+
+end
+
