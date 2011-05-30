23f11ce0b43ea0d6da45092ca508d462cc9a13f7
bcoles <bcoles@gmail.com>
2011-05-30 18:18:53 +0800

    [~] Updated speedstreamrouter plugin

--- plugins/speedstreamrouter.rb
+++ plugins/speedstreamrouter.rb
@@ -4,13 +4,41 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.3 # 2011-05-30 # Brendan Coles <bcoles@gmail.com> #
+# Added model detection
+# Added google dorks
+# Added MAC address detection
+##
 # Version 0.2
 # removed :certainty=>100
 ##
-Plugin.define "SiemensSpeedStreamRouter" do
+Plugin.define "Siemens-SpeedStream-Router" do
 author "Andrew Horton"
-version "0.2"
+version "0.3"
 description "Siemens SpeedStream 4200 and 6520 modems are only sold in Australia through selected internet providers, which include them in their internet access packs. - More Info: http://bc.whirlpool.net.au/bc/hardware/?action=h_view&model_id=279"
+
+# Google results as at 2011-05-30 #
+# 369 for intitle:"SpeedStream Router Management Interface"
+# 297 for intitle:"SpeedStream Router Management Interface" inurl:title.htm
+
+# Dorks #
+dorks [
+'intitle:"SpeedStream Router Management Interface" inurl:title.htm'
+]
+
+# Examples #
+examples %w|
+62.41.54.81
+195.7.129.176
+83.16.17.79
+83.16.117.131
+62.25.46.184
+194.123.127.224
+83.232.161.96
+194.120.31.81
+92.64.168.40
+83.16.171.235
+|
 
 # Matches #
 matches [
@@ -22,15 +50,15 @@ matches [
 {:certainty=>50, :regexp=>/<!-- Copyright(C) [0-9]+ Siemens Subscriber Networks -->/},
 
 # pflogin.htm Frame
-{:certainty=>75, :text=>'<FRAME SRC="pflogin.htm" NAME="rightFrame"'}
+{:certainty=>75, :text=>'<FRAME SRC="pflogin.htm" NAME="rightFrame"'},
+
+# Model Detection # /summary.htm
+{ :url=>"/summary.htm", :model=>/<B>System Type:<\/B><\/TD><TD>SpeedStream ([^\-]+-Series)<\/TD>/ },
+
+# MAC Address Detection # /summary.htm
+{ :url=>"/summary.htm", :string=>/<TD ALIGN="right" WIDTH="150"><B>MAC Address:<\/B><\/TD><TD>([\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2}:[\dA-F]{2})<\/TD><\/TR><\/TABLE>/ },
 
 ]
 
 end
-
-# identifying strings
-# <TITLE>SpeedStream Router Management Interface</TITLE>
-# <!-- Copyright(C) 2003 Siemens Subscriber Networks -->
-# <FRAME SRC="pflogin.htm" NAME="rightFrame"
-
 
