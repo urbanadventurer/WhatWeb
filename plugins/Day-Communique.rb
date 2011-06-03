bd262fcdd99b688d5c4c8f5abdc4dadb206956b4
bcoles <bcoles@gmail.com>
2011-06-04 04:38:59 +0930

    [+] Added Day-Communique plugin

---
+++ plugins/Day-Communique.rb
@@ -0,0 +1,50 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Day-Communique" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-06-04
+version "0.1"
+description "Day Communique (CQ5) WCM is a web content management system based on CRX for the content repository and Apache Sling as a web framework. Day Content Repository Extreme (Day CRX) is a content repository that implements the content repository API for Java (JSR-170). It is based on the open source reference implementation Apache Jackrabbit and serves as the foundation of all other products by Day Software. - Homepage: http://www.day.com/"
+# More Info: http://en.wikipedia.org/wiki/Day_Software
+
+# ShodanHQ results as at 2011-06-04 #
+# 246 for Communique
+
+# Examples #
+examples %w|
+164.109.48.187
+63.99.16.145
+194.114.32.12
+210.164.140.31
+205.140.228.1
+164.109.50.176
+164.109.48.215
+63.99.16.75
+184.73.183.220
+|
+
+# Passive #
+def passive
+	m=[]
+
+	# HTTP Server Header
+	if @meta["server"] =~ /Communique\/([^\s]+)/
+
+		# Version Detection
+		if @meta["server"] =~ /Communique\/([^\s]+ \([^\)]+\))/
+			m << { :version=>@meta["server"].scan(/Communique\/([^\s]+ \([^\)]+\))/) }
+		else
+			m << { :version=>@meta["server"].scan(/Communique\/([^\s]+)/) }
+		end
+
+	end
+
+	# Return passive matches
+	m
+end
+
+end
+
