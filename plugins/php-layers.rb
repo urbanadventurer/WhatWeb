ec43535d072f53097253bab30d1d4f815273b475
bcoles <bcoles@gmail.com>
2011-06-04 23:26:03 +0800

    [~] Updated php-layers plugin

--- plugins/php-layers.rb
+++ plugins/php-layers.rb
@@ -10,7 +10,7 @@
 Plugin.define "PHP-Layers" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-13 
 version "0.2"
-description 'PHP Layers Menu is a hierarchical menu system to prepare "on the fly" DHTML menus relying on the PHP scripting engine for the processing of data items.  - homepage: http://phplayersmenu.sourceforge.net'
+description 'PHP Layers Menu is a hierarchical menu system to prepare "on the fly" DHTML menus relying on the PHP scripting engine for the processing of data items. - Homepage: http://phplayersmenu.sourceforge.net'
 
 # Google results as at 2010-06-13 #
 # 33 for "Powered by PHP Layers"
@@ -37,8 +37,6 @@ grasstreegallery.com
 amsit.ca/account/
 rmohseni.de
 www.sv-leipzig-ost-1858.de
-grvc.us.es/staff/imaza/treemenu.php
-www.flightdeck.pl
 |
 
 # Matches #
@@ -54,10 +52,10 @@ matches [
 { :text=>'.png" alt="Powered by PHP Layers Menu" height="31" width="88" /></a>' },
 
 # Version Detection # HTML Comment
-{ :version=>/<!-- end of menu header - PHP Layers Menu ([\d\.\-a-z]+) \(C\) [0-9]{4}-[0-9]{4} Marco Pratesi/ },
+{ :version=>/<!-- end of menu header - PHP Layers Menu ([^\s]+) \(C\) [0-9]{4}-[0-9]{4} Marco Pratesi/ },
 
 # Version Detection # JavaScript Comment
-{ :version=>/\/\/ PHP Layers Menu ([\d\.\-a-z]+) \(C\) [0-9]{4}-[0-9]{4} Marco Pratesi/ },
+{ :version=>/\/\/ PHP Layers Menu ([^\s]+) \(C\) [0-9]{4}-[0-9]{4} Marco Pratesi/ },
 
 ]
 
