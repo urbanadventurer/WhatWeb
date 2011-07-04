c2d5610243facbf91104afdf55da58a4b65c49bd
bcoles <bcoles@gmail.com>
2011-07-04 11:04:59 +0930

    [+] Added Sphinx plugin

---
+++ plugins/Sphinx.rb
@@ -0,0 +1,45 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Sphinx" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-07-04
+version "0.1"
+description "Sphinx is a tool that makes it easy to create intelligent and beautiful documentation - Homepage: http://sphinx.pocoo.org/"
+
+# Google results as at 2011-07-04 #
+# 363 for "Created using Sphinx" +Copyright
+
+# Dorks #
+dorks [
+'"Created using Sphinx" +Copyright'
+]
+
+# Examples #
+examples %w|
+sphinx.pocoo.org
+packages.python.org/an_example_pypi_project/
+docs.zope.org
+pyquery.org
+chemistry.apache.org/python/docs/
+www.geotools.org
+opendatamanual.org
+docs.pylonsproject.org
+bluesock.org/~willg/socialmedia/
+|
+
+# Matches #
+matches [
+
+# Version Detection # Created using link
+{ :version=>/Created using <a href="http:\/\/sphinx\.pocoo\.org\/">Sphinx<\/a> ([^\s]+)\./ },
+
+# div class="sphinxsidebar" and div class="sphinxsidebarwrapper"
+{ :regexp=>/<div class="sphinxsidebar">[\s]+<div class="sphinxsidebarwrapper">/ },
+
+]
+
+end
+
