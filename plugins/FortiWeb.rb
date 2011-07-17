c0d14de8a35c0ff62cac8c3cf39a9c6da089b73d
Andrew Horton <horton.nz@gmail.com>
2011-07-18 00:25:09 +1200

    synchronising with bcoles changes

---
+++ plugins/FortiWeb.rb
@@ -0,0 +1,42 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "FortiWeb" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-05-14
+version "0.1"
+description "The FortiWeb family of Web application and XML firewalls protect, balance, and accelerate Web applications, databases, and the information exchanged between them. - Homepage: http://www.fortinet.com/products/fortiweb/"
+
+# ShodanHQ results as at 2011-05-14 #
+# 985 for FortiWeb
+
+# Examples #
+examples %w|
+83.111.115.199
+209.87.230.134
+114.31.240.205
+84.15.11.78
+207.207.43.17
+212.72.22.121
+173.167.4.194
+194.14.58.35
+|
+
+# Passive #
+def passive
+	m=[]
+
+	# Version Detection # HTTP Server Header
+	m << { :version=>@meta["server"].scan(/^FortiWeb-([\d\.]+)$/) } if @meta["server"] =~ /^FortiWeb-([\d\.]+)$/
+
+	# HTTP Server Header
+	m << { :name=>"HTTP Server Header" } if @meta["server"] =~ /^FortiWeb$/
+
+	# Return passive matches
+	m
+end
+
+end
+
---
+++ plugins/SMART-SOFT-VCard-Server.rb
@@ -0,0 +1,59 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "SMART-SOFT-VCard-Server" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-05-14
+version "0.1"
+description "SMART-SOFT VCard HTTP/SSL server - Homepage: http://www.smart-soft.ru/"
+
+# ShodanHQ results as at 2011-05-14 #
+# 36 for SMART-SOFT VCard
+# 36 for Directory%20browsing%20not%20allowed
+# All results are from the Russian Federation
+
+# Examples #
+examples %w|
+89.110.40.70
+212.3.153.128
+89.110.37.181
+83.174.227.132
+89.110.36.86
+83.174.236.221
+89.110.41.18
+212.3.156.175
+89.110.36.126
+|
+
+# Matches #
+matches [
+
+# Title and h1 title with hr line
+{ :text=>"<title>Error</title></head><body><h1>403 - Directory browsing not allowed</h1><hr>SMART-SOFT VCard HTTP/SSL server" },
+
+# Version Detection # Title and h1 title with hr line
+{ :version=>/<title>Error<\/title><\/head><body><h1>403 - Directory browsing not allowed<\/h1><hr>SMART-SOFT VCard HTTP\/SSL server \(([^\)]+)\)<br>/ },
+
+# Server Name Detection # Title and h1 title with hr line
+ { :string=>/<title>Error<\/title><\/head><body><h1>403 - Directory browsing not allowed<\/h1><hr>SMART-SOFT VCard HTTP\/SSL server \([^\)]+\)<br>Server - ([\w]+)/ },
+
+]
+
+# Passive #
+def passive
+	m=[]
+
+	# Version Detection # HTTP Server Header
+	m << { :version=>@meta["server"].scan(/^SMART-SOFT VCard HTTP\/SSL server \(([^\)]+)\)$/) } if @meta["server"] =~ /^SMART-SOFT VCard HTTP\/SSL server \(([^\)]+)\)$/
+
+	# Exception header
+	m << { :name=>"Exception header" } if @meta["exception"] =~ /^Directory%20browsing%20not%20allowed$/
+
+	# Return passive matches
+	m
+end
+
+end
+
