650bb568c5ae5ce9157d3dbee2b2a42ea946fe26
Andrew Horton <horton.nz@gmail.com>
2011-08-03 22:16:37 +1200

    Added a Samsung printer plugin

---
+++ plugins/samsung-printer.rb
@@ -0,0 +1,20 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+
+Plugin.define "Samsung-Printer" do
+author "Andrew Horton"
+version "0.1"
+description "Samsung. SyncThru Web Service - Embedded Web Server"
+examples %w|   |
+
+
+matches [
+{:text=>'var debugMode = ("$$GSI_TCPIP_IP_ADDR$$".indexOf(".")' }
+]
+
+end
+
