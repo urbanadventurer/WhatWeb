a193fcb6e7889320289b26101b2bec75c9841d58
bcoles <bcoles@gmail.com>
2011-08-05 03:10:30 +0800

    [~] Updated KaiBB plugin

--- plugins/KaiBB.rb
+++ plugins/KaiBB.rb
@@ -25,6 +25,7 @@ demo.opensourcecms.com/kaibb/
 demo.caot.net/kaibb/
 bkbox.free.fr/forum/
 164.115.5.58/fetpkai/
+ap7ita.hellospace.net/forum/
 |
 
 # Matches #
@@ -35,6 +36,9 @@ matches [
 
 # Version Detection # Powered by link
 { :version=>/Powered by <a href="http:\/\/www\.kaibb\.co\.uk" class="normfont">KaiBB ([^\s^<]+)<\/a>/ },
+
+# Version Detection # Powered by link # Old versions # Seen on version 1.0.0
+{ :version=>/Powered by <a href="http:\/\/\www\.mi-dia\.co\.uk" class="normfont">KaiBB ([^\s^<]+)<\/a>/ },
 
 # Meta Description
 { :text=>'<meta name="Description" http-equiv="Description" content="Forum powered by KaiBB - Powered by KaiBB" />' },
