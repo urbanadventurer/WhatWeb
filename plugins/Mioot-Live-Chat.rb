29d9bb04f832d427ee19211af8e31ca00f92d89d
bcoles <bcoles@gmail.com>
2011-09-06 21:19:13 +0800

    [+] Added Mioot-Live-Chat plugin

---
+++ plugins/Mioot-Live-Chat.rb
@@ -0,0 +1,39 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Mioot-Live-Chat" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-09-06
+version "0.1"
+description "Mioot live chat JavaScript - Homepage: http://www.mioot.com/"
+
+# Examples #
+examples %w|
+mioot.com
+72.41.150.42
+50.6.86.252
+72.41.192.32
+50.6.25.54
+|
+
+# Matches #
+matches [
+
+# div id="miootOnOffDiv"
+#{ :text=>'<div id="miootOnOffDiv" class="online">' },
+#{ :text=>'<div id="miootOnOffDiv" style="display:inline;">' },
+#{ :text=>'<div id="miootOnOffDiv">' },
+{ :text=>'<div id="miootOnOffDiv"' },
+
+# HTML Comment
+{ :text=>'<!-- Start LIVE CHAT image tag, for details visit www.mioot.com -->' },
+
+# OnOffImage('V')
+{ :text=>'<a href="javascript:OnOffImage(\'V\')" ' },
+
+]
+
+end
+
