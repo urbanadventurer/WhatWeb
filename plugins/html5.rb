a5d5747b5e03736ccd038bc6fcdd0ece58338adb
bcoles <bcoles@gmail.com>
2011-09-11 17:18:01 +0930

    [~] Updated html5 plugin

--- plugins/html5.rb
+++ plugins/html5.rb
@@ -4,23 +4,30 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
-
 # Version 0.2
 # removed :probability
+##
 Plugin.define "HTML5" do
 author "Andrew Horton"
 version "0.2"
 description "HTML version 5, detected by the doctype declaration"
 
+# Examples #
+examples %w|
+www.rubious.co.uk
+phatdish.tumblr.com
+joachim-simon.com/index.shtml
+audio.eserver.org/tc/jqtouch/demos/ext_offline/index.html
+|
 
-examples %w| www.rubious.co.uk phatdish.tumblr.com|
 
-
+# Matches #
 matches [
-{:regexp=>/<!DOCTYPE html>/i}
+
+{ :regexp=>/<!DOCTYPE html>/i },
+{ :string=>"applicationCache", :regexp=>/<html[^>]* manifest=/ },
+
 ]
 
 end
-
 
