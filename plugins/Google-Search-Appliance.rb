117e1aa8f06be3ffd22362c3314b34adc98eb668
bcoles <bcoles@gmail.com>
2011-09-16 04:01:36 +0930

    [+] Added Google-Search-Appliance plugin

---
+++ plugins/Google-Search-Appliance.rb
@@ -0,0 +1,65 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Google-Search-Appliance" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-09-16
+version "0.1"
+description "The Google Search Appliance (GSA) is a piece of hardware that corporations install on-premise so that employees can search enterprise data. - Homepage: http://www.google.com/enterprise/search/gsa.html"
+
+# ShodanHQ results as at 2011-09-16 #
+# 4 for EnterpriseController
+# 2 for GSE EnterpriseController
+
+# Google results as at 2011-09-16 #
+# 19 for inurl:"EnterpriseController" -code
+
+# Examples #
+examples %w|
+38.106.212.158
+https://38.106.212.158
+https://38.106.212.158:8443
+38.106.212.158:7800
+38.106.212.158:8000
+189.59.8.157
+googlebox.oit.umass.edu:8000/EnterpriseController
+webcrawl2.oit.umass.edu:8000/EnterpriseController
+google.informs.org:8000/EnterpriseController
+google.mars-inc.com:8000/EnterpriseController
+https://search.ku.edu:8443/EnterpriseController
+https://gsa12.enterprisedemo-google.com:8443/EnterpriseController
+https://rummage.murdoch.edu.au:8443/EnterpriseController
+https://search5.iu.edu:8443/EnterpriseController
+https://search.life.ku.dk:8443/EnterpriseController
+https://google.umn.edu:8443/EnterpriseController
+|
+
+# Matches #
+matches [
+
+{ :text=>'<font face=arial,sans-serif size=-1>If you are an end user looking for a Google search service on your network, please ask your system administrator for the URL of the search service.<p>' },
+
+# HTTP Server Header
+{ :certainty=>75, :search=>"headers[server]", :regexp=>/^GSE$/ },
+
+# Location Header
+{ :certainty=>75, :search=>"headers[location]", :regexp=>/^\/EnterpriseController/ },
+
+# About Google Search Appliance
+{ :text=>'<tr id="TopBar"><td id="TopBarName">About Google Search Appliance</td></tr></table></td></tr></table>' },
+
+# Welcome to the Google Search Appliance!
+{ :text=>'<tr id="TopBar"><td id="TopBarName">Welcome to the Google Search Appliance!</td></tr></table></td></tr></table>' },
+
+# Welcome to the Google Mini!
+{ :text=>'<tr id="TopBar"> <td id="TopBarName">  Welcome to the Google Mini!  </td> </tr> </table>' },
+
+# Version Detection # /EnterpriseController?actionType=about
+{ :version=>/<\/font>[\s]+<br><br>[\s]+<font face="arial,sans-serif">System Version:[\s]+([^\s]+)[\s]+<\/font>[\s]+<br><br>[\s]+<font face="arial,sans-serif">Appliance ID:/ },
+
+]
+
+end
+
