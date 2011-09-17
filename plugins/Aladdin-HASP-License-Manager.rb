6a02ae2dd821470b4f8ef26ba8dbd72631eb0097
bcoles <bcoles@gmail.com>
2011-09-17 18:11:42 +0930

    [+] Added Aladdin-HASP-License-Manager plugin

---
+++ plugins/Aladdin-HASP-License-Manager.rb
@@ -0,0 +1,26 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "Aladdin-HASP-License-Manager" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-09-17
+version "0.1"
+description "Aladdin HASP License Manager - Homepage: http://www3.safenet-inc.com/support/hasp/enduser.aspx#lm"
+
+# Examples #
+examples %w|
+152.1.60.42:1947
+|
+
+# Matches #
+matches [
+
+# Version Detection # HTTP Server Header
+{ :search=>"headers[server]", :version=>/^HASP LM\/([^\s]+)$/ },
+
+]
+
+end
+
