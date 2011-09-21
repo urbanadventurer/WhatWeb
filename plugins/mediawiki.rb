67685dac87c24d799fc17ac1a174a298ef1a98e8
bcoles <bcoles@gmail.com>
2011-09-21 11:44:26 +0800

    [~] Updated mediawiki plugin

--- plugins/mediawiki.rb
+++ plugins/mediawiki.rb
@@ -4,19 +4,28 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2011-09-21 # Brendan Coles <bcoles@gmail.com>
+# Added google dorks and version detection
+##
 Plugin.define "MediaWiki" do
 author "Tonmoy Saikia"
-version "0.1"
+version "0.2"
 description "MediaWiki is a free software wiki  package written in PHP, originally for use on Wikipedia http://www.mediawiki.org/wiki/MediaWiki"
+
+# Google results as at 2011-09-21 #
+# 802 for "Powered by MediaWiki"
+# 393 for inurl:wiki/MediaWiki
+#  47 for inurl:Special:Version "This wiki is powered by MediaWiki"
 
 # Dorks #
 dorks [
-'"Powered by MediaWiki"'
+'"Powered by MediaWiki"',
+'inurl:wiki/MediaWiki',
+'inurl:Special:Version "This wiki is powered by MediaWiki"'
 ]
 
 # Examples #
 examples %w|
-http://meta.wikimedia.org/Main_Page
 http://en.wikipedia.org/wiki/Main_Page
 http://en.wiktionary.org/wiki/Main_Page
 http://en.wikibooks.org/wiki/Main_Page
@@ -35,8 +44,19 @@ http://www.boincatpoland.org/wiki/Main_Page
 
 # Matches #
 matches [
-{:text=>'alt="Powered by MediaWiki"'},
-{:ghdb=>'inurl:wiki MediaWiki'}
+
+# Logo alt text
+{ :text=>'alt="Powered by MediaWiki"' },
+
+# GHDB # URL Pattern
+{ :certainty=>75, :ghdb=>'inurl:wiki MediaWiki' },
+
+# Version Detection # Meta Generator
+{ :version=>/<meta name="generator" content="MediaWiki ([^\s^"]+)" \/>/ },
+
+# Version Detection # wiki/Special:Version
+{ :version=>/<td><a href="http:\/\/www\.mediawiki\.org\/" class="external text" rel="nofollow">MediaWiki<\/a><\/td>[\s]+<td>([^<]+)<\/td>/ },
+
 ]
 
 end
