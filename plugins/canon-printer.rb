fdfcd96f0438d0fbe02b5ed2e92bda19a47b57fc
bcoles <bcoles@gmail.com>
2011-09-21 14:38:46 +0930

    [~] Updated canon-printer plugin

--- plugins/canon-printer.rb
+++ plugins/canon-printer.rb
@@ -4,30 +4,82 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
+# Version 0.2 # 2011-09-21 # Brendan Coles <bcoles@gmail.com>
+# Updated model detection
+# Updated http server header matches
+##
 Plugin.define "Canon-Printer" do
 author "Andrew Horton"
-version "0.1"
-description "Remote UI for Canon Printers. Homepage: www.canon.com"
-examples %w| 124.186.130.214 220.227.144.157|
+version "0.2"
+description "Remote UI for Canon Printers. Homepage: http://www.canon.com/"
 
-# each model needs a different match
+# Google results as at 2011-09-21 #
+# 10 for intitle:"Remote UI" intitle:"Top Page" intitle:Canon
 
-matches [
-{:model=>/<title>Remote UI<Top Page>: (\w+)/i}, # iR2018i
-{:model=>/<title>Remote UI&lt;Top page&gt;: ([^:]+)/i, :url=>"/frame.cgi"}, # iR C3220
-{:model=>/<title>Remote UI&lt;Top page&gt;: ([^:]+)/i, :url=>"/frame.cgi?PageFlag=t_frame.tpl"}, # iR2870
-{:text=>'<img src="top_canon.gif" width="123" height="33"', :url=>"_top.htm"}
+# ShodanHQ results as at 2011-09-21 #
+# 5,739 for CANON HTTP Server Ver
+# 2,707 for KS_HTTP
+#   817 for LPC Http Server
+
+# Dorks #
+dorks [
+'intitle:"Remote UI" intitle:"Top Page" intitle:Canon'
 ]
 
-def passive
-	m=[]
-	unless @headers["server"].nil?
-		m << { :string=>"Server header" } if @headers["server"] =~ (/^Canon Http Server/i)
-		m << { :version=>@headers["server"].scan(/Canon Http Server (.*)/i) } if @headers["server"] =~ (/Canon Http Server (.*)/i)
-	end
-	m
-end
+# Examples #
+examples %w|
+220.227.144.157
+62.107.51.124
+67.112.255.4
+165.229.157.135
+200.136.0.138
+ir2018.fisica.unimo.it
+141.20.154.81
+68.181.150.73
+50.83.77.144
+99.96.0.177
+218.103.67.103
+71.178.128.205
+140.125.45.59
+211.195.85.8
+131.95.58.246
+141.45.142.50
+146.145.166.201
+129.25.10.220
+142.27.194.155
+|
+
+# Matches #
+# each model needs a different match
+matches [
+
+# iR2018i
+{:model=>/<title>Remote UI<Top Page>: (\w+)/i},
+
+# iR C3220
+{:model=>/<title>Remote UI&lt;Top page&gt;: ([^:]+)/i, :url=>"/frame.cgi"},
+
+# iR2870
+{:model=>/<title>Remote UI&lt;Top page&gt;: ([^:]+)/i, :url=>"/frame.cgi?PageFlag=t_frame.tpl"},
+
+# Model Detection # Title
+{ :model=>/<link rel="shortcut icon" type="image\/x-icon" href="G24_favicon\.ico" \/><title>Remote UI \(Top Page\) : [^:^<]* : Canon ([^\s^<]+)<\/title>/ },
+{ :model=>/<title>Canon ([^\s]+) series Network Configuration \| / },
+
+# /_top.htm # Logo HTML
+{ :url=>"/_top.htm", :text=>'<img src="top_canon.gif" width="123" height="33"' },
+
+# HTTP Server Header
+{ :search=>"headers[server]", :regexp=>/^Canon Http Server/i },
+{ :search=>"headers[server]", :version=>/^Canon Http Server (Ver)?(.*)/i, :offset=>1 },
+
+# maybe used by other printers
+{ :certainty=>75, :search=>"headers[server]", :version=>/^KS_HTTP\/([^\s]+)/ },
+
+# also used by Konica-Minolta
+{ :certainty=>25, :search=>"headers[server]", :version=>/^LPC Http Server\/V([^\s]+)/ },
+
+]
 
 end
 
