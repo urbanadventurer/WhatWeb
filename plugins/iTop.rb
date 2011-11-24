3521a4f7ec0e3f7a9c0674e545bd09680193b3d2
bcoles <bcoles@gmail.com>
2011-11-24 14:59:23 +0800

    [+] Added iTop plugin

---
+++ plugins/iTop.rb
@@ -0,0 +1,58 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "iTop" do
+author "Brendan Coles <bcoles@gmail.com>" # 2011-11-24
+version "0.1"
+description "IT Operations Portal (iTop): a complete open source, ITIL, web based service management tool including a fully customizable CMDB, a helpdesk system and a document management tool. - Homepage: http://sourceforge.net/projects/itop/"
+
+# Google results as at 2011-11-24 #
+# 14 for intitle:"iTop Login"
+#  1 for "Unable to read the configuration file" "Please check the access rights on this file"
+#  1 for "Click here to ignore this warning and continue to run iTop."
+
+# Dorks #
+dorks [
+'intitle:"iTop Login"'
+]
+
+# Examples #
+examples %w|
+itop.available-on.com/
+desarrollo.ucaldas.edu.co/itop/
+www.itilv3.kilu.de
+gvsun.net/itop/pages/UI.php
+www.yves.eti.br/itop/
+netcmdb.com/LastItop/
+genesis.e2solutions.de
+gigahertz24.de
+helpdesk.disqr.com
+seitol.com
+support.justmakeit.de
+www.boce-informatik.ch/crm/pages/
+helpdesk.ziteam.com
+i.opensolucoes.com.br
+|
+
+# Matches #
+matches [
+
+# Error # Missing Config File
+{ :text=>"<p><b>Error</b>: Unable to read the configuration file: 'config-itop.php'. Please check the access rights on this file.</p>" },
+
+# Error # Writeable Config File
+{ :text=>"<p><b>Security Warning</b>: the configuration file 'config-itop.php' should be read-only.</p><p>Please modify the access rights to this file.</p>" },
+
+# ./pages/UI.php # Login Page # Title
+{ :text=>'<title>iTop Login</title>' },
+
+# ./pages/UI.php # Login Page # Version Detection
+{ :version=>/<div id="login-logo"><a href="http:\/\/www\.combodo\.com\/itop"><img title="iTop [^\s]+ ([^\s^"]+)" src="\.\.\/images\/itop-logo/ },
+
+]
+
+end
+
