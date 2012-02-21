afd4654cae1159a0e62d4aaca319a2640000e73a
bcoles <bcoles@gmail.com>
2012-02-22 04:26:32 +1030

    [~] Updated and enabled Script plugin

--- plugins/script.rb
+++ plugins/script.rb
@@ -7,7 +7,7 @@
 Plugin.define "Script" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-10-14
 version "0.1"
-description "This plugin detects instances of script HTML elements."
+description "This plugin detects instances of script HTML elements and returns the script language/type."
 
 # Google results as at 2010-10-14 #
 # 384 for "your browser does not support javascript"
@@ -19,13 +19,18 @@ morningstarsecurity.com
 twitter.com
 wordpress.com
 www.microsoft.com
+test.com
+example.com
 |
 
 # Matches #
 matches [
 
-# Extract source URL
-{ :regexp=>/<[\s]*script[^>]+src[\s]*=[\s]*["|']?([^>^"]+)/i },
+# <script> tag
+{ :regexp=>/<script(\s|>)/i },
+
+# <script> tag # Detect type/language
+{ :string=>/<script[^>]+(language|type)\s*=\s*['"]?([^'^"^\s]+)['"]?/, :offset=>1 },
 
 ]
 
