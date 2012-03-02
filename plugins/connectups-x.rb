ee0f29542f80cb063f88b41c810adb41bb9b3a00
bcoles <bcoles@gmail.com>
2012-03-02 15:50:38 +0800

    [~] Updated ConnectUPS-X plugin

--- plugins/connectups-x.rb
+++ plugins/connectups-x.rb
@@ -4,7 +4,6 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
 Plugin.define "ConnectUPS-X" do
 author "Andrew Horton"
 version "0.1"
@@ -12,20 +11,42 @@ description "The ConnectUPS-X Web/SNMP device allows you to connect various type
 
 # Default Password is 'admin'
 
-examples %w| 85.196.1.62 201.148.53.41|
+# ShodanHQ results as at 2012-03-02 #
+# 1,162 for UPS_Server/1.0
 
+# Google results as at 2012-03-02 #
+# 2 for intitle:"ConnectUPS" inurl:"PSummary.html"
+
+# Dorks #
+dorks [
+'intitle:"ConnectUPS" inurl:"PSummary.html"'
+]
+
+# Examples #
+examples %w|
+85.196.1.62
+201.148.53.41
+208.123.192.15
+bilagsflyt.com
+|
+
+# Matches #
 matches [
 
+# HTTP Server Header
 {:text=>"UPS_Server/1.0",:search=>"headers[server]" },
-{:text=>"<TITLE>ConnectUPS Web/SNMP Card</TITLE>"},
-{:text=>"<TITLE>ConnectUPS SNMP/WEB Adapter</TITLE>"},
 
+# Title
+{ :regexp=>/<TITLE>ConnectUPS (Web\/SNMP Card|SNMP\/WEB Adapter)<\/TITLE>/ },
+
+# /PSummary.html # Model
 {:url=>"/PSummary.html", :name=>"UPS Model", :model=>/<b>UPS Model<\/b><\/td>\s+<td><table><tr><td><b>([^<]+)/},
+
+# /PSummary.html # Firmware Version Detection
 {:url=>"/PSummary.html", :name=>"UPS Firmware version", :firmware=>/<b>UPS Firmware version<\/b><\/td>\s+<td><table><tr><td><b>([^<]+)/},
 {:url=>"/PSummary.html", :name=>"Firmware Revision", :firmware=>/<b>Firmware Revision<\/b><\/td>\s+<td><table><tr><td><b>\s+([^<]+)/}
 
 ]
-
 
 end
 
