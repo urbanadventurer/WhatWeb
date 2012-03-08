8f31a4dd85cdda840615c03f9118ab676b784eae
bcoles <bcoles@gmail.com>
2012-03-09 09:31:25 +1030

    [+] Added qTranslate plugin

--- plugins/qTranslate.rb
+++ plugins/qTranslate.rb
@@ -5,42 +5,34 @@
 # http://www.morningstarsecurity.com/research/whatweb
 ##
 Plugin.define "qTranslate" do
-author "Brendan Coles <bcoles@gmail.com>" # 2011-04-19
+author "Brendan Coles <bcoles@gmail.com>" # 2012-03-09
 version "0.1"
-description "qTranslate Wordpress plugin - qTranslate makes creation of multilingual content as easy as working with a single language. - Homepage: http://www.qianqin.de/qtranslate/"
+description "qTranslate Wordpress plugin - Homepage: http://www.qianqin.de/qtranslate/"
 
-# ShodanHQ results as at 2011-04-19 #
-# 820 for qtrans_cookie_test
+# ShodanHQ results as at 2012-03-09 #
+# 1,437 for qtrans_cookie_test
 
 # Examples #
 examples %w|
-194.245.151.44
-213.160.140.102
-94.23.117.179
-86.121.209.10
-69.93.8.221
-217.23.10.101
-72.167.244.210
-178.33.3.75
-67.20.77.89
-91.121.226.169
+212.52.176.22
+72.18.153.220
+67.159.221.18
+183.181.14.59
+77.55.63.158
+175.41.255.240
+208.64.68.60
+85.128.204.30
+217.16.1.209
+142.137.247.35
 |
 
-# Passive #
-def passive
-	m=[]
+# Matches #
+matches [
 
-	# Cookie # qtrans_cookie_test=qTranslate+Cookie+Test;
-	if @headers['set-cookie'] =~ /qtrans_cookie_test=qTranslate\+Cookie\+Test;/
+# Set-Cookie # qtrans_cookie_test
+{ :search=>"headers[set-cookie]", :regexp=>/qtrans_cookie_test=[^\+]+\+Cookie\+Test;/ },
 
-		m << { :name=>"qtrans_cookie_test cookie", :string=>"Wordpress" }
-
-	end
-
-	# Return passive matches
-	m
-end
+]
 
 end
-
 
