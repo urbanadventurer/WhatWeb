2342ff52839636b106e43c02074d4c1cdddeb437
bcoles <bcoles@gmail.com>
2012-03-20 06:51:36 +0800

    [~] Updated JQuery plugin

--- plugins/jquery.rb
+++ plugins/jquery.rb
@@ -4,27 +4,86 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
-
 # Version 0.2
 # removed :probability
+##
 # Version 0.3
 # rewrite
-
+##
 Plugin.define "JQuery" do
 author "Andrew Horton"
 version "0.3"
-description "Javascript library"
+description "A fast, concise, JavaScript that simplifies how to traverse HTML documents, handle events, perform animations, and add AJAX. - Homepage: http://jquery.com/"
 
-# identifying strings
-# <script type='text/javascript' src='http://blog.core-ed.net/derek/wp-includes/js/jquery/jquery.js?ver=1.2.3'></script>
-# <script src="/javascript/jquery-1.2.6.pack.js" type="text/javascript" charset="utf-8"></script>
-# <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js" type="text/javascript"></script>
+# Examples #
+examples %w|
+www.cercaziende.it
+www.freepik.com
+www.petrol.si
+www.biology.gatech.edu
+www.dribbbler.com
+www.fct.gov.ng
+www.lyonmag.com
+www.treshna.com
+www.introitconsulting.com
+vsevnet.ru
+www.blanquerna.url.edu
+www.gruenderszene.de
+www.mof.gov.cy
+www.meessendeclercq.be
+www.anc.org.za
+admin.credoreference.com
+www.onlinesbi.com
+m.pensketruckleasing.com
+m.gopenske.com
+xerohacking.com
+www.awsum.co
+www.iamplanet.com
+gigas.com
+chirkunov.me
+www.prisa.com
+enbridge.com
+www.beta.undp.org
+citypeoplegroup.com
+www.nsi-online.it
+www.computerworld.com
+runners.es
+www.infojobs.net
+printempspoetique.free.fr
+gothic-multiplayer.com
+iamplanet.com
+www.unicef.org
+www.tdx.com.cn
+habrahabr.ru
+www.snapp.fr
+200.150.7.81
+staatsbibliothek-berlin.de
+ace-spades.com
+www.ipa.or.id
+forumblog.org
+rexgame.pl
+nyack-ny.gov
+mmac.mo.gov
+www.floridapace.gov
+magnolia.delaware.gov
+www.bethel-oh.gov
+hamiltonwetlands.scc.ca.gov
+craigcountyva.gov
+farmington-mo.gov
+lccd.scc.wa.gov
+https://aero-academy.grc.nasa.gov
+|
 
-matches [{:regexp=>/<script [^>]*jquery/},
-{:version=>/jquery.js\?ver=([0-9\.]+)['"]/}
+# Matches #
+matches [
+
+# JavaScript
+{ :regexp=>/<script [^>]*jquery/ },
+
+# JavaScript # Version Detection
+{ :version=>/jquery(\.min)?\.js\?ver=([0-9\.]+)['"]/, :offset=>1 },
+
 ]
 
 end
-
 
