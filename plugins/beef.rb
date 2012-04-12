4f429349bc139b0b02ac0ca8736219626c7b7a59
bcoles <bcoles@gmail.com>
2012-04-12 21:58:22 +0930

    [~] Updated BeEF plugin

--- plugins/beef.rb
+++ plugins/beef.rb
@@ -4,6 +4,9 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.4 # 2012-04-12 #
+# Updated matches for new versions of BeEF
+##
 # Version 0.3 # 2011-03-26 #
 # Added version detection
 ##
@@ -12,39 +15,39 @@
 ##
 Plugin.define "BeEF" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-04
-version "0.2"
-description "BeEF - Browser Exploitation Framework - homepage: http://www.bindshell.net/tools/beef/"
+version "0.4"
+description "BeEF - Browser Exploitation Framework - Homepage: http://beefproject.com/"
 
 # Examples #
 examples %w|
 stage3i.free.fr/ui/
-manyeikshramit.com/beef/ui/
 |
 
 # Matches #
 matches [
 
-# Default Logo HTML
+# PHP BeEF # Default Logo HTML
 { :text=>'<img src="../images/beef.gif" onclick="new Effect.Shake(\'sidebar\');"></div> BeEF</h1>' },
 
-# Title
+# PHP BeEF # Title
 { :certainty=>75, :text=>'<title>Browser Exploitation Framework</title>' },
 
+# PHP BeEF # Injected script
+{ :regexp=>/<script[^>]+src=['"]?[^>^="'^"]+\/hook\/beefmagic\.js\.php['"]?/, :certainty=>75, :string=>"Hook" },
+
 # Injected script
-{ :regexp=>/<script[^>]+src=['"]?[^>^="'^"]+\/hook\/beefmagic\.js\.php['"]?/, :certainty=>75, :string=>"Injected Script" },
+{ :regexp=>/<script[^>]+src=['"]?https?:\/\/[^\/]+\/hook\.js['"]?/, :certainty=>75, :string=>"Hook" },
+
+# Logo HTML
+{ :url=>"/ui/authentication", :text=>'<div id="centered"><img id="beef-logo" src="/ui/media/images/beef.png" alt="BeEF - The Browser Exploitation Framework" /></div>' },
+
+# /ui/media/images/beef.png
+{ :url=>"/ui/media/images/beef.png", :md5=>"5f8cdcd65c5c05f875710f2c10503192" },
+
+# HTTP # Server # Version Detection
+{ :search=>"headers[server]", :version=>/^BeEF ([^\s]+)$/ },
 
 ]
-
-# Passive #
-def passive
-	m=[]
-
-	# HTTP # Server # Version Detection
-	m << { :version=>@headers["server"].scan(/^BeEF ([^\s]+)$/).flatten } if @headers["server"] =~ /^BeEF ([^\s]+)$/
-
-	# Return passive matches
-	m
-end
 
 end
 
