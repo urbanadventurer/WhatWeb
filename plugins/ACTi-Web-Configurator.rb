fb6f622ad9a0c32fd00bc1b95d312f57699a0165
bcoles <bcoles@gmail.com>
2012-04-27 08:58:33 +0930

    [+] Added ACTi-Web-Configurator plugin

---
+++ plugins/ACTi-Web-Configurator.rb
@@ -0,0 +1,56 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "ACTi-Web-Configurator" do
+author "Brendan Coles <bcoles@gmail.com>" # 2012-04-27
+version "0.1"
+description "ACTi Web Configurator - camera web interface - Homepage: http://www.acti.com/home/index.asp"
+
+# Google results as at 2012-04-27 #
+# 69 for intitle:"Web Configurator" "ACTi Corporation All Rights Reserved"
+# 28 for inurl:"cgi-bin/videoconfiguration.cgi"
+
+# Dorks #
+dorks [
+'intitle:"Web Configurator" "ACTi Corporation All Rights Reserved"'
+]
+
+# Examples #
+examples %w|
+129.130.90.140
+205.200.88.223
+69.50.89.43:6004
+80.25.118.143
+65.82.253.105:8085
+46.252.52.194:86/cgi-bin/videoconfiguration.cgi
+88.35.234.202:2121/cgi-bin/videoconfiguration.cgi
+g80fw.dyndns.org:2003/cgi-bin/videoconfiguration.cgi
+193.193.81.34:3467
+cam3.kuakini.org
+www.dilido.com:8100
+web-cam-2.la.net.ua
+129.130.90.140
+www.stulik.cz:89
+www.stulik.cz:90
+www.stulik.cz:91
+|
+
+# Matches #
+matches [
+
+# Login Page # Title # Version Detection
+{ :version=>/<title>Web Configurator - Version ([^\s]*\s?v[^<]+)<\/title>/ },
+
+# Login Page # Login Form
+{ :regexp=>/<form name="frm(LOGIN|Config)" method="POST" enctype="multipart\/form-data" action="videoconfiguration\.cgi">/ },
+
+# Login Page # Year Detetcion # Footer
+{ :string=>/<tr class="layout_footer_bgcolor">\s*<td width="776"[^>]*>\s+Copyright@2003-(20[\d]{2}) ACTi Corporation All Rights Reserved/ },
+
+]
+
+end
+
