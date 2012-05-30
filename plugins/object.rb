53bb72249510a7540ed18ad6ff3c3ed5440d01b7
bcoles <bcoles@gmail.com>
2012-05-30 22:50:00 +0930

    [~] Updated object plugin

--- plugins/object.rb
+++ plugins/object.rb
@@ -4,24 +4,32 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
 Plugin.define "Object" do
 author "Andrew Horton"
 version "0.1"
 description "HTML object tag. This can be audio, video, Flash, ActiveX, Python, etc. More info: http://www.w3schools.com/tags/tag_object.asp"
-#known bug - will output ' or " - why don't the (?:re) work? http://www.ruby-doc.org/docs/ProgrammingRuby/html/language.html
-examples %w| 58.152.54.87 www.morebankusa.com|
 
+# Exmples #
+examples %w|
+58.152.54.87
+www.morebankusa.com
+mheller.com/testax.htm
+|
+
+# Matches #
 matches [
-{:regexp=>/<object/i},
-{:module=>/<object [^>]*classid=((?:')([^']+)|(?:")([^"]+)|([^\s>]+))/i, :offset=>0, :name=>"classid" }, #"
 
-{:string=>/<object [^>]*codebase=('[^']+|"[^"]+|[^\s>]+)/i, :name=>"codebase" }, #"
-{:string=>/<object [^>]*codetype=('[^']+|"[^"]+|[^\s>]+)/i, :name=>"codetype" }, #"
-{:string=>/<object [^>]*type=('[^']+|"[^"]+|[^\s>]+)/i, :name=>"type" }, #"
+{:regexp=>/<object/i},
+
+# object tag # classid
+{:module=>/<object [^>]*classid\s*=\s*['"]([^"']+)['"]/i, :offset=>0, :name=>"classid" },
+{:module=>/<object [^>]*classid\s*=\s*([^\s>'"]+)/i, :offset=>0, :name=>"classid" },
+
+# object tag
+{:string=>/<object [^>]*(codebase|codetype|type)\s*=\s*['"]([^"']+)['"]/i, :offset=>1 },
+{:string=>/<object [^>]*(codebase|codetype|type)\s*=\s*([^\s>'"]+)/i, :offset=>1 },
 
 ]
-
 
 end
 
