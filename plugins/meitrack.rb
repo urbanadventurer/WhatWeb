e1602181791988c0f0ccb930e85842dc19be44ca
bcoles <bcoles@gmail.com>
2012-06-10 15:18:59 +0930

    [~] Updated meitrack plugin

--- plugins/meitrack.rb
+++ plugins/meitrack.rb
@@ -10,8 +10,23 @@ author "Andrew Horton"
 version "0.1"
 description "MS02 GPS Tracking System from MeiTrack. Provides a web server to manage tracking of vehicles, chidren, pets, etc. The devices have plenty of features including eavesdropping, control by SMS, RFID, GPRS, panic alarms, etc. Homepage: http://www.meitrack.net"
 
+# Examples #
+examples %w|
+http://unico1.itsyourdns.com/inweb/trackerlogin.aspx
+http://www.dw123456.com/trackerlogin.aspx
+http://www.02rc.com/trackerlogin.aspx
+|
+
+# Matches #
 matches [
+
+# JavaScript
 {:text=>'var _TrackerMain_GTVTSeries = "GT Series\\\\VT Series";'},
+
+# Form HTML
+{ :text=>'<form name="form1" method="post" action="trackerlogin.aspx" id="form1">' },
+
+
 ]
 
 end
