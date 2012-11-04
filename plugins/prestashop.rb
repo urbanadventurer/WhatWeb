6cdbf2cabbe3a867150a0294eba850f7e7ba73ce
bcoles <bcoles@gmail.com>
2012-11-04 18:09:42 +1030

    [+] Added PrestaShop plugin - from Chris, NetAgence.com

---
+++ plugins/prestashop.rb
@@ -0,0 +1,62 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+############################
+# WhatWeb plugin by NetAgence.com
+############################
+Plugin.define "PrestaShop" do
+author "Chris, NetAgence.com"
+version "1.0"
+description "Ecommerce CMS Prestashop. - Homepage: http://www.prestashop.com/"
+
+# Examples #
+examples %w|
+	http://www.unemaisonenbretagne.com/
+	http://www.tartine-et-chocolat-boutique.com/
+	http://www.archiduchesse.com/
+	http://www.presqueparfait.com/
+	http://www.fatherandsons.fr/
+|
+
+# Google results as at 2011-07-19 #
+# 989 for inurl:id_cms
+
+# Dorks #
+dorks [
+'inurl:id_cms'
+]
+
+# identifying strings
+# <meta name="description" content="Boutique propuls&eacute;e par PrestaShop" />
+# <meta name="generator" content="PrestaShop" />
+# <meta name="description" content="Shop powered by PrestaShop" />
+
+# Matches #
+matches [
+
+  {:name=>"PrestaShop-powered meta generator",
+  :regexp=>/<meta name="generator"[^>]*content="PrestaShop/},
+
+  {:string=>"FR",
+  :name=>"PrestaShop-Powered meta description",
+  :regexp=>/<meta name="description"[^>]*content="par PrestaShop/},
+
+  {:string=>"EN",
+  :name=>"PrestaShop-Powered meta description",
+  :regexp=>/<meta name="description"[^>]*content="Shop powered by PrestaShop/},
+
+  {:name=>"html comment",
+  :regexp=>/<!-- \/Block permanent links module HEADER -->/}
+
+#{:text=>"<meta name=\"description\" content=\"Boutique propuls&eacute;e par PrestaShop\" />", :name=>"PrestaShop-Powered meta description (FR)"},
+#{:text=>"<meta name=\"description\" content=\"Shop powered by PrestaShop\" />", :name=>"PrestaShop-Powered meta description (EN)"},
+#{:text=>"<meta name=\"generator\" content=\"PrestaShop\" />", :name=>"PrestaShop-Powered meta generator"}
+#{ :text=>'<!-- /Block permanent links module HEADER -->' }
+
+]
+
+end
+
