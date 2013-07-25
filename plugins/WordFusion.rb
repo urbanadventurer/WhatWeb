ea1636b08ea43782f7265b2989d7390d5f9a05a4
bcoles <bcoles@gmail.com>
2013-07-25 17:23:32 +0930

    [+] Add WordFusion plugin

---
+++ plugins/WordFusion.rb
@@ -0,0 +1,37 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+Plugin.define "WordFusion" do
+author "Brendan Coles <bcoles@gmail.com>" # 2013-07-25
+version "0.1"
+description "Zumesoft WordFusion - Enterprise document assembly and automation software - Homepage: http://www.zumesoft.com/"
+
+# 1 result for intitle:"WordFusion.Web" intext:"Get Microsoft Silverlight" @ 2013-07-25
+
+# Dorks #
+dorks [
+'intitle:"WordFusion.Web" intext:"Get Microsoft Silverlight"'
+]
+
+# Examples #
+examples %w|
+tui.zumesoft.com
+documentservices.zumesoft.com
+|
+
+# Matches #
+matches [
+
+# Title
+{ :regexp=>/<head id="Head"><title>\s+WordFusion\.Web\s+\<\/title>/ },
+
+# Silverlight param
+{ :text=>'<param name="source" value="ClientBin/WordFusion.Web.xap">' },
+
+]
+
+end
+
