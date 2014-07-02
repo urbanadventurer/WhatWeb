7fdc9f99fdb9b1aaf80a498649ce4af160098dab
lins05 <linshuai2012@gmail.com>
2014-07-02 20:12:32 +0800

    detect sits powered by townnews.com BLOX

---
+++ my-plugins/blox.rb
@@ -0,0 +1,25 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+##
+
+Plugin.define "BLOX" do
+author "Shuai Lin"
+version "0.1"
+description "BLOX is a CMS from townnews.com"
+
+examples %w|
+http://www.omaha.com/
+http://thetandd.com/
+http://www.billingsgazette.com/
+|
+
+matches [
+
+{:regexp => /BLOX CMS version: [\d.]+/},
+
+]
+
+end
