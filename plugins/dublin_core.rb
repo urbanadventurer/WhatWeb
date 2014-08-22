96056de3e8b2a3761cfbe1891c1c27d3bd4066b9
Peter <vanderlaan.pm@gmail.com>
2014-08-22 09:00:58 +0200

    Some plugin website URLs separated from descriptions

--- plugins/dublin_core.rb
+++ plugins/dublin_core.rb
@@ -9,17 +9,13 @@
 Plugin.define "DublinCore" do
 author "Caleb Anderson"
 version "0.1"
-description "Dublin Core Metadata Initiative (DCMI) supports shared innovation in metadata design and best practices across a broad range of purposes and business models. Homepage: http://dublincore.org"
-
+description "Dublin Core Metadata Initiative (DCMI) supports shared innovation in metadata design and best practices across a broad range of purposes and business models."
+website "http://dublincore.org"
 
 
 matches [
-{:name=>"dublin core",
-:regexp=>/<meta [^>]*name="DC\.title"[^>]*>/i},
-
+{:name=>"dublin core", :regexp=>/<meta [^>]*name="DC\.title"[^>]*>/i},
 ]
-
-
 end
 
 
