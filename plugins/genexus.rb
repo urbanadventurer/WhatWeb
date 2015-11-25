3a80eb7088532bcbaf7b7b9dea16da84694a16a1
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2015-11-25 13:53:31 -0300

    Add structure of plugins.

--- plugins/genexus.rb
+++ plugins/genexus.rb
@@ -3,4 +3,18 @@ Plugin.define "GeneXus" do
 author "Daniel Maldonado"
 version "0.1"
 description "GeneXus is a tool for agile development."
+website "http://caceriadespammers.com.ar"
+
+matches [
+	{:text=>"<meta name=\"Generator\" content=\"GeneXus\" />"},
+]
+
+def passive
+	m=[]
+end
+
+def aggressive
+	m=[]
+end
+
 end
