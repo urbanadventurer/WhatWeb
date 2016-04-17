decdfd9c1ee0ccc24b03798ec6bdc39e636be386
urbanadventurer <horton.nz@gmail.com>
2016-04-17 23:12:36 +1000

    Added a version match to Zest-Web-Engine

--- plugins/Zest-Web-Engine.rb
+++ plugins/Zest-Web-Engine.rb
@@ -7,6 +7,7 @@
 # Version 0.2 #
 # Removed passive function and replaced with :search=>"headers[location]"
 # Added the Powered by the match
+# Added another version match
 ##
 Plugin.define "Zest-Web-Engine" do
 author "Brendan Coles <bcoles@gmail.com>" # 2011-04-08
@@ -30,7 +31,9 @@ matches [
 # Version Detection # Powered by logo+url+text
 { :version=>/Powered by the <br \/><img src='http:\/\/www.[^\/]+\/images\/zestlogo\.gif' style='border:0 ; ' alt='Zest Web Engine' \/><br \/>Zest web engine<br \/>V([\d\.]+)<br \/>/ },
 { :search=>"headers[location]", :text=>"/cgi-bin/web.asp?title"},
-{ :text=>"Powered by the <a href='http://www.zest-leisure.com/'>Zest Web Engine</a>"}
+{ :text=>"Powered by the <a href='http://www.zest-leisure.com/'>Zest Web Engine</a>"},
+{ :version=>/Powered by the <a href='http:\/\/www.zest-leisure.com\/'>Zest Web Engine<\/a>[ ]+V ([0-9\.]+)</}
+
 ]
 
 end
