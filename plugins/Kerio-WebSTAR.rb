e52c2e2ece0c16a2c198d2435433c52366e3e1bf
urbanadventurer <horton.nz@gmail.com>
2016-04-20 18:51:48 +1000

    Moved patterns from passive function to matches[]

--- plugins/Kerio-WebSTAR.rb
+++ plugins/Kerio-WebSTAR.rb
@@ -4,44 +4,29 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.2 # 2016-04-20 # Andrew Horton
+# Moved patterns from passive function to matches[]
+##
 Plugin.define "Kerio-WebSTAR" do
 author "Brendan Coles <bcoles@gmail.com>" # 2011-05-31
-version "0.1"
+version "0.2"
 description "Kerio WebSTAR (formerly 4D WebSTAR and StarNine WebSTAR) is an HTTP web server for Mac OS X with older versions running on classic Mac OS. On June 30, 2008, WebSTAR was officially discontinued. Technical support for the product ended on June 30, 2009. - More info: en.wikipedia.org/wiki/Kerio_WebSTAR"
 
 # ShodanHQ results as at 2011-05-31 #
 # 5,486 for WebSTAR
 # 3,267 for WebSTAR -4d
 
+matches [
 
+	{ :regexp=>/^(Kerio_|WebSTAR|4D_WebStar)/, :search=>"headers[server]" },
 
-# Passive #
-def passive
-	m=[]
+	# Version Detection
+	{ :version=>/^WebSTAR\/([^\s^\(]+)/, :search=>"headers[server]" },
+	{ :version=>/^Kerio_WebSTAR\/([^\s^\(]+)/, :search=>"headers[server]" },
+	{ :version=>/^WebSTAR ([\d\.]+ .+)$/, :search=>"headers[server]" },
+	{ :version=>/^4D_WebStar_[A-Z]\/([\d\.]+)/, :search=>"headers[server]" },
 
-	# HTTP Server Header
-	if @headers["server"] =~ /^(Kerio_)?WebSTAR/
-
-		# Version Detection
-		m << { :version=>@headers["server"].scan(/^WebSTAR\/([^\s^\(]+)/) } if @headers["server"] =~ /^WebSTAR\/([^\s^\(]+)/
-		m << { :version=>@headers["server"].scan(/^Kerio_WebSTAR\/([^\s^\(]+)/) } if @headers["server"] =~ /^Kerio_WebSTAR\/([^\s^\(]+)/
-		m << { :version=>@headers["server"].scan(/^WebSTAR ([\d\.]+ .+)$/) } if @headers["server"] =~ /^WebSTAR ([\d\.]+ .+)$/
-
-	end
-
-	# HTTP Server Header
-	if @headers["server"] =~ /^4D_WebStar_[A-Z]\//
-
-		m << { :name=>"HTTP Server Header" }
-
-		# Version Detection
-		m << { :version=>@headers["server"].scan(/^4D_WebStar_[A-Z]\/([\d\.]+)/) } if @headers["server"] =~ /^4D_WebStar_[A-Z]\/([\d\.]+)/
-
-	end
-
-	# Return passive matches
-	m
-end
+]
 
 end
 
