baff22c605e3bb0f96c60406a2ac12e33b5d721f
urbanadventurer <horton.nz@gmail.com>
2016-05-01 22:02:37 +1000

    Updated cookie pattern

--- plugins/phpmyadmin.rb
+++ plugins/phpmyadmin.rb
@@ -21,7 +21,7 @@
 ##
 Plugin.define "phpMyAdmin" do
 author "Brendan Coles <bcoles@gmail.com>" # 2010-06-01 
-version "0.4"
+version "0.5"
 description "phpMyAdmin is a free software tool written in PHP intended to handle the administration of MySQL over the World Wide Web."
 website "http://www.phpmyadmin.net/home_page/index.php"
 
