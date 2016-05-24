589790b3b70972fe267c663e040b668e068ced04
urbanadventurer <horton.nz@gmail.com>
2016-05-25 00:16:13 +1000

    Replaced plugin-template.rb.txt and plugin-template-advanced.rb.txt with plugin tutorials.

---
+++ my-plugins/plugin-tutorial-4.rb
@@ -0,0 +1,23 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+#
+Plugin.define "Plugin-Tutorial-4" do
+author "Your preferred name <email@address>"
+description "GenericWAF is a commercial Web Application Firewall (WAF)."
+website "http://example.com/"
+
+# Matches #
+matches [
+
+	# HTTP Server Header
+	{ :search => "headers[server]", :regexp => /genericwaf\-nginx/ },
+
+	# Cookie 
+	{ :search => "headers[set-cookie]", :regexp => /__genericwafuid/, :name=>"__genericwafuid cookie" },
+
+] 
+
+end
