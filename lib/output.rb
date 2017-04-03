3c5054a60763ae105ba870afd5cd6515e4107cdc
Oliver Bristow <evilumbrella+github@gmail.com>
2017-04-03 18:23:49 +0100

    Reduce length of targets.target column in SQL output #180

--- lib/output.rb
+++ lib/output.rb
@@ -1001,7 +1001,7 @@ class OutputSQL < Output
 	def create_tables
 		# feel free to modify this
 		@f.puts "CREATE TABLE plugins (plugin_id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL,PRIMARY KEY (plugin_id), UNIQUE (name));"
-		@f.puts "CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target varchar(900) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );"
+		@f.puts "CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target varchar(255) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );"
 		@f.puts "CREATE TABLE scans (scan_id int NOT NULL AUTO_INCREMENT, plugin_id INT NOT NULL, target_id INT NOT NULL, version varchar(255), os varchar(255), string varchar(1024), account varchar(1024), model varchar(1024), firmware varchar(1024), module varchar(1024), filepath varchar(1024), certainty varchar(10) ,PRIMARY KEY (scan_id));"
 	
 		# plugins table
