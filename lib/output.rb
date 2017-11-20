96be1c8a925e5440f571c6da1f36d2fc76e15245
Andrew Horton <horton.nz@gmail.com>
2017-11-20 20:18:48 +0100

    Explained target length
    Changed target length pull request. Included explanation for target length requirement.

--- lib/output.rb
+++ lib/output.rb
@@ -1009,8 +1009,8 @@ class OutputSQL < Output
 		
 		# feel free to modify this
 		@f.puts "CREATE TABLE plugins (plugin_id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL,PRIMARY KEY (plugin_id), UNIQUE (name));"
-		@f.puts "CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target text(#{max_target_length}) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );"
-		@f.puts "CREATE TABLE scans (scan_id int NOT NULL AUTO_INCREMENT, plugin_id INT NOT NULL, target_id INT NOT NULL, version varchar(255), os varchar(255), string text(1024), account text(1024), model text(1024), firmware text(1024), module text(1024), filepath textf(1024), certainty varchar(10) ,PRIMARY KEY (scan_id));"
+		@f.puts "CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target varchar(#{max_target_length}) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );"
+		@f.puts "CREATE TABLE scans (scan_id int NOT NULL AUTO_INCREMENT, plugin_id INT NOT NULL, target_id INT NOT NULL, version varchar(255), os varchar(255), string varchar(1024), account varchar(1024), model varchar(1024), firmware varchar(1024), module varchar(1024), filepath varchar(1024), certainty varchar(10) ,PRIMARY KEY (scan_id));"
 	
 		# plugins table
 		@f.puts "INSERT INTO plugins (name) VALUES ('Custom-Plugin');"
