9c8410262c4e5fd3b006f65d75675e4774aa0971
urbanadventurer <horton.nz@gmail.com>
2017-11-22 02:51:26 +1100

    Fixed typo in SQL output

--- lib/output.rb
+++ lib/output.rb
@@ -1106,13 +1106,13 @@ class OutputSQL < Output
 		# max_hostname_length = 253
 		# max_uri_prefix = 10 # covers https://
 		# max_url_length = 2048 # old IE limit
-		max_taget_length = 2048
+		max_target_length = 2048
 		
 		# feel free to modify this
 		@f.puts "CREATE TABLE plugins (plugin_id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL,PRIMARY KEY (plugin_id), UNIQUE (name));"
-    @f.puts "CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target varchar(#{max_target_length}) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );"    
-    @f.puts "CREATE TABLE scans (scan_id int NOT NULL AUTO_INCREMENT, config_id INT NOT NULL, plugin_id INT NOT NULL, target_id INT NOT NULL, version varchar(255), os varchar(255), string varchar(1024), account varchar(1024), model varchar(1024), firmware varchar(1024), module varchar(1024), filepath varchar(1024), certainty varchar(10) ,PRIMARY KEY (scan_id));"
-    @f.puts "CREATE TABLE request_configs (config_id int NOT NULL AUTO_INCREMENT, value TEXT NOT NULL, PRIMARY KEY (config_id) );"
+    	@f.puts "CREATE TABLE targets (target_id int NOT NULL AUTO_INCREMENT, target varchar(#{max_target_length}) NOT NULL, status varchar(10),PRIMARY KEY (target_id), UNIQUE (target, status) );"    
+    	@f.puts "CREATE TABLE scans (scan_id int NOT NULL AUTO_INCREMENT, config_id INT NOT NULL, plugin_id INT NOT NULL, target_id INT NOT NULL, version varchar(255), os varchar(255), string varchar(1024), account varchar(1024), model varchar(1024), firmware varchar(1024), module varchar(1024), filepath varchar(1024), certainty varchar(10) ,PRIMARY KEY (scan_id));"
+    	@f.puts "CREATE TABLE request_configs (config_id int NOT NULL AUTO_INCREMENT, value TEXT NOT NULL, PRIMARY KEY (config_id) );"
     
     # plugins table
 		@f.puts "INSERT INTO plugins (name) VALUES ('Custom-Plugin');"
