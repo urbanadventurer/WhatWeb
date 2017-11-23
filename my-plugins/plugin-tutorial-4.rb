7556ef20ac3b405fe5c9648caf8d6a3f7e9f4265
urbanadventurer <horton.nz@gmail.com>
2017-11-24 03:44:24 +1100

    Format spacing for tutorials

--- my-plugins/plugin-tutorial-4.rb
+++ my-plugins/plugin-tutorial-4.rb
@@ -5,21 +5,21 @@
 # http://www.morningstarsecurity.com/research/whatweb
 #
 Plugin.define do
-name "Plugin-Tutorial-4"
-author "Your preferred name <email@address>" # 1999-12-31
-version "0.1"
-description "GenericWAF is a commercial Web Application Firewall (WAF)."
-website "http://example.com/"
+	name "Plugin-Tutorial-4"
+	author "Your preferred name <email@address>" # 1999-12-31
+	version "0.1"
+	description "GenericWAF is a commercial Web Application Firewall (WAF)."
+	website "http://example.com/"
 
-# Matches #
-matches [
+	# Matches #
+	matches [
 
-	# HTTP Server Header
-	{ :search => "headers[server]", :regexp => /genericwaf\-nginx/ },
+		# HTTP Server Header
+		{ :search => "headers[server]", :regexp => /genericwaf\-nginx/ },
 
-	# Cookie 
-	{ :search => "headers[set-cookie]", :regexp => /__genericwafuid/, :name=>"__genericwafuid cookie" },
+		# Cookie 
+		{ :search => "headers[set-cookie]", :regexp => /__genericwafuid/, :name=>"__genericwafuid cookie" },
 
-] 
+	] 
 
 end
