82ce168791c765e2d95ffc555e144c89e0bd6a6a
urbanadventurer <horton.nz@gmail.com>
2017-11-27 06:50:52 +1100

    Version update from Bhavin Senjaliya and Andrew Horton

--- plugins/blox.rb
+++ plugins/blox.rb
@@ -4,16 +4,24 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-
+# Version 0.3 # 2017-11-27 # Andrew Horton
+# Added website and updated description
+##
+# Version 0.2 # 2016-08-19 # Bhavin Senjaliya
+# Add cookie
+##
 Plugin.define do
 name "BLOX"
 author "Shuai Lin"
-version "0.1"
-description "BLOX is a CMS from townnews.com"
+version "0.2"
+description "BLOX is a cloud based CMS"
+website "https://www.townnews365.com./"
 
 matches [
 
-{:regexp => /BLOX CMS version: [\d.]+/},
+	{:regexp => /BLOX CMS version: [\d.]+/},
+
+	{ :search => "headers[set-cookie]", :regexp => /^TNNoMobile/, :name=>"TNNoMobile cookie" },
 
 ]
 
