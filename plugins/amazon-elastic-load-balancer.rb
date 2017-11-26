f7f91c059353003d710ae81b25bd81b84e804f86
urbanadventurer <horton.nz@gmail.com>
2017-11-27 03:00:51 +1100

    Plugin by Bhavin Senjaliya. Version bump to 0.2 by Andrew. Update website.

---
+++ plugins/amazon-elastic-load-balancer.rb
@@ -0,0 +1,26 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+## 
+# Version 0.2 # 2017-11-27 # Andrew Horton
+# Update Website. Remove comments
+##
+
+Plugin.define do
+name "new-AmazonElasticLoadBalancer"
+author "Bhavin Senjaliya <bhavin.senjaliya@gmail.com>" # 2016-08-19
+version "0.2"
+description "Amazon Elastic Load Balancer"
+website "https://aws.amazon.com/elasticloadbalancing/"
+
+# Matches #
+matches [
+
+	# Cookie 
+	{ :search => "headers[set-cookie]", :regexp => /AWSELB/, :name=>"AWSELB cookie" },
+
+] 
+
+end
