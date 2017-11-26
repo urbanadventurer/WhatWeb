7d97d4547594f6a2a3b1afe895558d070305c56b
urbanadventurer <horton.nz@gmail.com>
2017-11-27 06:05:33 +1100

    Version 0.3 Bhavin Senjaliya

--- plugins/citrix-netscaler.rb
+++ plugins/citrix-netscaler.rb
@@ -4,6 +4,9 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
+# Version 0.3 # 2017-11-27 # Bhavin Senjaliya
+# Add NSC_ cookie
+##
 # Version 0.2 # 2016-04-23 # Andrew Horton
 # Moved patterns from passive function to matches[]
 ##
@@ -11,7 +14,7 @@
 Plugin.define do
 name "Citrix-NetScaler"
 author "Aung Khant <http://yehg.net/>" # 2011-02-04
-version "0.2"
+version "0.3"
 description "Citrix NetScaler  - http://www.citrix.com/netscaler"
 
 matches [
@@ -19,6 +22,8 @@ matches [
 	{ :regexp=>/NS\-CACHE/, :search=>"headers[via]" },
 
 	{ :version=>/NS\-CACHE\-(\d{1,4}\.\d{1,4}):/, :search=>"headers[via]" },	
+
+	{ :search => "headers[set-cookie]", :regexp => /^NSC_/, :name=>"NSC_ cookie" },
 
 ]
 
