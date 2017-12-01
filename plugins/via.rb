1c2b594bfe339c0b27fa46a172b0e3d9bdf321aa
urbanadventurer <horton.nz@gmail.com>
2017-12-01 11:35:36 +1100

    Updated Via-Proxy plugin

--- plugins/via.rb
+++ plugins/via.rb
@@ -9,25 +9,17 @@ name "Via-Proxy"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2010-10-14
   # v0.2 # 2011-02-16 # Removed redundant @headers["Via"] match. 
+  "Andrew Horton", #2017-12-01 # Replaced passive function with match
 ]
-version "0.2"
+version "0.3"
 description "This plugin extracts the proxy server details from the Via param of the HTTP header."
 
 # ShodanHQ results as at 2010-10-14 #
 # 131,646 for "Via: "
 
-
-
-# Passive #
-passive do
-	m=[]
-
-	# Via HTTP header
-	m << { :string=>@headers["via"].to_s } unless @headers["via"].nil?
-
-	# Return passive matches
-	m
-end
+matches [
+	{ :search => "headers[via]", :string => /^.*$/ }
+]
 
 end
 
