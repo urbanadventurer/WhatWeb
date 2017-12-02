07a232aaa5a0520c59b7de5883dfe3a4d1d9058b
Brendan Coles <bcoles@gmail.com>
2017-12-02 07:01:00 +0000

    Re-indent with 2 spaces instead of tab

--- lib/colour.rb
+++ lib/colour.rb
@@ -14,21 +14,20 @@ You should have received a copy of the GNU General Public License along with Wha
 
 
 def colorize(text, color_code)   
-"#{color_code}#{text}\e[0m"
+  "#{color_code}#{text}\e[0m"
 end
 
-def red(text); 		colorize(text, "\e[1m\e[31m"); end
-def dark_red(text); 	colorize(text, "\e[31m"); end
-def green(text); 	colorize(text, "\e[1m\e[32m"); end     
-def dark_green(text); 	colorize(text, "\e[32m"); end     
-def yellow(text); 	colorize(text, "\e[1m\e[33m"); end    
-def dark_yellow(text); 	colorize(text, "\e[33m"); end    
-def blue(text); 	colorize(text, "\e[1m\e[34m"); end     
-def dark_blue(text); 	colorize(text, "\e[34m"); end     
-def magenta(text); 	colorize(text, "\e[1m\e[35m"); end
-def dark_magenta(text); colorize(text, "\e[35m"); end
-def cyan(text); 	colorize(text, "\e[1m\e[36m"); end
-def dark_cyan(text); 	colorize(text, "\e[36m"); end
-def white(text); 	colorize(text, "\e[1m\e[37m"); end
-def grey(text); 	colorize(text, "\e[37m"); end
-
+def red(text);           colorize(text, "\e[1m\e[31m"); end
+def dark_red(text);      colorize(text, "\e[31m"); end
+def green(text);         colorize(text, "\e[1m\e[32m"); end
+def dark_green(text);    colorize(text, "\e[32m"); end
+def yellow(text);        colorize(text, "\e[1m\e[33m"); end
+def dark_yellow(text);   colorize(text, "\e[33m"); end
+def blue(text);          colorize(text, "\e[1m\e[34m"); end
+def dark_blue(text);     colorize(text, "\e[34m"); end
+def magenta(text);       colorize(text, "\e[1m\e[35m"); end
+def dark_magenta(text);  colorize(text, "\e[35m"); end
+def cyan(text);          colorize(text, "\e[1m\e[36m"); end
+def dark_cyan(text);     colorize(text, "\e[36m"); end
+def white(text);         colorize(text, "\e[1m\e[37m"); end
+def grey(text);          colorize(text, "\e[37m"); end
