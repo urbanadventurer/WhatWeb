595642e08ec74102b01bd7f26461c94cf4b35263
Brendan Coles <bcoles@gmail.com>
2017-12-02 18:29:07 +0000

    Move word_wrap method to OutputHelper module

--- lib/output_helper.rb
+++ lib/output_helper.rb
@@ -15,8 +15,15 @@
 # You should have received a copy of the GNU General Public License
 # along with WhatWeb.  If not, see <http://www.gnu.org/licenses/>.
 
+#
+# Output helper methods
+#
 module OutputHelper 
-
+  #
+  # Takes an integer of certainty (between 1 - 100)
+  #
+  # returns String a word representing the certainty
+  #
   def certainty_to_words(p)
     case p
     when 0..49
@@ -28,4 +35,35 @@ module OutputHelper
     end
   end
 
+  #
+  # Word wraps a string. Used by plugin_info and OutputVerbose.
+  #
+  # returns Array an array of lines.
+  #
+  def word_wrap(s, width = 10)
+    ret = []
+    line = ''
+    s.split.map do |x|
+      word = x
+      if line.size + x.size + 1 <= width
+        line += x + ' '
+      else
+        if word.size > width
+          ret << line
+          line = ''
+          w = word.clone
+          while w.size > width
+            ret << w[0..(width - 1)]
+            w = w[width.to_i..-1]
+          end
+          ret << w unless w.empty?
+        else
+          ret << line
+          line = x + ' '
+        end
+      end
+    end
+    ret << line unless line.empty?
+    ret
+  end
 end
