d65137721ed8c49a3c391291b77f3e89f69ffac8
urbanadventurer <horton.nz@gmail.com>
2017-12-04 08:09:41 +1100

    Change Output to Logging in WhatWeb:Parser

--- lib/whatweb/parser.rb
+++ lib/whatweb/parser.rb
@@ -17,17 +17,17 @@
 
 module WhatWeb
 class Parser
-  def parse_results(target, result, output_list, use_custom_grep_plugin)
+  def parse_results(target, result, logging_list, use_custom_grep_plugin)
     # results
-    output_list.each do |output|
+    logging_list.each do |log|
       begin
-        # Hide output if Grep plugin did not match
+        # Hide log if Grep plugin did not match
         if use_custom_grep_plugin
           if result.map { |plugin_name, _plugin_result| plugin_name }.include? 'Grep'
-            output.out(target, target.status, result)
+            log.out(target, target.status, result)
           end
         else
-          output.out(target, target.status, result)
+          log.out(target, target.status, result)
         end
       rescue => err
         error("ERROR Logging failed: #{target} - #{err}")
