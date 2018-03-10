46742c9cb6fff6336f7a0f1ae6e1b1dc66eac4dc
Janosch Maier <janosch@crashtest-security.com>
2018-03-10 13:33:53 +0100

    Revert "feat: detect jquery when version appears in filepath"
    This reverts commit 5caee9bc8d286ecd6472855064d6e76985193aa9.

--- plugins/jquery.rb
+++ plugins/jquery.rb
@@ -29,7 +29,6 @@ matches [
 
 # JavaScript # Version Detection
 { :version=>/jquery(\.min)?\.js\?ver=([0-9\.]+)['"]/, :offset=>1 },
-{ :version=>/jquery\/([0-9\.]+)\/jquery(\.min)?\.js/, :offset=>0 },
 { :version=>/jquery-([0-9\.]+)(\.min)?\.js/, :offset=>0 }
 
 ]
