4b104772a78e2698c0139fb312920d8ce18f4bfe
urbanadventurer <horton.nz@gmail.com>
2018-06-10 02:39:46 +1000

    Add test plugin for deprecated plugin format

---
+++ test/plugins/deprecated-plugin-format.rb
@@ -0,0 +1,11 @@
+##
+# This file is part of WhatWeb and may be subject to
+# redistribution and commercial restrictions. Please see the WhatWeb
+# web site for more information on licensing and terms of use.
+# http://www.morningstarsecurity.com/research/whatweb
+#
+Plugin.define "Deprecated-Plugin-Format" do
+author "Andrew Horton"
+description "Tests the deprecated plugin format for WhatWeb version < 0.5.0."
+
+end
