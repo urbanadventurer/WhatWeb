3531d16b04a25035eaf4ccf685f7ba4bf623c8e7
Andrew Horton <andrew.horton@domaindefender.com>
2018-11-10 09:25:55 +1100

    Updated Django plugin. Thanks @zr000

--- plugins/django.rb
+++ plugins/django.rb
@@ -9,8 +9,10 @@ name "Django"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2012-02-27
   "Bhavin Senjaliya <bhavin.senjaliya@gmail.com>", # v0.2 # 2016-08-19 # Added and updated cookies. 
+  "zr000", # v0.3 # 2018-10-10 # Added another [\s]+ to <code>DEBUG = True<\/code> in[\s]+your
 ]
-version "0.2"
+
+version "0.3"
 description "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design."
 website "https://www.djangoproject.com/"
 
@@ -25,7 +27,7 @@ matches [
 { :certainty=>25, :search=>"headers[server]", :regexp=>/^WSGIServer\/[^\s]+ Python\/[^\s]+$/ },
 
 # Debug message # source: django/views/debug.py
-{ :regexp=>/<div id="explanation">[\s]+<p>[\s]+You're seeing this error because you have <code>DEBUG = True<\/code> in your[\s]+Django settings file/ },
+{ :regexp=>/<div id="explanation">[\s]+<p>[\s]+You're seeing this error because you have <code>DEBUG = True<\/code> in[\s]+your[\s]+Django settings file/ },
 
 # 404 Page # Debug message # source: django/views/debug.py
 { :url=>"/doesnotexist123highwaytothedangerzone", :string=>/<p>[\s]+Using the URLconf defined in <code>([^\.^\s]+)\.urls<\/code>,[\s]+Django tried these URL patterns, in this order:/ },
