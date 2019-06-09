4205a487074dec4d2f6fef17e7cce3bc10005c24
Andrew Horton <andrew.horton@domaindefender.com>
2019-06-10 06:44:28 +1000

    Fix warning: character class has duplicated range

--- plugins/microsoft-wince.rb
+++ plugins/microsoft-wince.rb
@@ -9,8 +9,9 @@ name "Microsoft-WinCE"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2011-03-13
   "Andrew Horton", # v0.2 # 2016-04-21 # Moved patterns from passive function to matches[]. 
+  "Andrew Horton", # v0.3 # 2019-07-10 # Fix warning: character class has duplicated range
 ]
-version "0.2"
+version "0.3"
 description "Microsoft Windows CE HTTP Server"
 website "http://msdn.microsoft.com/en-us/library/ms834461.aspx"
 
@@ -21,7 +22,7 @@ matches [
 	# HTTP Server Header
 	{ :regexp=>/^Microsoft-WinCE/, :search=>"headers[server]" },
 	# Version Detection # HTTP Server Header
-	{ :version=>/^Microsoft-WinCE\/([^\s^\r^\n]+)$/, :search=>"headers[server]" },
+	{ :version=>/^Microsoft-WinCE\/([\S]+)/, :search=>"headers[server]" },
 
 ]
 
