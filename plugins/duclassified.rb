ac1d3b540b05ae7780be4296db4d871975e7b8f2
urbanadventurer <horton.nz@gmail.com>
2019-06-10 06:21:39 +1000

    Fix warning: character class has duplicated range

--- plugins/duclassified.rb
+++ plugins/duclassified.rb
@@ -8,8 +8,9 @@ Plugin.define do
 name "DUclassified"
 authors [
   "Tonmoy Saikia",
+  "Andrew Horton", # v0.2 # 2019-07-10 # Fix warning: character class has duplicated range
 ]
-version "0.1"
+version "0.2"
 description "DUclassified is a free Classified Ad Management system."
 website "http://www.duware.com/"
 
@@ -18,17 +19,10 @@ dorks [
 '"powered by DUclassified" intitle:DUclassified'
 ]
 
-
-
-#<title>DUclassified</title>
-#:<title>DUclassified 4.0</title>
-# <link href="assets/DUclassified.css" rel="stylesheet" type="text/css"> 
-
-
 matches [
 {:certainty=>75, :ghdb=>'"powered by DUclassified" intitle:DUclassified' },
-{:name=>"default title", :regexp=>/<title>DUclassified[\s\d\.\d]*<\/title>/},
-{:name=>"assets/DUclassified.css", :regexp=>/<link[^>]href="[^"]*assets\/DUclassified.css"[^>]+>/ }#"
+{:name=>"default title", :regexp=>/<title>DUclassified[\s\d\.]*<\/title>/},
+{:name=>"assets/DUclassified.css", :regexp=>/<link[^>]href="[^"]*assets\/DUclassified.css"[^>]+>/ }
 
 ]
 
