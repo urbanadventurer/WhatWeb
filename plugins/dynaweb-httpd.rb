87538b3a51a74d2c82a45e1e1ac6bcefb64ccdde
urbanadventurer <horton.nz@gmail.com>
2019-06-10 06:17:04 +1000

    Fix warning: character class has duplicated range

--- plugins/dynaweb-httpd.rb
+++ plugins/dynaweb-httpd.rb
@@ -8,8 +8,9 @@ Plugin.define do
 name "DynaWeb-httpd"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2011-08-07
+  "Andrew Horton", # 2019-07-10 # Fix warning: character class has duplicated range
 ]
-version "0.1"
+version "0.2"
 description "DynaWeb web server. Ships with Sun Microsystem's AnswerBook"
 website "http://www.oracle.com/"
 
@@ -26,7 +27,7 @@ passive do
 	m=[]
 
 	# Version Detection # HTTP Server Header
-	if @headers["server"] =~ /^dwhttpd\/([^\s]+) \(([^\s^;^\)]+; [^\s^\)]+)\)$/
+	if @headers["server"] =~ /^dwhttpd\/([^\s]+) \(([^\s;\)]+; [^\s\)]+)\)$/
 		m << { :version=>"#{$1}" }
 		m << { :string=>"#{$2}" }
 	end
