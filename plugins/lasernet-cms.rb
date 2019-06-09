6f473272301b11324aa4758d44f15fd3defe45df
Andrew Horton <andrew.horton@domaindefender.com>
2019-06-10 06:42:07 +1000

    Fix warning: character class has duplicated range

--- plugins/lasernet-cms.rb
+++ plugins/lasernet-cms.rb
@@ -8,8 +8,9 @@ Plugin.define do
 name "Lasernet-CMS"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2011-08-10
+  "Andrew Horton", # v0.2 # 2019-07-10 # Fix warning: character class has duplicated range
 ]
-version "0.1"
+version "0.2"
 description "Lasernet CMS"
 website "http://lasernet.gr/cms.php"
 
@@ -27,7 +28,7 @@ dorks [
 matches [
 
 # table
-{ :regexp=>/<table width="100%" height="1[\d]{2}" border="0" cellpadding="0" cellspacing="0" background="images\/headers\/[^"^\/^>]* ">/ },
+{ :regexp=>/<table width="100%" height="1[\d]{2}" border="0" cellpadding="0" cellspacing="0" background="images\/headers\/[^"\/>]* ">/ },
 
 # Powered by text
 { :regexp=>/<font size="1" face="Verdana, Arial, Helvetica, sans-serif">Powered by<\/font><\/font>[\s]+<a href="http:\/\/lasernet\.gr"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">Lasernet<\/font><\/a>[\s]+<\/td>/ },
