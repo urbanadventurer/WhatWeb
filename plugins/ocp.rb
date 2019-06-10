e79380f31c0fa3308c2ac9a0ac90b18e89e79fd5
Andrew Horton <andrew.horton@domaindefender.com>
2019-06-10 11:56:52 +1000

    Added website field.

--- plugins/ocp.rb
+++ plugins/ocp.rb
@@ -3,12 +3,14 @@ Plugin.define do
 name "Open Commerce Platform"
 authors [
   "Claudio Salazar <csalazar at spect dot cl>",
+  "Andrew Horton", # v0.2 # 2019-07-10 # Added website field.
 ]
-version "0.1"
-description "Open Commerce Platform™ (OCP) is the industry-leading direct commerce
-  platform for business-to-consumer, business-to-business, mobile, order management,
-  and call center commerce solutions -
-  Homepage: http://www.micros-fidelio.eu/en/Solutions/Products-N-Z/OCP-Open-Commerce-Platform.aspx"
+version "0.2"
+description "Open Commerce Platform™ (OCP) is the industry-leading direct commerce platform for business-to-consumer, business-to-business, mobile, order management, and call center commerce solutions."
+website "http://www.micros-fidelio.eu/en/Solutions/Products-N-Z/OCP-Open-Commerce-Platform.aspx"
 
-matches [{:regexp=>/\/js\/ocpsdk\/base.js/ }]
+matches [
+	{:regexp=>/\/js\/ocpsdk\/base.js/ }
+]
+
 end
