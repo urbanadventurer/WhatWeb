d83e9c38ae976ffb8f07f0ab6d106c43b6ce3f88
Andrew Horton <andrew.horton@domaindefender.com>
2019-06-10 12:22:25 +1000

    Added website field

--- plugins/alter-native.rb
+++ plugins/alter-native.rb
@@ -9,9 +9,11 @@ name "Alter-Native"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2010-12-28
   "Andrew Horton", # v0.2 # 2016-04-19 # Replaced passive function with match for server header. 
+  # Andrew Horton # v0.3 # 2019-07-10 # Added website field.
 ]
-version "0.2"
-description "Alter-Native - IBM UniVerse database and Client/Server solutions provider. SPID Product provider, creating dynamic Web servers using database links on Unix systems. - Homepage [Offline] : http://www.alternative.fr/"
+version "0.3"
+description "Alter-Native - IBM UniVerse database and Client/Server solutions provider. SPID Product provider, creating dynamic Web servers using database links on Unix systems. - Homepage [Offline]"
+website "http://www.alternative.fr/"
 
 # ShodanHQ results as at 2010-12-28 #
 # 20 for SPID HTTP Server -apache -IIS -nginx
