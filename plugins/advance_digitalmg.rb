51488c1575462e56648b1a3335718f8ab2236a83
urbanadventurer <horton.nz@gmail.com>
2019-06-10 12:21:19 +1000

    Added website field

--- plugins/advance_digitalmg.rb
+++ plugins/advance_digitalmg.rb
@@ -3,10 +3,11 @@ Plugin.define do
 name "AdvanceDigital"
 authors [
   "Elias Dorneles",
+  "Andrew Horton", # v0.2 # 2019-07-10 # Added website field.
 ]
-version "0.1"
+version "0.2"
 description "Identifies a news website by Advance Digital MG network"
-
+website "https://www.advancelocal.com/"
 matches [
   {
     :name => "link to user agreement that it's present in the footer",
