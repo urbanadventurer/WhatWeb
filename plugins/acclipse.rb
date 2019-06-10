cf09a5b668b56c7e5893804ca6a3aa633f50617f
urbanadventurer <horton.nz@gmail.com>
2019-06-10 12:22:54 +1000

    Added website field

--- plugins/acclipse.rb
+++ plugins/acclipse.rb
@@ -8,10 +8,11 @@ Plugin.define do
 name "Acclipse"
 authors [
   "Andrew Horton",
+  # Andrew Horton # v0.3 # 2019-07-10 # Added website field.
 ]
-version "0.2"
-description "Acclipse is a web CMS found mainly in New Zealand and Australia. It is popular with accountants. Websites: www.acclipse.co.nz & www.acclipse.com.au"
-
+version "0.3"
+description "Acclipse is a web CMS found mainly in New Zealand and Australia. It is popular with accountants."
+website "www.acclipse.com"
 
 
 matches [
@@ -27,25 +28,6 @@ matches [
 {:name=>"https://secure.acclipse.com link",
 :regexp=>/https:\/\/secure\.acclipse(web)?\.com\/[^\/]*\/index\.cfm/}
 ]
-
-
-#
-# <a href="http://www.acclipse.co.nz/" target="_blank">Software solutions for accountants</a>
-# <a href="http://www.acclipse.co.nz" target="_blank" class="poweredbylink">Software solutions for accountants</a>
-# <a href="http://www.acclipseweb.co.nz" target="_blank" class="poweredbylink">Web design New Zealand</a>
-# <A href="http://www.acclipse.co.nz/" target=_blank></A>
-# <a href="http://www.acclipseweb.co.nz" target="_blank" class="poweredbylink">Web design Australia</a>
-# <a href="http://www.acclipse.co.nz/for_Businesses" target="_blank">Web design New Zealand</a> 
-# <a href="http://www.acclipse.com.au/" target="_blank">Web design Australia</a>
-# Website powered by <A href="http://www.acclipse.com.au" target=_blank>Acclipse</A>
-
-#  <a href="https://secure.acclipse.com/hphanna.co.nz/index.cfm/Client_Login.html
-# <a href="https://secure.acclipseweb.com/btp.co.nz/index.cfm/Client_Login.html
-# url=https://secure.acclipseweb.com/barkers.co.nz/index.cfm/PRODUCTS
-#
-# can't detect testsites: bentleys.com.au, jacksonandjackson.co.nz, glynmorrisandco.com.au
-# 
-# ways to be sure -- license key in milonic_src.js, maybe some path
 
 
 end
