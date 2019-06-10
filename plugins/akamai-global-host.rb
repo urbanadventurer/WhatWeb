602dd179ac82ae281522c7e93409451b1d9fe02a
urbanadventurer <horton.nz@gmail.com>
2019-06-10 12:21:51 +1000

    Added website field

--- plugins/akamai-global-host.rb
+++ plugins/akamai-global-host.rb
@@ -9,13 +9,13 @@ name "Akamai-Global-Host"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2010-10-26
   # v0.2 # 2011-01-07 # Updated HTTP Server Header match. 
+  "Andrew Horton", # v0.3 # 2019-07-10 # Added website field.
 ]
-version "0.2"
+version "0.3"
 description "Akamai-Global-Host HTTPd"
+website "https://www.akamai.com"
 
 # About 624,447 ShodanHQ results for "server: AkamaiGHost" @ 2010-10-26
-
-
 
 # Matches #
 matches [
