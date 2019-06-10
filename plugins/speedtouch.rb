3d4e59f9f9dad9166e69a9838425c4a0f4581154
Andrew Horton <andrew.horton@domaindefender.com>
2019-06-10 11:56:25 +1000

    Added website field.

--- plugins/speedtouch.rb
+++ plugins/speedtouch.rb
@@ -9,10 +9,11 @@ name "SpeedTouch"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2011-05-30
   "Andrew Horton", # v0.2 # 2016-04-23 # Moved patterns from passive function to matches[]. 
+  # Andrew Horton # v0.3 # 2019-07-10 # Added website field.
 ]
-version "0.2"
+version "0.3"
 description "SpeedTouch is the brand name of a line of networking equipment produced by Alcatel and Technicolor SA. Before 27 January 2010 Technicolor was known as Thomson SA. Under the SpeedTouch name Alcatel and Technicolor retail a variety of equipment including ADSL and ADSL2+ modems, residential gateways, wireless access equipment, VoIP handsets and SHDSL interconnect equipment. They are a major brand in home and business networking products. - More Info: http://en.wikipedia.org/wiki/SpeedTouch"
-# Homepage: http://www.thomsontelecom.com.au/speedtouch/support.htm
+website "http://www.thomsontelecom.com.au/speedtouch/support.htm"
 
 # ShodanHQ results as at 2011-05-30 #
 # 435,213 for SpeedTouch -SIP http
@@ -22,7 +23,6 @@ matches [
 
 # Default Favicon
 { :url=>"/favicon.ico", :md5=>"befcded36aec1e59ea624582fcb3225c" },
-
 
 { :regexp=>/(Basic|Digest) realm="SpeedTouch/, :search=>"headers[www-authenticate]", :name=>"WWW-Authenticate realm" },
 
