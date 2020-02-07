1556cf33920b6b0b696a79520a80d7a3a85c72c8
Janosch Maier <janosch@crashtest-security.com>
2020-02-07 10:25:33 +0100

    Improve bootstrap plugin by adding aggressive detection based on css and js files that leak the version number

--- plugins/bootstrap.rb
+++ plugins/bootstrap.rb
@@ -7,7 +7,8 @@
 Plugin.define do
 	name "Bootstrap"
 	authors [
-		"Janosch Maier @Phylu", # v0.1 # 2020-02-05 # Created plugin
+        "Janosch Maier @Phylu", # v0.1 # 2020-02-05 # Created plugin
+                                # v0.2 # 2020-02-07 # Added aggressive version detection
 	]
 	version "0.1"
 	description "Bootstrap is an open source toolkit for developing with HTML, CSS, and JS. "
@@ -30,7 +31,19 @@ Plugin.define do
     # CSS # Version Detection
     { :version=>/bootstrap(\.min)?\.css\?ver=([0-9\.]+)['"]/, :offset=>1 },
     { :version=>/bootstrap\/([0-9\.]+)(\/css)?\/bootstrap(\.min)?\.css/, :offset=>0 },
-    { :version=>/bootstrap-([0-9\.]+)(\.min)?\.css/, :offset=>0 }
+    { :version=>/bootstrap-([0-9\.]+)(\.min)?\.css/, :offset=>0 },
+
+    # JavaScript # Version Detection # Agressive
+    { :url=>"bootstrap/js/bootstrap.js", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+    { :url=>"bootstrap/js/bootstrap.bundle.js", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+    { :url=>"bootstrap/js/bootstrap.min.js", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+    { :url=>"bootstrap/js/bootstrap.bundle.min.js", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+
+    # CSS # Version Detection # Agressive
+    { :url=>"bootstrap/css/bootstrap.css", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+    { :url=>"bootstrap/css/bootstrap.bundle.css", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+    { :url=>"bootstrap/css/bootstrap.min.css", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 },
+    { :url=>"bootstrap/css/bootstrap.bundle.min.css", :version=>/Bootstrap ([vV])?([0-9\.]+)/, :offset=>1 }
 
 	] 
 
