dc3adc9a0a05705d090976c5207f9e3cf53e6b65
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2020-04-04 21:41:43 -0300

    Updating PHP-Slim plugin

--- plugins/php-slim.rb
+++ plugins/php-slim.rb
@@ -6,33 +6,32 @@
 ##
 # Version 0.1 # 2020-04-04 # Marcelo Gimenes
 ##
-Plugin.define "PHP-Slim" do
-  author "Marcelo Gimenes <marcelo.gimenes.oliveira@gmail.com>" # 2020-04-04
+Plugin.define do
+  name "PHP-Slim"
+  authors [
+    "Marcelo Gimenes <marcelo.gimenes.oliveira@gmail.com>"
+  ]
   version "0.1"
   description "Slim Framework"
   website "https://www.slimframework.com/"
 
   matches [
     { :name => 'Slim < 4.0', 
-      :status => 404, 
       :text => "to ensure your URL is spelled correctly. If all else fails, you can" },
     { :name => 'Slim > 4.0', 
-      :status => 404, 
       :text => "The requested resource could not be found. Please verify the URI and try again." }
   ] 
 
-  def random_string(length=32)
-    # should probably be moved somewhere else to be used in other plugins
-    (1..length).map{|i| ('a'..'z').to_a[rand(26)]}.join
-  end
+  aggressive do
+    random_string = (1..32).map{|i| ('a'..'z').to_a[rand(26)]}.join
 
-  ##
-  # get a random page to check for default 404 slim page
-  ##
-  def from_404(target, redirections=0)
+    target = @base_uri.dup
+    target.path = "/#{random_string}"
+
     info = []
+    redirections = 0
     begin
-      status, url, ip, body, headers = open_target(target)
+      status, url, ip, body, headers = open_target(target.to_s)
 
       if status == 301 && redirections < 3 
         info = from_404(headers['location'], redirections+1)
@@ -52,12 +51,6 @@ Plugin.define "PHP-Slim" do
     rescue
     end
     info
-  end
-
-  def aggressive
-    target = @base_uri.dup
-    target.path = "/#{random_string}"
-    from_404 target.to_s
   end
 end
 
