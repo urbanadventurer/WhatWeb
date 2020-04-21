66a7f10ccd842b0abbf5985ad125476eb6b2fb45
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2020-04-21 06:36:38 -0300

    Updating PHP-Slim plugin

--- plugins/php-slim.rb
+++ plugins/php-slim.rb
@@ -5,52 +5,24 @@
 # http://www.morningstarsecurity.com/research/whatweb
 ##
 # Version 0.1 # 2020-04-04 # Marcelo Gimenes
+# Version 0.2 # 2020-04-21 # Marcelo Gimenes
 ##
 Plugin.define do
   name "PHP-Slim"
   authors [
     "Marcelo Gimenes <marcelo.gimenes.oliveira@gmail.com>"
   ]
-  version "0.1"
+  version "0.2"
   description "Slim Framework"
   website "https://www.slimframework.com/"
 
   matches [
     { :name => 'Slim < 4.0', 
-      :text => "to ensure your URL is spelled correctly. If all else fails, you can" },
+      :text => "to ensure your URL is spelled correctly. If all else fails, you can",
+      :url => randstr()},
     { :name => 'Slim > 4.0', 
-      :text => "The requested resource could not be found. Please verify the URI and try again." }
+      :text => "The requested resource could not be found. Please verify the URI and try again.",
+      :url => randstr()}
   ] 
-
-  aggressive do
-    random_string = (1..32).map{|i| ('a'..'z').to_a[rand(26)]}.join
-
-    target = @base_uri.dup
-    target.path = "/#{random_string}"
-
-    info = []
-    redirections = 0
-    begin
-      status, url, ip, body, headers = open_target(target.to_s)
-
-      if status == 301 && redirections < 3 
-        info = from_404(headers['location'], redirections+1)
-      end
-      if status == 404
-        texts = [
-          { :text => "to ensure your URL is spelled correctly. If all else fails, you can" }, # Slim < 4.0
-          { :text => "The requested resource could not be found. Please verify the URI and try again." } # Slim > 4.0
-        ]
-        texts.each do |m|
-          s = body.scan(m[:text])
-          unless s.empty?
-            info <<  { :certainty => m[:certainty] }
-          end
-        end
-      end
-    rescue
-    end
-    info
-  end
 end
 
