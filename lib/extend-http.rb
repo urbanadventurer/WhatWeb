dadf943d2bb8e067cc26cf081faa1c1fd03bed8c
urbanadventurer <horton.nz@gmail.com>
2020-08-05 23:44:03 +1000

    move @local_host and @local_port into connect

--- lib/extend-http.rb
+++ lib/extend-http.rb
@@ -24,10 +24,6 @@ class ExtendedHTTP < Net::HTTP #:nodoc:
 
     @address = address
     @port    = (port || HTTP.default_port)
-    @local_host = $local_host
-    @local_port = $local_port
-
-    puts "hi again from ExtendedHTTP.initialize @local_host: #{@local_host.inspect}, @local_port: #{@local_port.inspect}"
 
     @curr_http_version = HTTPVersion
     @keep_alive_timeout = 2
@@ -75,6 +71,12 @@ class ExtendedHTTP < Net::HTTP #:nodoc:
       conn_address = address
       conn_port    = port
     end
+
+    @local_host = $local_host
+    @local_port = $local_port
+
+    puts "hi again from ExtendedHTTP.connect @local_host: #{@local_host.inspect}, @local_port: #{@local_port.inspect}"
+
 
     D "opening connection to #{conn_address}:#{conn_port}..."
     s = Timeout.timeout(@open_timeout, Net::OpenTimeout) do
