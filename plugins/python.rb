9cf77aaceccb5910d8f343a71ba9d958e47ddd20
Andrew Horton <horton.nz@gmail.com>
2021-02-28 17:07:14 +1100

    2021-02-28 # Added File Extension, convereted passive block to matches array

--- plugins/python.rb
+++ plugins/python.rb
@@ -8,24 +8,23 @@ Plugin.define do
 name "Python"
 authors [
   "Brendan Coles <bcoles@gmail.com>", # 2010-10-26
+  "Andrew Horton", # v0.2 # 2021-02-28 # Added File Extension, convereted passive block to matches array
 ]
-version "0.1"
+version "0.2"
 description "Python is a programming language that lets you work more quickly and integrate your systems more effectively. You can learn to use Python and see almost immediate gains in productivity and lower maintenance costs."
 website "http://www.python.org/"
 
 # About 170076 ShodanHQ results for "server: python" @ 2010-10-26
 
-
-# HTTP Header
-passive do
-	m=[]
-
+# Matches #
+matches [
+	
 	# Server # Version Detection
-	m << { :version=>@headers["server"].to_s.scan(/[^\r^\n]* Python\/([^\s^\r^\n]+)/).flatten } if @headers["server"].to_s =~ /[^\r^\n]* Python\/([^\s^\r^\n]+)/
+	{ :name=>"# Server # Version Detection", :search=>"headers[server]", :version => /[^\r^\n]* Python\/([^\s^\r^\n]+)/},
 
-	m
-
-end
+	# File Extension
+	{ :name=>"File extension", :regexp=>/^(py|pyc|py3|pyo|pyw|pyx|pyd|pxd|pyi|pyz|pywz|rpy|pyde|pyp|pyt|xpy|ipynb)$/, :search=>"uri.extension" },
+]
 
 end
 
